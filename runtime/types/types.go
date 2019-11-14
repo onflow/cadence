468c8a557c1947ee382c958c15dc17fb86ce7bb0
Peter Siemens <peterjsiemens@gmail.com>
2019-11-13 17:45:27 -0800

    Add remaining types and values

--- runtime/types/types.go
+++ runtime/types/types.go
@@ -2,7 +2,33 @@ package types
 
 type Type interface{}
 
+type Void struct{}
+
+type Bool bool
+
 type String struct{}
+
+type Int struct{}
+
+type Int8 struct{}
+
+type Int16 struct{}
+
+type Int32 struct{}
+
+type Int64 struct{}
+
+type Uint8 struct{}
+
+type Uint16 struct{}
+
+type Uint32 struct{}
+
+type Uint64 struct{}
+
+type Array struct {
+	ElementType Type
+}
 
 type Composite struct {
 	FieldTypes []Type
--- runtime/values/values.go
+++ runtime/values/values.go
@@ -1,11 +1,73 @@
 package values
 
-type Value interface{}
+type Value interface {
+	// TODO: remove this function after encoding/decoding is implemented
+	ToGoValue() interface{}
+}
+
+type Void struct{}
+
+func (v Void) ToGoValue() interface{} { return nil }
+
+type Nil struct{}
+
+func (v Nil) ToGoValue() interface{} { return nil }
+
+type Bool bool
+
+func (v Bool) ToGoValue() interface{} { return bool(v) }
 
 type String string
+
+func (v String) ToGoValue() interface{} { return string(v) }
+
+// TODO: use big.Int to represent Int value
+type Int int
+
+func (v Int) ToGoValue() interface{} { return int(v) }
+
+type Int8 int8
+
+func (v Int8) ToGoValue() interface{} { return int8(v) }
+
+type Int16 int16
+
+func (v Int16) ToGoValue() interface{} { return int16(v) }
+
+type Int32 int32
+
+func (v Int32) ToGoValue() interface{} { return int32(v) }
+
+type Int64 int64
+
+func (v Int64) ToGoValue() interface{} { return int64(v) }
+
+type Uint8 uint8
+
+func (v Uint8) ToGoValue() interface{} { return uint8(v) }
+
+type Uint16 uint16
+
+func (v Uint16) ToGoValue() interface{} { return uint16(v) }
+
+type Uint32 uint32
+
+func (v Uint32) ToGoValue() interface{} { return uint32(v) }
+
+type Uint64 uint64
+
+func (v Uint64) ToGoValue() interface{} { return uint64(v) }
+
+type Array []Value
+
+func (v Array) ToGoValue() interface{} { panic("not implemented") }
 
 type Composite struct {
 	Fields []Value
 }
 
+func (v Composite) ToGoValue() interface{} { panic("not implemented") }
+
 type Dictionary map[Value]Value
+
+func (v Dictionary) ToGoValue() interface{} { panic("not implemented") }
