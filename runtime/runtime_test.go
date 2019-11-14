6f276f5f5defc030699a1147cfad5b377e8de912
Peter Siemens <peterjsiemens@gmail.com>
2019-11-13 17:47:15 -0800

    Update runtime_test.go

--- runtime/runtime_test.go
+++ runtime/runtime_test.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/dapperlabs/flow-go/language/runtime/errors"
 	"github.com/dapperlabs/flow-go/language/runtime/values"
