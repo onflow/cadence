d6e146ce49ceffd8ad28137497fa7f420f89dc41
Bastian Müller <bastian@axiomzen.co>
2019-11-19 17:17:32 -0800

    simplify tests

--- runtime/tests/checker/member_test.go
+++ runtime/tests/checker/member_test.go
@@ -6,194 +6,95 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/dapperlabs/flow-go/language/runtime/ast"
-	"github.com/dapperlabs/flow-go/language/runtime/common"
 	"github.com/dapperlabs/flow-go/language/runtime/sema"
-	"github.com/dapperlabs/flow-go/language/runtime/stdlib"
 	. "github.com/dapperlabs/flow-go/language/runtime/tests/utils"
 )
 
 func TestCheckOptionalChainingFieldRead(t *testing.T) {
 
-	testType := &sema.CompositeType{
-		Kind:       common.CompositeKindStructure,
-		Identifier: "Test",
-		Members:    map[string]*sema.Member{},
-	}
+	checker, err := ParseAndCheck(t, `
+      struct Test {
+          let x: Int
+          init(x: Int) {
+              self.x = x
+          }
+      }
 
-	fieldType := &sema.IntType{}
-
-	testType.Members["x"] = &sema.Member{
-		Type:            fieldType,
-		DeclarationKind: common.DeclarationKindField,
-		VariableKind:    ast.VariableKindConstant,
-	}
-
-	values := stdlib.StandardLibraryValues{
-		{
-			Name: "test",
-			Type: &sema.OptionalType{
-				Type: testType,
-			},
-			Kind:       common.DeclarationKindConstant,
-			IsConstant: true,
-		},
-	}.ToValueDeclarations()
-
-	checker, err := ParseAndCheckWithOptions(t,
-		`
-          let x = test?.x
-        `,
-		ParseAndCheckOptions{
-			Options: []sema.Option{
-				sema.WithPredeclaredValues(values),
-			},
-		},
-	)
+      let test: Test? = Test(x: 1)
+      let x = test?.x
+    `)
 
 	require.Nil(t, err)
 
-	assert.True(t, checker.GlobalValues["x"].Type.Equal(
-		&sema.OptionalType{Type: fieldType},
-	))
+	assert.Equal(t,
+		&sema.OptionalType{Type: &sema.IntType{}},
+		checker.GlobalValues["x"].Type,
+	)
 }
 
 func TestCheckOptionalChainingFunctionRead(t *testing.T) {
 
-	testType := &sema.CompositeType{
-		Kind:       common.CompositeKindStructure,
-		Identifier: "Test",
-		Members:    map[string]*sema.Member{},
-	}
+	checker, err := ParseAndCheck(t, `
+      struct Test {
+          fun x(): Int {
+              return 42
+          }
+      }
 
-	functionType := &sema.FunctionType{
-		ReturnTypeAnnotation: &sema.TypeAnnotation{
-			Type: &sema.IntType{},
-		},
-	}
-	testType.Members["x"] = &sema.Member{
-		Type:            functionType,
-		DeclarationKind: common.DeclarationKindFunction,
-		VariableKind:    ast.VariableKindConstant,
-	}
-
-	values := stdlib.StandardLibraryValues{
-		{
-			Name: "test",
-			Type: &sema.OptionalType{
-				Type: testType,
-			},
-			Kind:       common.DeclarationKindConstant,
-			IsConstant: true,
-		},
-	}.ToValueDeclarations()
-
-	checker, err := ParseAndCheckWithOptions(t,
-		`
-          let x = test?.x
-        `,
-		ParseAndCheckOptions{
-			Options: []sema.Option{
-				sema.WithPredeclaredValues(values),
-			},
-		},
-	)
+      let test: Test? = Test()
+      let x = test?.x
+    `)
 
 	require.Nil(t, err)
 
-	assert.True(t, checker.GlobalValues["x"].Type.Equal(
-		&sema.OptionalType{Type: functionType},
-	))
+	assert.True(t,
+		checker.GlobalValues["x"].Type.Equal(
+			&sema.OptionalType{
+				Type: &sema.FunctionType{
+					ReturnTypeAnnotation: &sema.TypeAnnotation{
+						Type: &sema.IntType{},
+					},
+				},
+			},
+		),
+	)
 }
 
 func TestCheckOptionalChainingFunctionCall(t *testing.T) {
 
-	testType := &sema.CompositeType{
-		Kind:       common.CompositeKindStructure,
-		Identifier: "Test",
-		Members:    map[string]*sema.Member{},
-	}
+	checker, err := ParseAndCheck(t, `
+      struct Test {
+          fun x(): Int {
+              return 42
+          }
+      }
 
-	returnType := &sema.IntType{}
-
-	functionType := &sema.FunctionType{
-		ReturnTypeAnnotation: &sema.TypeAnnotation{
-			Type: returnType,
-		},
-	}
-	testType.Members["x"] = &sema.Member{
-		Type:            functionType,
-		DeclarationKind: common.DeclarationKindFunction,
-		VariableKind:    ast.VariableKindConstant,
-	}
-
-	values := stdlib.StandardLibraryValues{
-		{
-			Name: "test",
-			Type: &sema.OptionalType{
-				Type: testType,
-			},
-			Kind:       common.DeclarationKindConstant,
-			IsConstant: true,
-		},
-	}.ToValueDeclarations()
-
-	checker, err := ParseAndCheckWithOptions(t,
-		`
-          let x = test?.x()
-        `,
-		ParseAndCheckOptions{
-			Options: []sema.Option{
-				sema.WithPredeclaredValues(values),
-			},
-		},
-	)
+      let test: Test? = Test()
+      let x = test?.x()
+    `)
 
 	require.Nil(t, err)
 
-	assert.True(t, checker.GlobalValues["x"].Type.Equal(
-		&sema.OptionalType{Type: returnType},
-	))
+	assert.True(t,
+		checker.GlobalValues["x"].Type.Equal(
+			&sema.OptionalType{Type: &sema.IntType{}},
+		),
+	)
 }
 
 func TestCheckInvalidOptionalChainingNonOptional(t *testing.T) {
 
-	testType := &sema.CompositeType{
-		Kind:       common.CompositeKindStructure,
-		Identifier: "Test",
-		Members:    map[string]*sema.Member{},
-	}
+	_, err := ParseAndCheck(t, `
+      struct Test {
+          let x: Int
+          init(x: Int) {
+              self.x = x
+          }
+      }
 
-	functionType := &sema.FunctionType{
-		ReturnTypeAnnotation: &sema.TypeAnnotation{
-			Type: &sema.IntType{},
-		},
-	}
-	testType.Members["x"] = &sema.Member{
-		Type:            functionType,
-		DeclarationKind: common.DeclarationKindFunction,
-		VariableKind:    ast.VariableKindConstant,
-	}
-
-	values := stdlib.StandardLibraryValues{
-		{
-			Name:       "test",
-			Type:       testType,
-			Kind:       common.DeclarationKindConstant,
-			IsConstant: true,
-		},
-	}.ToValueDeclarations()
-
-	_, err := ParseAndCheckWithOptions(t,
-		`
-          let x = test?.x
-        `,
-		ParseAndCheckOptions{
-			Options: []sema.Option{
-				sema.WithPredeclaredValues(values),
-			},
-		},
-	)
+      let test = Test(x: 1)
+      let x = test?.x
+    `)
 
 	errs := ExpectCheckerErrors(t, err, 1)
 
@@ -202,43 +103,19 @@ func TestCheckInvalidOptionalChainingNonOptional(t *testing.T) {
 
 func TestCheckInvalidOptionalChainingFieldAssignment(t *testing.T) {
 
-	testType := &sema.CompositeType{
-		Kind:       common.CompositeKindStructure,
-		Identifier: "Test",
-		Members:    map[string]*sema.Member{},
-	}
-
-	fieldType := &sema.IntType{}
-
-	testType.Members["x"] = &sema.Member{
-		Type:            fieldType,
-		DeclarationKind: common.DeclarationKindField,
-		VariableKind:    ast.VariableKindVariable,
-	}
-
-	values := stdlib.StandardLibraryValues{
-		{
-			Name: "test",
-			Type: &sema.OptionalType{
-				Type: testType,
-			},
-			Kind:       common.DeclarationKindConstant,
-			IsConstant: true,
-		},
-	}.ToValueDeclarations()
-
-	_, err := ParseAndCheckWithOptions(t,
-		`
-          fun foo() {
-              test?.x = 1
+	_, err := ParseAndCheck(t, `
+      struct Test {
+          var x: Int
+          init(x: Int) {
+              self.x = x
           }
-        `,
-		ParseAndCheckOptions{
-			Options: []sema.Option{
-				sema.WithPredeclaredValues(values),
-			},
-		},
-	)
+      }
+
+      fun test() {
+          let test: Test? = Test(x: 1)
+          test?.x = 2
+      }
+    `)
 
 	errs := ExpectCheckerErrors(t, err, 1)
 
