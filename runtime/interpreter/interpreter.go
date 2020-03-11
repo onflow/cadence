da0110576021d616d54dc21a107c32dcbf1bc4e7
Yahya Hassanzadeh <14860644+yhassanzadeh@users.noreply.github.com>
2020-03-11 16:29:37 -0700

    VER: Yahya/2611 active triggerring (#2819)
    * Fixes broken tests
    
    * refactors verification per chunk
    
    * Adds minor updates to comments
    
    * Moves chunk assigner to module root
    
    * Adds mocks
    
    * Solves CI failure
    
    * Fixes missing collection test
    
    * Removes commented uncessary files
    
    * renames chunk assigner
    
    * refactors chunk assignment
    
    * resolves circular dependency
    
    * solves circular dependency
    
    * solves public assign tests
    
    * updates verification happy path test
    
    * Adds comments
    
    * Update engine/verification/ingest/engine.go
    
    Co-Authored-By: Jordan Schalm <jordan@dapperlabs.com>
    
    * Update engine/verification/ingest/engine.go
    
    Co-Authored-By: Jordan Schalm <jordan@dapperlabs.com>
    
    * Update engine/verification/ingest/engine.go
    
    Co-Authored-By: Jordan Schalm <jordan@dapperlabs.com>
    
    * Update engine/verification/ingest/engine.go
    
    Co-Authored-By: Jordan Schalm <jordan@dapperlabs.com>
    
    * Update engine/verification/test/verification_test.go
    
    Co-Authored-By: Jordan Schalm <jordan@dapperlabs.com>
    
    * Update model/flow/identifiertList.go
    
    Co-Authored-By: Jordan Schalm <jordan@dapperlabs.com>
    
    * Update engine/verification/ingest/engine.go
    
    Co-Authored-By: Jordan Schalm <jordan@dapperlabs.com>
    
    * Moves chunk assignment to model
    
    * Resolves build problems
    
    * Renames Assign to Add
    
    * Renames Assigner to Assign
    
    * Resolves build error
    
    * Update engine/verification/ingest/engine.go
    
    Co-Authored-By: Jordan Schalm <jordan@dapperlabs.com>
    
    * Resolves typoes
    
    * Resolve typo
    
    Co-authored-by: Jordan Schalm <jordan@dapperlabs.com>

--- runtime/interpreter/interpreter.go
+++ runtime/interpreter/interpreter.go
@@ -1185,7 +1185,7 @@ func (interpreter *Interpreter) VisitSwapStatement(swap *ast.SwapStatement) ast.
 						rightGetterSetter.set(NilValue{})
 					}
 
-					// Assign right value to left target
+					// Add right value to left target
 					// and left value to right target
 
 					rightValueCopy := interpreter.copyAndConvert(rightValue.(Value), rightType, leftType)
