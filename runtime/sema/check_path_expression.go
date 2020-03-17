067f443fa335a85bccd2590ab3ee1361a23c2fe9
Bastian Müller <bastian@axiomzen.co>
2020-03-17 14:44:35 -0700

    add missing checker file

---
+++ runtime/sema/check_path_expression.go
@@ -0,0 +1,24 @@
+package sema
+
+import (
+	"github.com/dapperlabs/flow-go/language/runtime/ast"
+	"github.com/dapperlabs/flow-go/language/runtime/common"
+)
+
+func (checker *Checker) VisitPathExpression(expression *ast.PathExpression) ast.Repr {
+
+	// Check that the domain is valid
+
+	domain := expression.Domain
+
+	if _, ok := common.AllPathDomainsByIdentifier[domain.Identifier]; !ok {
+		checker.report(
+			&InvalidPathDomainError{
+				ActualDomain: domain.Identifier,
+				Range:        ast.NewRangeFromPositioned(domain),
+			},
+		)
+	}
+
+	return &PathType{}
+}
