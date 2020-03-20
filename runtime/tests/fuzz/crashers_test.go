7dc0a4eb6901543cb0affdf6ee9b37cb5ffc3260
Bastian Müller <bastian@axiomzen.co>
2020-03-19 19:43:48 -0700

    add fuzzing function for parser and checker, add test for crashers

---
+++ runtime/tests/fuzz/crashers_test.go
@@ -0,0 +1,49 @@
+package fuzz
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dapperlabs/cadence"
+)
+
+const crashersDir = "../../../crashers"
+
+func TestCrashers(t *testing.T) {
+
+	f, err := os.Open(crashersDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	files, err := f.Readdir(-1)
+	_ = f.Close()
+
+	for _, file := range files {
+
+		name := file.Name()
+		if path.Ext(name) != "" {
+			continue
+		}
+
+		t.Run(name, func(t *testing.T) {
+
+			data, err := ioutil.ReadFile(path.Join(crashersDir, name))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.NotPanics(t,
+				func() {
+					cadence.Fuzz(data)
+				},
+				string(data),
+			)
+		})
+
+	}
+}
