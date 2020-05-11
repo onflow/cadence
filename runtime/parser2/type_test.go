57c5f6142c017f4ee2f9165b02638f69fe7918c7
Bastian Müller <bastian@axiomzen.co>
2020-05-10 17:19:05 -0700

    parse types

---
+++ runtime/parser2/type_test.go
@@ -0,0 +1,190 @@
+/*
+ * Cadence - The resource-oriented smart contract programming language
+ *
+ * Copyright 2019-2020 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package parser2
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/cadence/runtime/ast"
+	"github.com/onflow/cadence/runtime/tests/utils"
+)
+
+func TestParseNominalType(t *testing.T) {
+
+	t.Run("simple", func(t *testing.T) {
+		result, errs := ParseType("Int")
+		require.Empty(t, errs)
+
+		utils.AssertEqualWithDiff(t,
+			&ast.NominalType{
+				Identifier: ast.Identifier{
+					Identifier: "Int",
+					Pos:        ast.Position{Line: 1, Column: 0, Offset: 0},
+				},
+			},
+			result,
+		)
+	})
+
+	t.Run("nested", func(t *testing.T) {
+		result, errs := ParseType("Foo.Bar")
+		require.Empty(t, errs)
+
+		utils.AssertEqualWithDiff(t,
+			&ast.NominalType{
+				Identifier: ast.Identifier{
+					Identifier: "Foo",
+					Pos:        ast.Position{Line: 1, Column: 0, Offset: 0},
+				},
+				NestedIdentifiers: []ast.Identifier{
+					{
+						Identifier: "Bar",
+						Pos:        ast.Position{Line: 1, Column: 4, Offset: 4},
+					},
+				},
+			},
+			result,
+		)
+	})
+}
+
+func TestParseArrayType(t *testing.T) {
+
+	t.Run("variable", func(t *testing.T) {
+		result, errs := ParseType("[Int]")
+		require.Empty(t, errs)
+
+		utils.AssertEqualWithDiff(t,
+			&ast.VariableSizedType{
+				Type: &ast.NominalType{
+					Identifier: ast.Identifier{
+						Identifier: "Int",
+						Pos:        ast.Position{Line: 1, Column: 1, Offset: 1},
+					},
+				},
+				Range: ast.Range{
+					StartPos: ast.Position{Line: 1, Column: 0, Offset: 0},
+					EndPos:   ast.Position{Line: 1, Column: 4, Offset: 4},
+				},
+			},
+			result,
+		)
+	})
+
+	t.Run("constant", func(t *testing.T) {
+		result, errs := ParseType("[Int ; 2 ]")
+		require.Empty(t, errs)
+
+		utils.AssertEqualWithDiff(t,
+			&ast.ConstantSizedType{
+				Type: &ast.NominalType{
+					Identifier: ast.Identifier{
+						Identifier: "Int",
+						Pos:        ast.Position{Line: 1, Column: 1, Offset: 1},
+					},
+				},
+				Size: &ast.IntegerExpression{
+					Value: big.NewInt(1),
+					Base:  10,
+					Range: ast.Range{
+						StartPos: ast.Position{Line: 1, Column: 7, Offset: 7},
+						EndPos:   ast.Position{Line: 1, Column: 7, Offset: 7},
+					},
+				},
+				Range: ast.Range{
+					StartPos: ast.Position{Line: 1, Column: 0, Offset: 0},
+					EndPos:   ast.Position{Line: 1, Column: 9, Offset: 9},
+				},
+			},
+			result,
+		)
+	})
+
+}
+
+func TestParseOptionalType(t *testing.T) {
+
+	t.Run("nominal", func(t *testing.T) {
+		result, errs := ParseType("Int?")
+		require.Empty(t, errs)
+
+		utils.AssertEqualWithDiff(t,
+			&ast.OptionalType{
+				Type: &ast.NominalType{
+					Identifier: ast.Identifier{
+						Identifier: "Int",
+						Pos:        ast.Position{Line: 1, Column: 0, Offset: 0},
+					},
+				},
+				EndPos: ast.Position{Line: 1, Column: 3, Offset: 3},
+			},
+			result,
+		)
+	})
+}
+
+func TestParseReferenceType(t *testing.T) {
+
+	t.Run("unauthorized, nominal", func(t *testing.T) {
+		result, errs := ParseType("&Int")
+		require.Empty(t, errs)
+
+		utils.AssertEqualWithDiff(t,
+			&ast.ReferenceType{
+				Authorized: false,
+				Type: &ast.NominalType{
+					Identifier: ast.Identifier{
+						Identifier: "Int",
+						Pos:        ast.Position{Line: 1, Column: 1, Offset: 1},
+					},
+				},
+				StartPos: ast.Position{Line: 1, Column: 0, Offset: 0},
+			},
+			result,
+		)
+	})
+}
+
+func TestParseOptionalReferenceType(t *testing.T) {
+
+	t.Run("unauthorized", func(t *testing.T) {
+		result, errs := ParseType("&Int?")
+		require.Empty(t, errs)
+
+		utils.AssertEqualWithDiff(t,
+			&ast.OptionalType{
+				Type: &ast.ReferenceType{
+					Authorized: false,
+					Type: &ast.NominalType{
+						Identifier: ast.Identifier{
+							Identifier: "Int",
+							Pos:        ast.Position{Line: 1, Column: 1, Offset: 1},
+						},
+					},
+					StartPos: ast.Position{Line: 1, Column: 0, Offset: 0},
+				},
+				EndPos: ast.Position{Line: 1, Column: 4, Offset: 4},
+			},
+			result,
+		)
+	})
+}
