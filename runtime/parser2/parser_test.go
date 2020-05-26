487162185aac510e18ddbe78cc819181469de65c
Bastian Müller <bastian@axiomzen.co>
2020-05-26 16:07:31 -0700

    add support for buffering tokens and replaying them

--- runtime/parser2/parser_test.go
+++ runtime/parser2/parser_test.go
@@ -19,10 +19,14 @@
 package parser2
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
+
+	"github.com/onflow/cadence/runtime/parser2/lexer"
 )
 
 func TestMain(m *testing.M) {
@@ -35,4 +39,185 @@ func TestParseInvalid(t *testing.T) {
 
 	_, errs := ParseExpression("#")
 	require.NotEmpty(t, errs)
+}
+
+func TestParseBuffering(t *testing.T) {
+
+	t.Parallel()
+
+	t.Run("buffer and accept, valid", func(t *testing.T) {
+
+		t.Parallel()
+
+		_, errs := Parse("a b c d", func(p *parser) interface{} {
+			p.mustOneString(lexer.TokenIdentifier, "a")
+			p.mustOne(lexer.TokenSpace)
+
+			p.startBuffering()
+
+			p.mustOneString(lexer.TokenIdentifier, "b")
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "c")
+
+			p.acceptBuffered()
+
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "d")
+
+			return nil
+		})
+
+		assert.Empty(t, errs)
+	})
+
+	t.Run("buffer and accept, invalid", func(t *testing.T) {
+
+		t.Parallel()
+
+		_, errs := Parse("a b x d", func(p *parser) interface{} {
+			p.mustOneString(lexer.TokenIdentifier, "a")
+			p.mustOne(lexer.TokenSpace)
+
+			p.startBuffering()
+
+			p.mustOneString(lexer.TokenIdentifier, "b")
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "c")
+
+			p.acceptBuffered()
+
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "d")
+
+			return nil
+		})
+
+		assert.Equal(t,
+			[]error{
+				errors.New("expected token identifier with string value c"),
+			},
+			errs,
+		)
+	})
+
+	t.Run("buffer and replay, valid", func(t *testing.T) {
+
+		t.Parallel()
+
+		_, errs := Parse("a b c d", func(p *parser) interface{} {
+			p.mustOneString(lexer.TokenIdentifier, "a")
+			p.mustOne(lexer.TokenSpace)
+
+			p.startBuffering()
+
+			p.mustOneString(lexer.TokenIdentifier, "b")
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "c")
+
+			p.replayBuffered()
+
+			p.mustOneString(lexer.TokenIdentifier, "b")
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "c")
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "d")
+
+			return nil
+		})
+
+		assert.Empty(t, errs)
+	})
+
+	t.Run("buffer and replay, invalid first", func(t *testing.T) {
+
+		t.Parallel()
+
+		_, errs := Parse("a b c d", func(p *parser) interface{} {
+			p.mustOneString(lexer.TokenIdentifier, "a")
+			p.mustOne(lexer.TokenSpace)
+
+			p.startBuffering()
+
+			firstSucceeded := false
+			firstFailed := false
+
+			(func() {
+				defer func() {
+					if r := recover(); r != nil {
+						firstFailed = true
+					}
+				}()
+
+				p.mustOneString(lexer.TokenIdentifier, "x")
+				p.mustOne(lexer.TokenSpace)
+				p.mustOneString(lexer.TokenIdentifier, "c")
+
+				firstSucceeded = true
+			})()
+
+			assert.True(t, firstFailed)
+			assert.False(t, firstSucceeded)
+
+			p.replayBuffered()
+
+			p.mustOneString(lexer.TokenIdentifier, "b")
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "c")
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "d")
+
+			return nil
+		})
+
+		assert.Empty(t, errs)
+	})
+
+	t.Run("buffer and replay, invalid first and invalid second", func(t *testing.T) {
+
+		t.Parallel()
+
+		_, errs := Parse("a b c x", func(p *parser) interface{} {
+			p.mustOneString(lexer.TokenIdentifier, "a")
+			p.mustOne(lexer.TokenSpace)
+
+			p.startBuffering()
+
+			firstSucceeded := false
+			firstFailed := false
+
+			(func() {
+				defer func() {
+					if r := recover(); r != nil {
+						firstFailed = true
+					}
+				}()
+
+				p.mustOneString(lexer.TokenIdentifier, "x")
+				p.mustOne(lexer.TokenSpace)
+				p.mustOneString(lexer.TokenIdentifier, "c")
+
+				firstSucceeded = true
+			})()
+
+			assert.True(t, firstFailed)
+			assert.False(t, firstSucceeded)
+
+			p.replayBuffered()
+
+			p.mustOneString(lexer.TokenIdentifier, "b")
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "c")
+			p.mustOne(lexer.TokenSpace)
+			p.mustOneString(lexer.TokenIdentifier, "d")
+
+			return nil
+		})
+
+		assert.Equal(t,
+			[]error{
+				errors.New("expected token identifier with string value d"),
+			},
+			errs,
+		)
+	})
 }
