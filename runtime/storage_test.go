a52e2cda465c1b42de3c9dd235e984720e224f2d
Bastian Müller <bastian@axiomzen.co>
2020-07-13 17:55:15 -0700

    test that deploy, write, and change result in correct high-level write callbacks

--- runtime/storage_test.go
+++ runtime/storage_test.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/onflow/cadence"
 	"github.com/onflow/cadence/runtime/common"
+	"github.com/onflow/cadence/runtime/tests/utils"
 )
 
 func TestRuntimeHighLevelStorage(t *testing.T) {
@@ -34,37 +35,219 @@ func TestRuntimeHighLevelStorage(t *testing.T) {
 
 	runtime := NewInterpreterRuntime()
 
-	tx := []byte(`
-      transaction {
-        prepare(signer: AuthAccount) {
-          signer.save(42, to: /storage/number)
-        }
-      }
+	address := common.BytesToAddress([]byte{0xCA, 0xDE})
+	contract := []byte(`
+       pub contract Test {
+
+           pub resource R {
+               pub var i: Int
+
+               init(_ i: Int) {
+                   self.i = i
+               }
+
+               pub fun update(_ i: Int) {
+                   self.i = i
+               }
+           }
+
+           pub fun createR(_ i: Int): @R {
+               return <-create R(i)
+           }
+       }
     `)
 
-	var storedValue cadence.Value
+	deployTx := utils.DeploymentTransaction(contract)
+
+	setupTx := []byte(`
+	  import Test from 0xCADE
+
+	  transaction {
+
+	      prepare(signer: AuthAccount) {
+	          let rs <- {
+	             "r1": <- Test.createR(3),
+	             "r2": <- Test.createR(4)
+	          }
+	          signer.save(<-rs, to: /storage/rs)
+	      }
+	   }
+	`)
+
+	changeTx := []byte(`
+	  import Test from 0xCADE
+
+	  transaction {
+
+	      prepare(signer: AuthAccount) {
+	          let rs = signer.borrow<&{String: Test.R}>(from: /storage/rs)!
+              rs["r1"]?.update(5)
+	      }
+	   }
+	`)
+
+	var accountCode []byte
+	var events []cadence.Event
+
+	type write struct {
+		owner common.Address
+		key   string
+		value cadence.Value
+	}
+
+	var writes []write
 
 	runtimeInterface := &testRuntimeInterface{
+		resolveImport: func(_ Location) (bytes []byte, err error) {
+			return accountCode, nil
+		},
 		storage: newTestStorage(nil, nil),
 		getSigningAccounts: func() []Address {
-			return []Address{
-				common.BytesToAddress([]byte{42}),
-			}
+			return []Address{address}
+		},
+		updateAccountCode: func(address Address, code []byte) (err error) {
+			accountCode = code
+			return nil
+		},
+		emitEvent: func(event cadence.Event) {
+			events = append(events, event)
 		},
 		setCadenceValue: func(owner common.Address, key string, value cadence.Value) (err error) {
-			if storedValue != nil {
-				t.Fail()
-			}
-			storedValue = value
-
+			writes = append(writes, write{
+				owner: owner,
+				key:   key,
+				value: value,
+			})
 			return nil
 		},
 	}
 
 	nextTransactionLocation := newTransactionLocationGenerator()
 
-	err := runtime.ExecuteTransaction(tx, nil, runtimeInterface, nextTransactionLocation())
+	writes = nil
+
+	err := runtime.ExecuteTransaction(deployTx, nil, runtimeInterface, nextTransactionLocation())
 	require.NoError(t, err)
 
-	assert.Equal(t, cadence.NewInt(42), storedValue)
+	assert.NotNil(t, accountCode)
+
+	rType := cadence.ResourceType{
+		TypeID:     "A.000000000000cade.Test.R",
+		Identifier: "R",
+		Fields: []cadence.Field{
+			{
+				Identifier: "i",
+				Type:       cadence.IntType{},
+			},
+			{
+				Identifier: "update",
+				Type: cadence.Function{
+					Parameters: []cadence.Parameter{
+						{
+							Label:      "_",
+							Identifier: "i",
+							Type:       cadence.IntType{},
+						},
+					},
+					ReturnType: cadence.VoidType{},
+				}.WithID("((Int):Void)"),
+			},
+			{
+				Identifier: "uuid",
+				Type:       cadence.UInt64Type{},
+			},
+		},
+	}
+
+	assert.Equal(t,
+		[]write{
+			{
+				address,
+				"contract",
+				cadence.NewContract([]cadence.Value{}).WithType(cadence.ContractType{
+					TypeID:     "A.000000000000cade.Test",
+					Identifier: "Test",
+					Fields: []cadence.Field{
+						{
+							Identifier: "createR",
+							Type: cadence.Function{
+								Parameters: []cadence.Parameter{
+									{
+										Label:      "_",
+										Identifier: "i",
+										Type:       cadence.IntType{},
+									},
+								},
+								ReturnType: rType,
+							}.WithID("((Int):A.000000000000cade.Test.R)"),
+						},
+					},
+					Initializers: nil,
+				}),
+			},
+		},
+		writes,
+	)
+
+	writes = nil
+
+	err = runtime.ExecuteTransaction(setupTx, nil, runtimeInterface, nextTransactionLocation())
+	require.NoError(t, err)
+
+	assert.Equal(t,
+		[]write{
+			{
+				address,
+				"storage\x1frs",
+				cadence.NewDictionary([]cadence.KeyValuePair{
+					{
+						Key: cadence.NewString("r1"),
+						Value: cadence.NewResource([]cadence.Value{
+							cadence.NewInt(3),
+							cadence.NewUInt64(0),
+						}).WithType(rType),
+					},
+					{
+						Key: cadence.NewString("r2"),
+						Value: cadence.NewResource([]cadence.Value{
+							cadence.NewInt(4),
+							cadence.NewUInt64(0),
+						}).WithType(rType),
+					},
+				}),
+			},
+		},
+		writes,
+	)
+
+	writes = nil
+
+	err = runtime.ExecuteTransaction(changeTx, nil, runtimeInterface, nextTransactionLocation())
+	require.NoError(t, err)
+
+	assert.Equal(t,
+		[]write{
+			{
+				address,
+				"storage\x1frs",
+				cadence.NewDictionary([]cadence.KeyValuePair{
+					{
+						Key: cadence.NewString("r1"),
+						Value: cadence.NewResource([]cadence.Value{
+							cadence.NewInt(5),
+							cadence.NewUInt64(0),
+						}).WithType(rType),
+					},
+					{
+						Key: cadence.NewString("r2"),
+						Value: cadence.NewResource([]cadence.Value{
+							cadence.NewInt(4),
+							cadence.NewUInt64(0),
+						}).WithType(rType),
+					},
+				}),
+			},
+		},
+		writes,
+	)
 }
