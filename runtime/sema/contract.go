adfa166aba0d5e668fd2d0f0857cfa07fc0b0201
Bastian Müller <bastian@axiomzen.co>
2020-08-20 19:19:57 -0700

    declare constructor function for Contract

--- runtime/sema/contract.go
+++ runtime/sema/contract.go
@@ -21,6 +21,7 @@ package sema
 import (
 	"github.com/onflow/cadence/runtime/ast"
 	"github.com/onflow/cadence/runtime/common"
+	"github.com/onflow/cadence/runtime/errors"
 )
 
 // ContractType represents the type `Contract`
@@ -114,4 +115,35 @@ func (t *ContractType) GetMembers() map[string]MemberResolver {
 			},
 		},
 	})
+}
+
+func init() {
+	addressType := &ContractType{}
+	typeName := addressType.String()
+
+	// check type is not accidentally redeclared
+	if _, ok := BaseValues[typeName]; ok {
+		panic(errors.NewUnreachableError())
+	}
+
+	BaseValues[typeName] = baseFunction{
+		name: typeName,
+		invokableType: &FunctionType{
+			Parameters: []*Parameter{
+				{
+					Identifier:     "name",
+					TypeAnnotation: NewTypeAnnotation(&StringType{}),
+				},
+				{
+					Identifier: "code",
+					TypeAnnotation: NewTypeAnnotation(
+						&VariableSizedType{
+							Type: &UInt8Type{},
+						},
+					),
+				},
+			},
+			ReturnTypeAnnotation: NewTypeAnnotation(addressType),
+		},
+	}
 }
