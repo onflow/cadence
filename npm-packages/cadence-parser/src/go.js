c9ea99b9b0fda0b4754d6f3beed1cda51c7f8655
Bastian Müller <bastian@axiomzen.co>
2020-10-14 17:13:23 -0700

    add a test

--- npm-packages/cadence-parser/src/go.js
+++ npm-packages/cadence-parser/src/go.js
@@ -48,17 +48,19 @@ global.fs = {
 };
 
 // NOTE: required and used by the WASM binary
-global.process = {
-  getuid() { return -1; },
-  getgid() { return -1; },
-  geteuid() { return -1; },
-  getegid() { return -1; },
-  getgroups() { throw enosys(); },
-  pid: -1,
-  ppid: -1,
-  umask() { throw enosys(); },
-  cwd() { throw enosys(); },
-  chdir() { throw enosys(); },
+if (!global.process) {
+  global.process = {
+    getuid() { return -1; },
+    getgid() { return -1; },
+    geteuid() { return -1; },
+    getegid() { return -1; },
+    getgroups() { throw enosys(); },
+    pid: -1,
+    ppid: -1,
+    umask() { throw enosys(); },
+    cwd() { throw enosys(); },
+    chdir() { throw enosys(); },
+  }
 }
 
 class Go {
