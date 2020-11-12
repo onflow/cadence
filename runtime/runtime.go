a18bb9752dd0dd884b6d35607bfb28d40ab7678b
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2020-11-12 00:07:09 -0800

    Add extended debug checking error for tracking unexpected execution errors

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -284,6 +284,13 @@ func (r *interpreterRuntime) ExecuteTransaction(
 
 	checker, err := r.parseAndCheckProgram(script, runtimeInterface, location, functions, nil, false)
 	if err != nil {
+		ee, is := err.(*ExtendedParsingCheckingError)
+		if is {
+			ee.RuntimeStorage = runtimeStorage
+			ee.Functions = functions
+			//return newError(ee.Err)
+			return newError(ee)
+		}
 		return newError(err)
 	}
 
@@ -488,14 +495,28 @@ func (r *interpreterRuntime) parseAndCheckProgram(
 			program, err = runtimeInterface.GetCachedProgram(location)
 		})
 		if err != nil {
-			return nil, err
+			return nil, &ExtendedParsingCheckingError{
+				Err:       err,
+				Code:      code,
+				Location:  location,
+				Functions: functions,
+				Options:   options,
+				UseCache:  useCache,
+			}
 		}
 	}
 
 	if program == nil {
 		program, err = r.parse(location, code, runtimeInterface)
 		if err != nil {
-			return nil, err
+			return nil, &ExtendedParsingCheckingError{
+				Err:       err,
+				Code:      code,
+				Location:  location,
+				Functions: functions,
+				Options:   options,
+				UseCache:  useCache,
+			}
 		}
 	}
 
@@ -555,12 +576,28 @@ func (r *interpreterRuntime) parseAndCheckProgram(
 		)...,
 	)
 	if err != nil {
-		return nil, err
+		return nil, &ExtendedParsingCheckingError{
+			Err:       err,
+			Code:      code,
+			Location:  location,
+			Functions: functions,
+			Options:   options,
+			UseCache:  useCache,
+			Checker:   checker,
+		}
 	}
 
 	err = checker.Check()
 	if err != nil {
-		return nil, err
+		return nil, &ExtendedParsingCheckingError{
+			Err:       err,
+			Code:      code,
+			Location:  location,
+			Functions: functions,
+			Options:   options,
+			UseCache:  useCache,
+			Checker:   checker,
+		}
 	}
 
 	// After the program has passed semantic analysis, cache the program AST.
