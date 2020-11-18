bc2d0b0900ae25646267bd6844f3e0e7934414d2
Bastian Müller <bastian@axiomzen.co>
2020-11-17 16:24:46 -0800

    remove dead code

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -291,7 +291,6 @@ func (r *interpreterRuntime) ExecuteTransaction(
 		if is {
 			ee.RuntimeStorage = runtimeStorage
 			ee.Functions = functions
-			//return newError(ee.Err)
 			return newError(ee)
 		}
 		return newError(err)
