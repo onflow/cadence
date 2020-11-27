7b3aa0e49ba279d3a1ef2799d1fa405611a57612
Ramtin Mehdizadeh Seraj <ramtin@axiomzen.co>
2020-11-27 10:46:22 -0800

    apply changes requested by review

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -641,13 +641,13 @@ func (r *interpreterRuntime) newInterpreter(
 			r.injectedCompositeFieldsHandler(runtimeInterface, runtimeStorage),
 		),
 		interpreter.WithUUIDHandler(func() (uuid uint64) {
+			var err error
 			wrapPanic(func() {
-				var err error
 				uuid, err = runtimeInterface.GenerateUUID()
-				if err != nil {
-					panic(err)
-				}
 			})
+			if err != nil {
+				panic(err)
+			}
 			return
 		}),
 		interpreter.WithContractValueHandler(
@@ -1321,12 +1321,13 @@ func (r *interpreterRuntime) newGetAccountFunction(runtimeInterface Interface) i
 func (r *interpreterRuntime) newLogFunction(runtimeInterface Interface) interpreter.HostFunction {
 	return func(invocation interpreter.Invocation) trampoline.Trampoline {
 		message := fmt.Sprint(invocation.Arguments[0])
+		var err error
 		wrapPanic(func() {
-			err := runtimeInterface.Log(message)
-			if err != nil {
-				panic(err)
-			}
+			err = runtimeInterface.Log(message)
 		})
+		if err != nil {
+			panic(err)
+		}
 		result := interpreter.VoidValue{}
 		return trampoline.Done{Result: result}
 	}
