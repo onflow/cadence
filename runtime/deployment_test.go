bbb660560d012ecfa9bd3636d19a1d0b17b3c89e
Bastian Müller <bastian@axiomzen.co>
2021-01-13 13:11:53 -0800

    improve the parsing / checking error: only include location, include programs in runtime error

--- runtime/deployment_test.go
+++ runtime/deployment_test.go
@@ -77,7 +77,7 @@ func TestRuntimeTransactionWithContractDeployment(t *testing.T) {
 		require.Equal(t, expectedCodeHash[:], actualCodeHash)
 	}
 
-	expectFailure := func(expectedErrorMessage string) expectation {
+	expectFailure := func(expectedProgramCount int, expectedErrorMessage string) expectation {
 		return func(t *testing.T, err error, accountCode []byte, events []cadence.Event, _ cadence.Type) {
 			var runtimeErr Error
 			utils.RequireErrorAs(t, err, &runtimeErr)
@@ -85,6 +85,9 @@ func TestRuntimeTransactionWithContractDeployment(t *testing.T) {
 			println(runtimeErr.Error())
 
 			assert.EqualError(t, runtimeErr, expectedErrorMessage)
+
+			assert.Len(t, runtimeErr.Codes, 2)
+			assert.Len(t, runtimeErr.Programs, expectedProgramCount)
 
 			assert.Nil(t, accountCode)
 			assert.Len(t, events, 0)
@@ -96,118 +99,209 @@ func TestRuntimeTransactionWithContractDeployment(t *testing.T) {
 	}
 
 	type testCase struct {
-		name      string
 		contract  string
 		arguments []argument
 		check     func(t *testing.T, err error, accountCode []byte, events []cadence.Event, expectedEventType cadence.Type)
 	}
 
-	tests := []testCase{
-		{
-			name: "no arguments",
+	test := func(t *testing.T, test testCase) {
+
+		t.Parallel()
+
+		contractArrayCode := fmt.Sprintf(
+			`"%s".decodeHex()`,
+			hex.EncodeToString([]byte(test.contract)),
+		)
+
+		argumentCodes := make([]string, len(test.arguments))
+
+		for i, argument := range test.arguments {
+			argumentCodes[i] = argument.String()
+		}
+
+		argumentCode := strings.Join(argumentCodes, ", ")
+		if len(test.arguments) > 0 {
+			argumentCode = ", " + argumentCode
+		}
+
+		script := []byte(fmt.Sprintf(
+			`
+              transaction {
+
+                  prepare(signer: AuthAccount) {
+                      signer.contracts.add(name: "Test", code: %s%s)
+                  }
+              }
+            `,
+			contractArrayCode,
+			argumentCode,
+		))
+
+		runtime := NewInterpreterRuntime()
+
+		var accountCode []byte
+		var events []cadence.Event
+
+		runtimeInterface := &testRuntimeInterface{
+			storage: newTestStorage(nil, nil),
+			getSigningAccounts: func() ([]Address, error) {
+				return []Address{{42}}, nil
+			},
+			getAccountContractCode: func(_ Address, _ string) (code []byte, err error) {
+				return accountCode, nil
+			},
+			updateAccountContractCode: func(_ Address, _ string, code []byte) error {
+				accountCode = code
+				return nil
+			},
+			emitEvent: func(event cadence.Event) error {
+				events = append(events, event)
+				return nil
+			},
+		}
+
+		nextTransactionLocation := newTransactionLocationGenerator()
+
+		err := runtime.ExecuteTransaction(
+			Script{
+				Source: script,
+			},
+			Context{
+				Interface: runtimeInterface,
+				Location:  nextTransactionLocation(),
+			},
+		)
+		exportedEventType := ExportType(
+			stdlib.AccountContractAddedEventType,
+			map[sema.TypeID]cadence.Type{},
+		)
+		test.check(t, err, accountCode, events, exportedEventType)
+	}
+
+	t.Run("no arguments", func(t *testing.T) {
+		test(t, testCase{
 			contract: `
-		     pub contract Test {}
-		   `,
+              pub contract Test {}
+            `,
 			arguments: []argument{},
 			check:     expectSuccess,
-		},
-		{
-			name: "with argument",
+		})
+	})
+
+	t.Run("with argument", func(t *testing.T) {
+		test(t, testCase{
 			contract: `
-		     pub contract Test {
-		         init(_ x: Int) {}
-		     }
-		   `,
+              pub contract Test {
+                  init(_ x: Int) {}
+              }
+            `,
 			arguments: []argument{
 				interpreter.NewIntValueFromInt64(1),
 			},
 			check: expectSuccess,
-		},
-		{
-			name: "with incorrect argument",
+		})
+	})
+
+	t.Run("with incorrect argument", func(t *testing.T) {
+		test(t, testCase{
 			contract: `
-		     pub contract Test {
-		         init(_ x: Int) {}
-		     }
-		   `,
+              pub contract Test {
+                  init(_ x: Int) {}
+              }
+            `,
 			arguments: []argument{
 				interpreter.BoolValue(true),
 			},
-			check: expectFailure("Execution failed:\n" +
-				"error: invalid argument 0: expected type `Int`, got `Bool`\n" +
-				" --> 00:5:26\n" +
-				"  |\n" +
-				"5 |                           signer.contracts.add(name: \"Test\", code: \"0a0909202020202070756220636f6e74726163742054657374207b0a0909202020202020202020696e6974285f20783a20496e7429207b7d0a090920202020207d0a0909202020\".decodeHex(), true)\n" +
-				"  |                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n",
+			check: expectFailure(
+				2,
+				"Execution failed:\n"+
+					"error: invalid argument 0: expected type `Int`, got `Bool`\n"+
+					" --> 00:5:22\n"+
+					"  |\n"+
+					"5 |                       signer.contracts.add(name: \"Test\", code: \"0a202020202020202020202020202070756220636f6e74726163742054657374207b0a202020202020202020202020202020202020696e6974285f20783a20496e7429207b7d0a20202020202020202020202020207d0a202020202020202020202020\".decodeHex(), true)\n"+
+					"  |                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n",
 			),
-		},
-		{
-			name: "additional argument",
+		})
+	})
+
+	t.Run("additional argument", func(t *testing.T) {
+		test(t, testCase{
 			contract: `
-		     pub contract Test {}
-		   `,
+              pub contract Test {}
+            `,
 			arguments: []argument{
 				interpreter.NewIntValueFromInt64(1),
 			},
-			check: expectFailure("Execution failed:\n" +
-				"error: invalid argument count, too many arguments: expected 0, got 1\n" +
-				" --> 00:5:26\n" +
-				"  |\n" +
-				"5 |                           signer.contracts.add(name: \"Test\", code: \"0a0909202020202070756220636f6e74726163742054657374207b7d0a0909202020\".decodeHex(), 1)\n" +
-				"  |                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n",
+			check: expectFailure(
+				2,
+				"Execution failed:\n"+
+					"error: invalid argument count, too many arguments: expected 0, got 1\n"+
+					" --> 00:5:22\n"+
+					"  |\n"+
+					"5 |                       signer.contracts.add(name: \"Test\", code: \"0a202020202020202020202020202070756220636f6e74726163742054657374207b7d0a202020202020202020202020\".decodeHex(), 1)\n"+
+					"  |                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n",
 			),
-		},
-		{
-			name: "additional code which is invalid at top-level",
+		})
+	})
+
+	t.Run("additional code which is invalid at top-level", func(t *testing.T) {
+		test(t, testCase{
 			contract: `
-		     pub contract Test {}
-		
-		     fun testCase() {}
-		   `,
+              pub contract Test {}
+
+              fun testCase() {}
+            `,
 			arguments: []argument{},
-			check: expectFailure("Execution failed:\n" +
-				"error: cannot deploy invalid contract\n" +
-				" --> 00:5:26\n" +
-				"  |\n" +
-				"5 |                           signer.contracts.add(name: \"Test\", code: \"0a0909202020202070756220636f6e74726163742054657374207b7d0a09090a0909202020202066756e2074657374436173652829207b7d0a0909202020\".decodeHex())\n" +
-				"  |                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" +
-				"\n" +
-				"error: function declarations are not valid at the top-level\n" +
-				" --> 2a00000000000000.Test:4:11\n" +
-				"  |\n" +
-				"4 | \t\t     fun testCase() {}\n" +
-				"  |            ^^^^^^^^\n" +
-				"\n" +
-				"error: missing access modifier for function\n" +
-				" --> 2a00000000000000.Test:4:7\n" +
-				"  |\n" +
-				"4 | \t\t     fun testCase() {}\n" +
-				"  |        ^\n",
+			check: expectFailure(
+				2,
+				"Execution failed:\n"+
+					"error: cannot deploy invalid contract\n"+
+					" --> 00:5:22\n"+
+					"  |\n"+
+					"5 |                       signer.contracts.add(name: \"Test\", code: \"0a202020202020202020202020202070756220636f6e74726163742054657374207b7d0a0a202020202020202020202020202066756e2074657374436173652829207b7d0a202020202020202020202020\".decodeHex())\n"+
+					"  |                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n"+
+					"\n"+
+					"error: function declarations are not valid at the top-level\n"+
+					" --> 2a00000000000000.Test:4:18\n"+
+					"  |\n"+
+					"4 |               fun testCase() {}\n"+
+					"  |                   ^^^^^^^^\n"+
+					"\n"+
+					"error: missing access modifier for function\n"+
+					" --> 2a00000000000000.Test:4:14\n"+
+					"  |\n"+
+					"4 |               fun testCase() {}\n"+
+					"  |               ^\n",
 			),
-		},
-		{
-			name: "invalid contract, parsing error",
+		})
+	})
+
+	t.Run("invalid contract, parsing error", func(t *testing.T) {
+		test(t, testCase{
 			contract: `
               X
             `,
 			arguments: []argument{},
 			check: expectFailure(
-				"Execution failed:\n" +
-					"error: cannot deploy invalid contract\n" +
-					" --> 00:5:26\n" +
-					"  |\n" +
-					"5 |                           signer.contracts.add(name: \"Test\", code: \"0a2020202020202020202020202020580a202020202020202020202020\".decodeHex())\n" +
-					"  |                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" +
-					"\n" +
-					"error: unexpected token: identifier\n" +
-					" --> 2a00000000000000.Test:2:14\n" +
-					"  |\n" +
-					"2 |               X\n" +
+				1,
+				"Execution failed:\n"+
+					"error: cannot deploy invalid contract\n"+
+					" --> 00:5:22\n"+
+					"  |\n"+
+					"5 |                       signer.contracts.add(name: \"Test\", code: \"0a2020202020202020202020202020580a202020202020202020202020\".decodeHex())\n"+
+					"  |                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n"+
+					"\n"+
+					"error: unexpected token: identifier\n"+
+					" --> 2a00000000000000.Test:2:14\n"+
+					"  |\n"+
+					"2 |               X\n"+
 					"  |               ^\n",
 			),
-		},
-		{
-			name: "invalid contract, checking error",
+		})
+	})
+
+	t.Run("invalid contract, checking error", func(t *testing.T) {
+		test(t, testCase{
 			contract: `
               pub contract Test {
                   pub fun test() { X }
@@ -215,100 +309,21 @@ func TestRuntimeTransactionWithContractDeployment(t *testing.T) {
             `,
 			arguments: []argument{},
 			check: expectFailure(
-				"Execution failed:\n" +
-					"error: cannot deploy invalid contract\n" +
-					" --> 00:5:26\n" +
-					"  |\n" +
-					"5 |                           signer.contracts.add(name: \"Test\", code: \"0a202020202020202020202020202070756220636f6e74726163742054657374207b0a2020202020202020202020202020202020207075622066756e20746573742829207b2058207d0a20202020202020202020202020207d0a202020202020202020202020\".decodeHex())\n" +
-					"  |                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" +
-					"\n" +
-					"error: cannot find variable in this scope: `X`\n" +
-					" --> 2a00000000000000.Test:3:35\n" +
-					"  |\n" +
-					"3 |                   pub fun test() { X }\n" +
+				2,
+				"Execution failed:\n"+
+					"error: cannot deploy invalid contract\n"+
+					" --> 00:5:22\n"+
+					"  |\n"+
+					"5 |                       signer.contracts.add(name: \"Test\", code: \"0a202020202020202020202020202070756220636f6e74726163742054657374207b0a2020202020202020202020202020202020207075622066756e20746573742829207b2058207d0a20202020202020202020202020207d0a202020202020202020202020\".decodeHex())\n"+
+					"  |                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n"+
+					"\n"+
+					"error: cannot find variable in this scope: `X`\n"+
+					" --> 2a00000000000000.Test:3:35\n"+
+					"  |\n"+
+					"3 |                   pub fun test() { X }\n"+
 					"  |                                    ^ not found in this scope\n",
 			),
-		},
-	}
-
-	test := func(test testCase) {
-
-		t.Run(test.name, func(t *testing.T) {
-
-			t.Parallel()
-
-			contractArrayCode := fmt.Sprintf(
-				`"%s".decodeHex()`,
-				hex.EncodeToString([]byte(test.contract)),
-			)
-
-			argumentCodes := make([]string, len(test.arguments))
-
-			for i, argument := range test.arguments {
-				argumentCodes[i] = argument.String()
-			}
-
-			argumentCode := strings.Join(argumentCodes, ", ")
-			if len(test.arguments) > 0 {
-				argumentCode = ", " + argumentCode
-			}
-
-			script := []byte(fmt.Sprintf(
-				`
-                  transaction {
-
-                      prepare(signer: AuthAccount) {
-                          signer.contracts.add(name: "Test", code: %s%s)
-                      }
-                  }
-                `,
-				contractArrayCode,
-				argumentCode,
-			))
-
-			runtime := NewInterpreterRuntime()
-
-			var accountCode []byte
-			var events []cadence.Event
-
-			runtimeInterface := &testRuntimeInterface{
-				storage: newTestStorage(nil, nil),
-				getSigningAccounts: func() ([]Address, error) {
-					return []Address{{42}}, nil
-				},
-				getAccountContractCode: func(_ Address, _ string) (code []byte, err error) {
-					return accountCode, nil
-				},
-				updateAccountContractCode: func(_ Address, _ string, code []byte) error {
-					accountCode = code
-					return nil
-				},
-				emitEvent: func(event cadence.Event) error {
-					events = append(events, event)
-					return nil
-				},
-			}
-
-			nextTransactionLocation := newTransactionLocationGenerator()
-
-			err := runtime.ExecuteTransaction(
-				Script{
-					Source: script,
-				},
-				Context{
-					Interface: runtimeInterface,
-					Location:  nextTransactionLocation(),
-				},
-			)
-			exportedEventType := ExportType(
-				stdlib.AccountContractAddedEventType,
-				map[sema.TypeID]cadence.Type{},
-			)
-			test.check(t, err, accountCode, events, exportedEventType)
 		})
-	}
+	})
 
-	for _, testCase := range tests {
-		test(testCase)
-	}
 }
