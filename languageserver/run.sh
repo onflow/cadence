15fcb83e38f34e085ff03fcbffae12562b61ea12
Bastian Müller <bastian@axiomzen.co>
2021-01-19 12:48:03 -0800

    only intercept language server command, forward all other commands to installed Flow CLI

--- languageserver/run.sh
+++ languageserver/run.sh
@@ -2,4 +2,10 @@
 
 SCRIPTPATH=$(dirname "$0")
 
-(cd "$SCRIPTPATH" && /usr/local/bin/go run ./cmd/languageserver/main.go "$@")
+
+if [ $1 == "cadence" -a $2 == "language-server" ] ; then
+	(cd "$SCRIPTPATH" && /usr/local/bin/go run ./cmd/languageserver/main.go "$@");
+else
+	flow "$@"
+fi
+
