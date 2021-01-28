167ce4612f0631b214ff2faa55cd69ac6950ae92
Bastian Müller <bastian@axiomzen.co>
2021-01-28 07:23:20 -0800

    add program file

---
+++ runtime/interpreter/program.go
@@ -0,0 +1,36 @@
+/*
+ * Cadence - The resource-oriented smart contract programming language
+ *
+ * Copyright 2019-2020 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package interpreter
+
+import (
+	"github.com/onflow/cadence/runtime/ast"
+	"github.com/onflow/cadence/runtime/sema"
+)
+
+type Program struct {
+	Program     *ast.Program
+	Elaboration *sema.Elaboration
+}
+
+func ProgramFromChecker(checker *sema.Checker) *Program {
+	return &Program{
+		Program:     checker.Program,
+		Elaboration: checker.Elaboration,
+	}
+}
