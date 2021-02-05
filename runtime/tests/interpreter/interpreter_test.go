056e1f94cffd3af58a8d0596c8bb382918cdd4af
Bastian Müller <bastian@axiomzen.co>
2021-02-04 16:57:20 -0800

    debug log decode calls

--- runtime/tests/interpreter/interpreter_test.go
+++ runtime/tests/interpreter/interpreter_test.go
@@ -7877,6 +7877,7 @@ func TestInterpretDictionaryValueEncodingOrder(t *testing.T) {
 			bytes.NewReader(encoded),
 			owner,
 			interpreter.CurrentEncodingVersion,
+			nil,
 		)
 		require.NoError(t, err)
 
