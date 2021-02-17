a71a20b6d84d608041b24086cae8e724b2c0c128
Bastian Müller <bastian@axiomzen.co>
2021-02-17 15:01:54 -0800

    use ordered maps for imported values and types

--- runtime/tests/interpreter/import_test.go
+++ runtime/tests/interpreter/import_test.go
@@ -102,14 +102,17 @@ func TestInterpretVirtualImport(t *testing.T) {
 			CheckerOptions: []sema.Option{
 				sema.WithImportHandler(
 					func(checker *sema.Checker, location common.Location) (sema.Import, error) {
+
+						valueElements := sema.NewStringImportElementOrderedMap()
+
+						valueElements.Set("Foo", sema.ImportElement{
+							DeclarationKind: common.DeclarationKindStructure,
+							Access:          ast.AccessPublic,
+							Type:            fooType,
+						})
+
 						return sema.VirtualImport{
-							ValueElements: map[string]sema.ImportElement{
-								"Foo": {
-									DeclarationKind: common.DeclarationKindStructure,
-									Access:          ast.AccessPublic,
-									Type:            fooType,
-								},
-							},
+							ValueElements: valueElements,
 						}, nil
 					},
 				),
