b6464a3b8e38886db735d7c051d93c3e5e6c0d40
Max Daunarovich <max.daunarovich@gmail.com>
2021-03-10 13:28:17 +0300

    Add utility printers

--- tools/prettier-plugin-cadence/src/utils.ts
+++ tools/prettier-plugin-cadence/src/utils.ts
@@ -1,3 +1,6 @@
+import { doc } from "prettier"
+const { line, group, softline } = doc.builders
+
 export function splitByLength(input: string, size: number): string[] {
 	const result = input.split(" ").reduce(
 		(acc: any, word: string): any => {
@@ -17,4 +20,50 @@ export function splitByLength(input: string, size: number): string[] {
 		}
 	)
 	return result.arr
+}
+
+function smartJoin(separator: string, parts: any[]) {
+	let result: any[];
+	let lastPart: any;
+
+	result = [];
+
+	parts.filter(x => x).forEach((part, index) => {
+		const firstPart = part.parts ? part.parts[0] : part;
+		if (
+			index > 0 &&
+			firstPart !== ":" &&
+			firstPart !== "..." &&
+			firstPart !== ")" &&
+			firstPart !== "<" &&
+			lastPart !== "("
+		) {
+			result.push(separator);
+		}
+
+		lastPart = part;
+		result.push(part);
+	});
+
+	return concat(result);
+}
+
+function concat(parts: any[]) {
+	parts = parts.filter(x => x);
+
+	switch (parts.length) {
+		case 0:
+			return "";
+		case 1:
+			return parts[0];
+		default:
+			return doc.builders.concat(parts);
+	}
+}
+
+export function printList(path: any, print: any) {
+	return concat([
+		softline,
+		group(smartJoin(concat([",", line]), path.map(print, "layout"))),
+	])
 }
