2fddc8dbc35d60f9c66d767b825c3367d73f7338
Bastian Müller <bastian@axiomzen.co>
2021-03-10 11:47:08 -0800

    add a clear function to ordered maps

--- runtime/sema/ordered_map_position_resourceuse.go
+++ runtime/sema/ordered_map_position_resourceuse.go
@@ -46,6 +46,14 @@ func NewAstPositionResourceUseOrderedMap() *AstPositionResourceUseOrderedMap {
 	}
 }
 
+// Clear removes all entries from this ordered map.
+func (om *AstPositionResourceUseOrderedMap) Clear() {
+	om.list.Init()
+	for key := range om.pairs {
+		delete(om.pairs, key)
+	}
+}
+
 // Get returns the value associated with the given key.
 // Returns nil if not found.
 // The second return value indicates if the key is present in the map.
