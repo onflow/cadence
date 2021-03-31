8142028278b3048eedd37eda69100b473ac9a6c8
Bastian Müller <bastian@axiomzen.co>
2021-03-30 17:35:57 -0700

    adjust tests

--- runtime/tests/checker/function_test.go
+++ runtime/tests/checker/function_test.go
@@ -162,14 +162,29 @@ func TestCheckReturnWithoutExpression(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestCheckAnyReturnType(t *testing.T) {
+func TestCheckFunctionUseInsideFunction(t *testing.T) {
 
 	t.Parallel()
 
 	_, err := ParseAndCheck(t, `
-      fun foo(): AnyStruct {
-          return foo
+      fun foo() {
+          foo()
       }
+    `)
+
+	require.NoError(t, err)
+}
+
+func TestCheckFunctionReturnFunction(t *testing.T) {
+
+	t.Parallel()
+
+	_, err := ParseAndCheck(t, `
+      fun foo(): ((Int): Void) {
+          return bar
+      }
+
+      fun bar(_ n: Int) {}
     `)
 
 	require.NoError(t, err)
