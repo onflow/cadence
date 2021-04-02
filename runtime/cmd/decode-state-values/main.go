2e7fd44206253087deea2cd56a51636177abea0f
Bastian Müller <bastian@axiomzen.co>
2021-04-02 12:02:04 -0700

    improve state decoding tool
    - parallelize
    - show progress bar
    - decode owner from key
    - also re-encode

--- runtime/cmd/decode-state-values/main.go
+++ runtime/cmd/decode-state-values/main.go
@@ -21,44 +21,44 @@
 package main
 
 import (
+	"bufio"
 	"encoding/hex"
 	"encoding/json"
 	"io"
 	"log"
 	"os"
+	"runtime"
+	"sync"
+	"sync/atomic"
 
+	"github.com/schollz/progressbar/v3"
+
+	"github.com/onflow/cadence/runtime/common"
 	"github.com/onflow/cadence/runtime/interpreter"
 )
 
-func main() {
-	file, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+type keyPart struct {
+	Value string
+}
 
-	var entry struct {
-		Value string
-	}
+type key struct {
+	KeyParts []keyPart
+}
 
-	var decoded int
+type entry struct {
+	Value string
+	Key   key
+}
 
-	decoder := json.NewDecoder(file)
-	for lines := 0; ; lines++ {
-		if lines%100 == 0 {
-			print(".")
-		}
+func worker(jobs <-chan entry, wg *sync.WaitGroup, decoded *uint64) {
+	defer wg.Done()
 
-		err = decoder.Decode(&entry)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatal(err)
-		}
+	var err error
+	var data []byte
 
-		var data []byte
-		data, err = hex.DecodeString(entry.Value)
+	for e := range jobs {
+
+		data, err = hex.DecodeString(e.Value)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -68,15 +68,90 @@ func main() {
 		if version == 0 {
 			continue
 		}
-		_, err = interpreter.DecodeValue(data, nil, nil, version, nil)
+
+		rawOwner, err := hex.DecodeString(e.Key.KeyParts[1].Value)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		decoded++
+		owner := common.BytesToAddress(rawOwner)
+
+		var value interpreter.Value
+		value, err = interpreter.DecodeValue(data, &owner, nil, version, nil)
+		if err != nil {
+			log.Fatalf("failed to decode value: %s\n%s\n", err, e.Value)
+		}
+
+		var deferrals *interpreter.EncodingDeferrals
+		_, deferrals, err = interpreter.EncodeValue(value, nil, true, nil)
+		if err != nil {
+			log.Fatalf("failed to encode value: %s\n%s\n", err, e.Value)
+		}
+
+		if len(deferrals.Values) > 0 {
+			log.Fatalf("re-encoding produced deferred values: %s\n%s\n", err, e.Value)
+		}
+
+		if len(deferrals.Moves) > 0 {
+			log.Fatalf("re-encoding produced deferred moves: %s\n%s\n", err, e.Value)
+		}
+
+		atomic.AddUint64(decoded, 1)
 	}
+}
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	jobs := make(chan entry)
+
+	var decoded uint64
+
+	var wg sync.WaitGroup
+
+	workerCount := runtime.NumCPU()
+
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go worker(jobs, &wg, &decoded)
+	}
+
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileSize := stat.Size()
+
+	bar := progressbar.DefaultBytes(fileSize, "(processed JSON bytes)")
+
+	progressReader := progressbar.NewReader(file, bar)
+	reader := bufio.NewReader(&progressReader)
+
+	decoder := json.NewDecoder(reader)
+	for {
+		var e entry
+
+		err = decoder.Decode(&e)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+
+		jobs <- e
+	}
+
+	close(jobs)
+
+	wg.Wait()
 
 	println()
 
-	log.Printf("successfully decoded %d values", decoded)
+	log.Printf("successfully decoded %d values\n", decoded)
 }
