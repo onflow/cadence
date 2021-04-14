8b8e6c27230c46623eb74593a9c868d168582394
Bastian Müller <bastian@axiomzen.co>
2021-04-13 17:54:41 -0700

    rename SpecialFunctionType to ConstructorFunctionType, allow FunctionType to have members

--- runtime/sema/elaboration.go
+++ runtime/sema/elaboration.go
@@ -39,7 +39,7 @@ type Elaboration struct {
 	AssignmentStatementValueTypes       map[*ast.AssignmentStatement]Type
 	AssignmentStatementTargetTypes      map[*ast.AssignmentStatement]Type
 	CompositeDeclarationTypes           map[*ast.CompositeDeclaration]*CompositeType
-	SpecialFunctionTypes                map[*ast.SpecialFunctionDeclaration]*SpecialFunctionType
+	ConstructorFunctionTypes            map[*ast.SpecialFunctionDeclaration]*ConstructorFunctionType
 	FunctionExpressionFunctionType      map[*ast.FunctionExpression]*FunctionType
 	InvocationExpressionArgumentTypes   map[*ast.InvocationExpression][]Type
 	InvocationExpressionParameterTypes  map[*ast.InvocationExpression][]Type
@@ -88,7 +88,7 @@ func NewElaboration() *Elaboration {
 		AssignmentStatementValueTypes:       map[*ast.AssignmentStatement]Type{},
 		AssignmentStatementTargetTypes:      map[*ast.AssignmentStatement]Type{},
 		CompositeDeclarationTypes:           map[*ast.CompositeDeclaration]*CompositeType{},
-		SpecialFunctionTypes:                map[*ast.SpecialFunctionDeclaration]*SpecialFunctionType{},
+		ConstructorFunctionTypes:            map[*ast.SpecialFunctionDeclaration]*ConstructorFunctionType{},
 		FunctionExpressionFunctionType:      map[*ast.FunctionExpression]*FunctionType{},
 		InvocationExpressionArgumentTypes:   map[*ast.InvocationExpression][]Type{},
 		InvocationExpressionParameterTypes:  map[*ast.InvocationExpression][]Type{},
