736a661647ba9572d3d25b056e65910b3951fa6b
Supun Setunga <supun.setunga@gmail.com>
2021-05-10 17:55:25 +0530

    Improve error handling in docgen

--- tools/docgen/gen/doc_generator.go
+++ tools/docgen/gen/doc_generator.go
@@ -5,8 +5,6 @@ import (
 	"path"
 	"strings"
 
-	"path/filepath"
-
 	"text/template"
 
 	"github.com/markbates/pkger"
@@ -15,6 +13,161 @@ import (
 	"github.com/onflow/cadence/runtime/common"
 	"github.com/onflow/cadence/runtime/parser2"
 )
+
+type DocGenerator struct {
+	entryPageGen     *template.Template
+	compositePageGen *template.Template
+	typeNames        []string
+	outputDir        string
+}
+
+func NewDocGenerator() *DocGenerator {
+	gen := &DocGenerator{}
+
+	functions["fileName"] = func(decl ast.Declaration) string {
+		pathPrefix := gen.fileName()
+		if len(pathPrefix) == 0 {
+			return decl.DeclarationIdentifier().String() + ".md"
+		}
+
+		return gen.fileName() + "_" + decl.DeclarationIdentifier().String() + ".md"
+	}
+
+	entryPageGen := template.New("base-template")
+	entryPageGen.Funcs(functions)
+	entryPageGen = registerTemplates(entryPageGen)
+
+	compositePageGen := template.New("composite-full-template")
+	compositePageGen.Funcs(functions)
+	compositePageGen = registerTemplates(compositePageGen)
+
+	gen.compositePageGen = compositePageGen
+	gen.entryPageGen = entryPageGen
+
+	return gen
+}
+
+func registerTemplates(tmpl *template.Template) *template.Template {
+	for _, templateFile := range templateFiles {
+		info, err := pkger.Current()
+		if err != nil {
+			panic(err)
+		}
+
+		filePath := path.Join(info.Dir, "gen", "templates", templateFile)
+
+		tmpl, err = tmpl.ParseFiles(filePath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return tmpl
+}
+
+func (gen *DocGenerator) Generate(source string, outputDir string) error {
+	gen.outputDir = outputDir
+
+	program, err := parser2.ParseProgram(source)
+	if err != nil {
+		return err
+	}
+
+	return gen.genProgram(program)
+}
+
+func (gen *DocGenerator) genProgram(program *ast.Program) error {
+
+	var soleDecl ast.Declaration = program.SoleContractDeclaration()
+	if soleDecl == nil {
+		soleDecl = program.SoleContractInterfaceDeclaration()
+	}
+
+	// If its not a sole-declaration, and has multiple top level declarations
+	// then generated an entry page.
+	if soleDecl == nil {
+		// Generate entry page
+		f, err := os.Create(gen.outputDir + "/index.md")
+		if err != nil {
+			return err
+		}
+
+		err = gen.entryPageGen.Execute(f, program)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Generate dedicated pages for all the nested composite declarations
+	return gen.genDeclarations(program.Declarations())
+}
+
+func (gen *DocGenerator) genDeclarations(decls []ast.Declaration) error {
+	var err error
+	for _, decl := range decls {
+		switch astDecl := decl.(type) {
+		case *ast.CompositeDeclaration:
+			err = gen.genCompositeDeclaration(astDecl)
+		case *ast.InterfaceDeclaration:
+			err = gen.genInterfaceDeclaration(astDecl)
+		default:
+			// do nothing
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (gen *DocGenerator) genCompositeDeclaration(declaration *ast.CompositeDeclaration) error {
+	if declaration.DeclarationKind() == common.DeclarationKindEvent {
+		return nil
+	}
+
+	declName := declaration.DeclarationIdentifier().String()
+	return gen.genCompositeDecl(declName, declaration.Members, declaration)
+}
+
+func (gen *DocGenerator) genInterfaceDeclaration(declaration *ast.InterfaceDeclaration) error {
+	declName := declaration.DeclarationIdentifier().String()
+	return gen.genCompositeDecl(declName, declaration.Members, declaration)
+}
+
+func (gen *DocGenerator) genCompositeDecl(name string, members *ast.Members, decl ast.Declaration) error {
+	gen.typeNames = append(gen.typeNames, name)
+
+	defer func() {
+		gen.typeNames = gen.typeNames[:len(gen.typeNames)-1]
+	}()
+
+	output := gen.outputDir + "/" + gen.fileName() + ".md"
+	f, err := os.Create(output)
+	if err != nil {
+		panic(err)
+	}
+
+	err = gen.compositePageGen.Execute(f, decl)
+	if err != nil {
+		panic(err)
+	}
+
+	return gen.genDeclarations(members.Declarations())
+}
+
+func (gen *DocGenerator) fileName() string {
+	builder := strings.Builder{}
+	for index, name := range gen.typeNames {
+		if index > 0 {
+			builder.WriteString("_")
+		}
+		builder.WriteString(name)
+	}
+
+	return builder.String()
+}
 
 var templateFiles = []string{
 	"base-template",
@@ -77,325 +230,4 @@ var functions = template.FuncMap{
 			return false
 		}
 	},
-}
-
-var ROOT_DIR = func() string {
-	wd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	return filepath.Dir(wd)
-}()
-
-type DocGenerator struct {
-	entryPageGen     *template.Template
-	compositePageGen *template.Template
-	typeNames        []string
-	outputDir        string
-}
-
-func NewDocGenerator() *DocGenerator {
-	gen := &DocGenerator{}
-
-	functions["fileName"] = func(decl ast.Declaration) string {
-		pathPrefix := gen.fileName()
-		if len(pathPrefix) == 0 {
-			return decl.DeclarationIdentifier().String() + ".md"
-		}
-
-		return gen.fileName() + "_" + decl.DeclarationIdentifier().String() + ".md"
-	}
-
-	entryPageGen := template.New("base-template")
-	entryPageGen.Funcs(functions)
-	entryPageGen = registerTemplates(entryPageGen)
-
-	compositePageGen := template.New("composite-full-template")
-	compositePageGen.Funcs(functions)
-	compositePageGen = registerTemplates(compositePageGen)
-
-	gen.compositePageGen = compositePageGen
-	gen.entryPageGen = entryPageGen
-
-	return gen
-}
-
-func registerTemplates(tmpl *template.Template) *template.Template {
-	for _, templateFile := range templateFiles {
-		info, err := pkger.Current()
-		if err != nil {
-			panic(err)
-		}
-
-		filePath := path.Join(info.Dir, "gen", "templates", templateFile)
-
-		tmpl, err = tmpl.ParseFiles(filePath)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	return tmpl
-}
-
-func (gen *DocGenerator) Generate(source string, outputDir string) error {
-	gen.outputDir = outputDir
-
-	program, err := parser2.ParseProgram(source)
-	if err != nil {
-		return err
-	}
-
-	program.Accept(gen)
-	return nil
-}
-
-var _ ast.Visitor = &DocGenerator{}
-
-func (gen *DocGenerator) VisitProgram(program *ast.Program) ast.Repr {
-
-	var soleDecl ast.Declaration = program.SoleContractDeclaration()
-	if soleDecl == nil {
-		soleDecl = program.SoleContractInterfaceDeclaration()
-	}
-
-	// If its not a sole-declaration, and has multiple top level declarations
-	// then generated an entry page.
-	if soleDecl == nil {
-		// Generate entry page
-		f, err := os.Create(gen.outputDir + "/index.md")
-		if err != nil {
-			return err
-		}
-
-		err = gen.entryPageGen.Execute(f, program)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Generate dedicated pages for all the nested composite declarations
-	for _, decl := range program.Declarations() {
-		decl.Accept(gen)
-	}
-
-	return nil
-}
-
-func (gen *DocGenerator) VisitFunctionDeclaration(declaration *ast.FunctionDeclaration) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitCompositeDeclaration(declaration *ast.CompositeDeclaration) ast.Repr {
-	if declaration.DeclarationKind() == common.DeclarationKindEvent {
-		return nil
-	}
-
-	declName := declaration.DeclarationIdentifier().String()
-	return gen.genCompositeDecl(declName, declaration.Members, declaration)
-}
-
-func (gen *DocGenerator) genCompositeDecl(name string, members *ast.Members, decl ast.Declaration) ast.Repr {
-	gen.typeNames = append(gen.typeNames, name)
-
-	defer func() {
-		gen.typeNames = gen.typeNames[:len(gen.typeNames)-1]
-	}()
-
-	output := gen.outputDir + "/" + gen.fileName() + ".md"
-	f, err := os.Create(output)
-	if err != nil {
-		panic(err)
-	}
-
-	err = gen.compositePageGen.Execute(f, decl)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, decl := range members.Declarations() {
-		decl.Accept(gen)
-	}
-
-	return nil
-}
-
-func (gen *DocGenerator) fileName() string {
-	builder := strings.Builder{}
-	for index, name := range gen.typeNames {
-		if index > 0 {
-			builder.WriteString("_")
-		}
-		builder.WriteString(name)
-	}
-
-	return builder.String()
-}
-
-func (gen *DocGenerator) VisitInterfaceDeclaration(declaration *ast.InterfaceDeclaration) ast.Repr {
-	declName := declaration.DeclarationIdentifier().String()
-	return gen.genCompositeDecl(declName, declaration.Members, declaration)
-}
-
-// Unused methods
-
-func (gen *DocGenerator) VisitFieldDeclaration(declaration *ast.FieldDeclaration) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitEnumCaseDeclaration(declaration *ast.EnumCaseDeclaration) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitPragmaDeclaration(declaration *ast.PragmaDeclaration) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitImportDeclaration(declaration *ast.ImportDeclaration) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitTransactionDeclaration(declaration *ast.TransactionDeclaration) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitReturnStatement(statement *ast.ReturnStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitBreakStatement(statement *ast.BreakStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitContinueStatement(statement *ast.ContinueStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitIfStatement(statement *ast.IfStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitSwitchStatement(statement *ast.SwitchStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitWhileStatement(statement *ast.WhileStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitForStatement(statement *ast.ForStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitEmitStatement(statement *ast.EmitStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitVariableDeclaration(declaration *ast.VariableDeclaration) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitAssignmentStatement(statement *ast.AssignmentStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitSwapStatement(statement *ast.SwapStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitExpressionStatement(statement *ast.ExpressionStatement) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitBoolExpression(expression *ast.BoolExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitNilExpression(expression *ast.NilExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitIntegerExpression(expression *ast.IntegerExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitFixedPointExpression(expression *ast.FixedPointExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitArrayExpression(expression *ast.ArrayExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitDictionaryExpression(expression *ast.DictionaryExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitIdentifierExpression(expression *ast.IdentifierExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitInvocationExpression(expression *ast.InvocationExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitMemberExpression(expression *ast.MemberExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitIndexExpression(expression *ast.IndexExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitConditionalExpression(expression *ast.ConditionalExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitUnaryExpression(expression *ast.UnaryExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitBinaryExpression(expression *ast.BinaryExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitFunctionExpression(expression *ast.FunctionExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitStringExpression(expression *ast.StringExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitCastingExpression(expression *ast.CastingExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitCreateExpression(expression *ast.CreateExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitDestroyExpression(expression *ast.DestroyExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitReferenceExpression(expression *ast.ReferenceExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitForceExpression(expression *ast.ForceExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitPathExpression(expression *ast.PathExpression) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitBlock(block *ast.Block) ast.Repr {
-	return nil
-}
-
-func (gen *DocGenerator) VisitFunctionBlock(block *ast.FunctionBlock) ast.Repr {
-	return nil
 }
