b9b807243fbaa40415e3e817a0dcfd46c1e70764
Bastian Müller <bastian@axiomzen.co>
2021-05-24 14:05:13 -0700

    include composite declaration in conformance error

--- runtime/sema/errors.go
+++ runtime/sema/errors.go
@@ -925,6 +925,7 @@ type InitializerMismatch struct {
 //  use `InitializerMismatch`, `MissingMembers`, `MemberMismatches`, etc
 
 type ConformanceError struct {
+	CompositeDeclaration           *ast.CompositeDeclaration
 	CompositeType                  *CompositeType
 	InterfaceType                  *InterfaceType
 	InitializerMismatch            *InitializerMismatch
