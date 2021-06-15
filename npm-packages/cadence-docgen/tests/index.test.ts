5f81bc4f4e015c9f04ebb8a20b806f9001bc8654
Supun Setunga <supun.setunga@gmail.com>
2021-06-15 15:39:00 +0530

    Add docgen npm tests

--- npm-packages/cadence-docgen/tests/index.test.ts
+++ npm-packages/cadence-docgen/tests/index.test.ts
@@ -17,3 +17,28 @@ test("docgen simple", async () => {
     }
   })
 })
+
+test("docgen errors", async () => {
+  const binary = fs.readFileSync(require.resolve('../dist/cadence-docgen.wasm'))
+  const docgen = await CadenceDocgen.create(binary)
+
+  const res = docgen.generate(`
+    /// This contract doesn't have a name - Syntax error.
+    contract {
+    }
+  `)
+
+  expect(res.docs).toEqual(null)
+
+  expect(res.error.Errors).toEqual([
+    {
+      "Message": "expected identifier, got '{'",
+      "Pos": {
+        "Column": 13,
+        "Line": 3,
+        "Offset": 72,
+      },
+    },
+  ])
+
+})
