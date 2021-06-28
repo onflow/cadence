fa25ed2810728179e60623719becfabf0e5bca2c
Joe Soroka <joe.soroka@dapperlabs.team>
2021-06-28 12:58:39 -0700

    verify json marshaling by roundtrip unmarshaling

--- runtime/ast/unmarshal.go
+++ runtime/ast/unmarshal.go
@@ -2,8 +2,28 @@ package ast
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
 )
 
 func jsonMarshalAndVerify(i interface{}) ([]byte, error) {
-	return json.Marshal(i)
+	b, err := json.Marshal(i)
+	if err != nil { return b, err }
+
+	expected := string(b)
+
+	unmarshalled := reflect.New(reflect.TypeOf(i)).Interface()
+	err = json.Unmarshal(b, &unmarshalled)
+	if err != nil { return b, err }
+
+	b2, err2 := json.Marshal(unmarshalled)
+	if err2 != nil { return b2, err2 }
+
+	actual := string(b2)
+
+	if expected != actual {
+		return nil, fmt.Errorf("un/marshal failed:\n%s\n%s\n", expected, actual)
+	}
+
+	return b, err
 }
