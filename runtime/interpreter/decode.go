342fd281628fbd9f5d0e57211702368a6861ea6f
Bastian Müller <bastian@axiomzen.co>
2021-06-30 12:53:48 -0700

    Revert "remove delegation to previous decoder"
    This reverts commit f913717a80ed137cc8766c018dfb9b81b9f06214.

--- runtime/interpreter/decode.go
+++ runtime/interpreter/decode.go
@@ -1950,6 +1950,9 @@ func decodeDictionaryMetaInfo(v *DictionaryValue, content []byte) error {
 }
 
 func decodeDictionaryEntries(v *DictionaryValue, content []byte) error {
+	if v.encodingVersion == 4 {
+		return decodeDictionaryEntriesV4(v, content)
+	}
 
 	d, err := NewByteDecoder(content, v.Owner, v.encodingVersion, v.decodeCallback)
 	if err != nil {
