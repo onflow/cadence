91d26f2d32e446026d2bc8985a705dce4e2610e0
Bastian Müller <bastian@axiomzen.co>
2021-08-02 09:40:08 -0700

    stored values are not optionals

--- runtime/tests/interpreter/account_test.go
+++ runtime/tests/interpreter/account_test.go
@@ -163,11 +163,7 @@ func TestInterpretAuthAccount_save(t *testing.T) {
 			require.Len(t, storedValues, 1)
 			for _, value := range storedValues {
 
-				require.IsType(t, &interpreter.SomeValue{}, value)
-
-				innerValue := value.(*interpreter.SomeValue).Value
-
-				assert.IsType(t, &interpreter.CompositeValue{}, innerValue)
+				assert.IsType(t, &interpreter.CompositeValue{}, value)
 			}
 
 		})
@@ -214,11 +210,7 @@ func TestInterpretAuthAccount_save(t *testing.T) {
 			require.Len(t, storedValues, 1)
 			for _, value := range storedValues {
 
-				require.IsType(t, &interpreter.SomeValue{}, value)
-
-				innerValue := value.(*interpreter.SomeValue).Value
-
-				assert.IsType(t, &interpreter.CompositeValue{}, innerValue)
+				assert.IsType(t, &interpreter.CompositeValue{}, value)
 			}
 
 		})
