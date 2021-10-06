4079b38d0af290800ddb39d83cc691c0730cba41
Bastian Müller <bastian@axiomzen.co>
2021-10-06 11:03:06 -0700

    restore max depth configuration

--- runtime/tests/utils/utils.go
+++ runtime/tests/utils/utils.go
@@ -43,6 +43,10 @@ const ImportedLocation = common.StringLocation("imported")
 // If the objects are not equal, this function prints a human-readable diff.
 func AssertEqualWithDiff(t *testing.T, expected, actual interface{}) {
 	if !assert.Equal(t, expected, actual) {
+		// the maximum levels of a struct to recurse into
+		// this prevents infinite recursion from circular references
+		deep.MaxDepth = 100
+
 		diff := deep.Equal(expected, actual)
 
 		if len(diff) != 0 {
