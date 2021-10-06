714de752f2eceb3078d7ea30311b4e142af55a8e
Bastian Müller <bastian@axiomzen.co>
2021-10-06 10:54:10 -0700

    add comment explaining byte size of 1 for non-storable storable
    Co-authored-by: Ramtin M. Seraj <ramtinms@users.noreply.github.com>

--- runtime/interpreter/value.go
+++ runtime/interpreter/value.go
@@ -66,7 +66,10 @@ func (s NonStorable) Encode(_ *atree.Encoder) error {
 }
 
 func (s NonStorable) ByteSize() uint32 {
-	return 1
+    // Return 1 so that atree split and merge operations don't have to handle special cases. 
+    // Any value larger than 0 and smaller than half of the max slab size works,
+    // but 1 results in less number of slabs which is ideal for non-storable values.
+    return 1
 }
 
 func (s NonStorable) StoredValue(_ atree.SlabStorage) (atree.Value, error) {
