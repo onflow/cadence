40e61bcddd57bee0a51fae142cc985a99fee8116
Bastian Müller <bastian@axiomzen.co>
2021-11-16 15:14:08 -0800

    lint

--- runtime/storage.go
+++ runtime/storage.go
@@ -27,6 +27,7 @@ import (
 	"strings"
 
 	"github.com/onflow/atree"
+
 	"github.com/onflow/cadence/runtime/common"
 	"github.com/onflow/cadence/runtime/errors"
 	"github.com/onflow/cadence/runtime/interpreter"
