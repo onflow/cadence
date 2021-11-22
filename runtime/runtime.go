c02574f37566df7bb155f739ea4bcc761ce18adc
Daniel Sainati <sainatidaniel@gmail.com>
2021-11-22 10:30:16 -0500

    respond to review

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -3294,7 +3294,7 @@ func verifyBLSPOP(
 
 	// if the crypto layer produces an error, we have invalid input, return false
 	if err != nil {
-		return interpreter.BoolValue(false), nil //nolint
+		return interpreter.BoolValue(false), nil //nolint:nilerr
 	}
 
 	return interpreter.BoolValue(valid), nil
@@ -3328,7 +3328,7 @@ func aggregateBLSPublicKeys(
 
 	// if the crypto layer produces an error, we have invalid input, return nil
 	if err != nil {
-		return interpreter.NilValue{}, nil //nolint
+		return interpreter.NilValue{}, nil //nolint:nilerr
 	}
 
 	return interpreter.NewSomeValueNonCopying(
