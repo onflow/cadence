435d1713e7d23780a7bab15e887f22cda1b4f0c7
Bastian Müller <bastian@axiomzen.co>
2022-01-27 14:03:03 -0800

    fix integer string length estimation and tests

--- runtime/interpreter/big_test.go
+++ runtime/interpreter/big_test.go
@@ -19,63 +19,148 @@
 package interpreter
 
 import (
+	"math"
 	"math/big"
+	"strconv"
 	"testing"
 
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/gen"
+	"github.com/leanovate/gopter/prop"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestOverEstimateIntStringLength(t *testing.T) {
-	assert.LessOrEqual(t, 5, OverEstimateIntStringLength(-1000))
-	assert.LessOrEqual(t, 4, OverEstimateIntStringLength(-999))
-	assert.LessOrEqual(t, 4, OverEstimateIntStringLength(-100))
-	assert.LessOrEqual(t, 3, OverEstimateIntStringLength(-99))
-	assert.LessOrEqual(t, 3, OverEstimateIntStringLength(-10))
-	assert.LessOrEqual(t, 2, OverEstimateIntStringLength(-9))
-	assert.LessOrEqual(t, 2, OverEstimateIntStringLength(-1))
-	assert.LessOrEqual(t, 1, OverEstimateIntStringLength(0))
-	assert.LessOrEqual(t, 1, OverEstimateIntStringLength(1))
-	assert.LessOrEqual(t, 1, OverEstimateIntStringLength(9))
-	assert.LessOrEqual(t, 2, OverEstimateIntStringLength(10))
-	assert.LessOrEqual(t, 2, OverEstimateIntStringLength(99))
-	assert.LessOrEqual(t, 3, OverEstimateIntStringLength(100))
-	assert.LessOrEqual(t, 3, OverEstimateIntStringLength(999))
-	assert.LessOrEqual(t, 4, OverEstimateIntStringLength(1000))
+
+	properties := gopter.NewProperties(nil)
+
+	properties.Property("estimate is greater or equal to actual length", prop.ForAll(
+		func(v int) bool {
+			return OverEstimateIntStringLength(v) >= len(strconv.Itoa(v))
+		},
+		gen.Int(),
+	))
+
+	properties.TestingRun(t)
+
+	for _, v := range []int{
+		math.MinInt,
+		-1000,
+		-999,
+		-100,
+		-99,
+		-10,
+		-9,
+		-1,
+		0,
+		1,
+		9,
+		10,
+		99,
+		100,
+		999,
+		1000,
+		math.MaxInt,
+	} {
+		assert.LessOrEqual(t,
+			len(strconv.Itoa(v)),
+			OverEstimateIntStringLength(v),
+		)
+	}
 }
 
 func TestOverEstimateBigIntStringLength(t *testing.T) {
-	assert.LessOrEqual(t, 5, OverEstimateBigIntStringLength(big.NewInt(-1000)))
-	assert.LessOrEqual(t, 4, OverEstimateBigIntStringLength(big.NewInt(-999)))
-	assert.LessOrEqual(t, 4, OverEstimateBigIntStringLength(big.NewInt(-100)))
-	assert.LessOrEqual(t, 3, OverEstimateBigIntStringLength(big.NewInt(-99)))
-	assert.LessOrEqual(t, 3, OverEstimateBigIntStringLength(big.NewInt(-10)))
-	assert.LessOrEqual(t, 2, OverEstimateBigIntStringLength(big.NewInt(-9)))
-	assert.LessOrEqual(t, 2, OverEstimateBigIntStringLength(big.NewInt(-1)))
-	assert.LessOrEqual(t, 1, OverEstimateBigIntStringLength(big.NewInt(0)))
-	assert.LessOrEqual(t, 1, OverEstimateBigIntStringLength(big.NewInt(1)))
-	assert.LessOrEqual(t, 1, OverEstimateBigIntStringLength(big.NewInt(9)))
-	assert.LessOrEqual(t, 2, OverEstimateBigIntStringLength(big.NewInt(10)))
-	assert.LessOrEqual(t, 2, OverEstimateBigIntStringLength(big.NewInt(99)))
-	assert.LessOrEqual(t, 3, OverEstimateBigIntStringLength(big.NewInt(100)))
-	assert.LessOrEqual(t, 3, OverEstimateBigIntStringLength(big.NewInt(999)))
-	assert.LessOrEqual(t, 4, OverEstimateBigIntStringLength(big.NewInt(1000)))
+
+	properties := gopter.NewProperties(nil)
+
+	properties.Property("estimate is greater or equal to actual length", prop.ForAll(
+		func(v *big.Int) bool {
+			return OverEstimateBigIntStringLength(v) >= len(v.String())
+		},
+		gen.Int64().Map(func(v int64) *big.Int {
+			b := big.NewInt(v)
+			return b.Exp(b, big.NewInt(42), nil)
+		}),
+	))
+
+	properties.TestingRun(t)
+
+	for _, v := range []*big.Int{
+		big.NewInt(0),
+		big.NewInt(1),
+		big.NewInt(9),
+		big.NewInt(10),
+		big.NewInt(99),
+		big.NewInt(100),
+		big.NewInt(999),
+		big.NewInt(1000),
+		big.NewInt(math.MaxUint16),
+		big.NewInt(math.MaxUint32),
+		new(big.Int).SetUint64(math.MaxUint64),
+		func() *big.Int {
+			v := new(big.Int).SetUint64(math.MaxUint64)
+			return v.Mul(v, big.NewInt(2))
+		}(),
+		func() *big.Int {
+			v := new(big.Int).SetUint64(math.MaxUint64)
+			return v.Mul(v, new(big.Int).SetUint64(math.MaxUint64))
+		}(),
+	} {
+		assert.LessOrEqual(t,
+			len(v.String()),
+			OverEstimateBigIntStringLength(v),
+		)
+
+		neg := new(big.Int).Neg(v)
+		assert.LessOrEqual(t,
+			len(neg.String()),
+			OverEstimateBigIntStringLength(neg),
+		)
+	}
 }
 
 func TestOverEstimateFixedPointStringLength(t *testing.T) {
+
+	properties := gopter.NewProperties(nil)
+
+	properties.Property("estimate is greater or equal to actual length", prop.ForAll(
+		func(v NumberValue, scale int) bool {
+			return OverEstimateFixedPointStringLength(v, scale) >= len(v.String())+1+scale
+		},
+		gen.Int64().Map(func(v int64) NumberValue {
+			return NewIntValueFromInt64(v)
+		}),
+		gen.UInt8().Map(func(v uint8) int {
+			return int(v)
+		}),
+	))
+
+	properties.TestingRun(t)
+
 	const testScale = 10
-	assert.LessOrEqual(t, 5+testScale, OverEstimateFixedPointStringLength(Int64Value(-1000), testScale))
-	assert.LessOrEqual(t, 4+testScale, OverEstimateFixedPointStringLength(Int64Value(-999), testScale))
-	assert.LessOrEqual(t, 4+testScale, OverEstimateFixedPointStringLength(Int64Value(-100), testScale))
-	assert.LessOrEqual(t, 3+testScale, OverEstimateFixedPointStringLength(Int64Value(-99), testScale))
-	assert.LessOrEqual(t, 3+testScale, OverEstimateFixedPointStringLength(Int64Value(-10), testScale))
-	assert.LessOrEqual(t, 2+testScale, OverEstimateFixedPointStringLength(Int64Value(-9), testScale))
-	assert.LessOrEqual(t, 2+testScale, OverEstimateFixedPointStringLength(Int64Value(-1), testScale))
-	assert.LessOrEqual(t, 1+testScale, OverEstimateFixedPointStringLength(Int64Value(0), testScale))
-	assert.LessOrEqual(t, 1+testScale, OverEstimateFixedPointStringLength(Int64Value(1), testScale))
-	assert.LessOrEqual(t, 1+testScale, OverEstimateFixedPointStringLength(Int64Value(9), testScale))
-	assert.LessOrEqual(t, 2+testScale, OverEstimateFixedPointStringLength(Int64Value(10), testScale))
-	assert.LessOrEqual(t, 2+testScale, OverEstimateFixedPointStringLength(Int64Value(99), testScale))
-	assert.LessOrEqual(t, 3+testScale, OverEstimateFixedPointStringLength(Int64Value(100), testScale))
-	assert.LessOrEqual(t, 3+testScale, OverEstimateFixedPointStringLength(Int64Value(999), testScale))
-	assert.LessOrEqual(t, 4+testScale, OverEstimateFixedPointStringLength(Int64Value(1000), testScale))
+
+	for _, v := range []int{
+		math.MinInt,
+		-1000,
+		-999,
+		-100,
+		-99,
+		-10,
+		-9,
+		-1,
+		0,
+		1,
+		9,
+		10,
+		99,
+		100,
+		999,
+		1000,
+		math.MaxInt,
+	} {
+		assert.LessOrEqual(t,
+			len(strconv.Itoa(v))+1+testScale,
+			OverEstimateFixedPointStringLength(NewIntValueFromInt64(int64(v)), testScale),
+		)
+	}
 }
