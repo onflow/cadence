9f5932ef84bd121b7d65b6f5dc9f8c547a62c9e2
ramtinms <ramtin@axiomzen.co>
2022-02-01 22:02:00 -0800

    improve structure

--- runtime/interpreter/rlp/rlp.go
+++ runtime/interpreter/rlp/rlp.go
@@ -1,7 +1,8 @@
-package interpreter
+package rlp
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 )
 
@@ -15,16 +16,11 @@ const (
 )
 
 const (
-	RLPByteRangeStart        = 0x00 // not in use, here only for inclusivity
-	RLPByteRangeEnd          = 0x7f
-	RLPShortStringRangeStart = 0x80
-	RLPShortStringRangeEnd   = 0xb7
-	RLPLongStringRangeStart  = 0xb8
-	RLPLongStringRangeEnd    = 0xbf
-	RLPShortListRangeStart   = 0xc0
-	RLPShortListRangeEnd     = 0xf7
-	RLPLongListRangeStart    = 0xf8
-	RLPLongListRangeEnd      = 0xff // not in use, here only for inclusivity
+	// TODO adjust these numbers based on requirements
+	MaxInputByteSize  = 1 << 32
+	MaxStringSize     = 1 << 16
+	MaxListItemCounts = 1 << 16
+	MaxDepthAllowed   = 1 << 16
 )
 
 func (it ItemType) String() string {
@@ -40,19 +36,6 @@ func (it ItemType) String() string {
 	}
 }
 
-func peakNextType(inp []byte, startIndex int) ItemType {
-	// TODO check len
-	firstByte := inp[startIndex]
-	switch {
-	case firstByte < RLPShortStringRangeStart:
-		return Byte
-	case firstByte < RLPShortListRangeStart:
-		return String
-	default:
-		return List
-	}
-}
-
 type Item interface {
 	Type() ItemType
 }
@@ -61,7 +44,7 @@ var _ Item = ByteItem(0)
 var _ Item = StringItem("")
 var _ Item = ListItem{}
 
-type ByteItem uint8
+type ByteItem int
 
 func (ByteItem) Type() ItemType {
 	return Byte
@@ -83,51 +66,95 @@ func (l ListItem) Get(index int) Item {
 	return l[index]
 }
 
+const (
+	ByteRangeStart        = 0x00 // not in use, here only for inclusivity
+	ByteRangeEnd          = 0x7f
+	ShortStringRangeStart = 0x80
+	ShortStringRangeEnd   = 0xb7
+	LongStringRangeStart  = 0xb8
+	LongStringRangeEnd    = 0xbf
+	ShortListRangeStart   = 0xc0
+	ShortListRangeEnd     = 0xf7
+	LongListRangeStart    = 0xf8
+	LongListRangeEnd      = 0xff // not in use, here only for inclusivity
+)
+
+func peakNextType(inp []byte, startIndex int) ItemType {
+	// TODO check len
+	firstByte := inp[startIndex]
+	switch {
+	case firstByte < ShortStringRangeStart:
+		return Byte
+	case firstByte < ShortListRangeStart:
+		return String
+	default:
+		return List
+	}
+}
+
 // TODO add max size limits
 
-// func RLPDecode(input []byte) (offset int, dataLen int, dataType itemType, err error) {
-// 	length = len(input)
-// 	if length == 0 {
-// 		return 0, 0, 0, errors.New("input is null")
-// 	}
-// }
+func Decode(inp []byte) (Item, error) {
 
-// func RLPReadUInt8 ([UInt8Value], rest [UInt8Value], error ) // reading a byte
-// RLPReadString (data string, rest [UInt8], error)
-// RLPReadList   (array of [UInt8], rest [UInt8], error)
+	if len(inp) == 0 {
+		return nil, errors.New("data is empty")
+	}
+	if len(inp) >= MaxInputByteSize {
+		return nil, errors.New("max input size has reached")
+	}
+	var item Item
+	var nextIndex int
+	var err error
+	switch peakNextType(inp, 0) {
+	case Byte:
+		item, nextIndex, err = ReadByteItem(inp, 0)
+	case String:
+		item, nextIndex, err = ReadStringItem(inp, 0)
+	case List:
+		item, nextIndex, err = ReadListItem(inp, 0, 0)
+	}
+	if err != nil {
+		return nil, err
+	}
+	if len(inp) != nextIndex {
+		return nil, errors.New("unused data in the stream")
+	}
+	return item, nil
+}
 
-// TODO change inp []byte to the our ideal inputs
-// make it a reader
-
-func RLPReadByteItem(inp []byte, startIndex int) (data ByteItem, nextStartIndex int, err error) {
+func ReadByteItem(inp []byte, startIndex int) (data ByteItem, nextStartIndex int, err error) {
 	if startIndex >= len(inp) {
 		return 0, startIndex, fmt.Errorf("startIndex error") // TODO make this more formal
 	}
+	if len(inp) >= MaxInputByteSize {
+		return 0, 0, errors.New("max input size has reached")
+	}
 	firstByte := inp[startIndex]
 	startIndex++
-	if firstByte > RLPByteRangeEnd {
+	if firstByte > ByteRangeEnd {
 		return 0, startIndex, fmt.Errorf("type mismatch")
 	}
 	return ByteItem(firstByte), startIndex, nil
 }
 
-func RLPReadStringItem(inp []byte, startIndex int) (str StringItem, nextStartIndex int, err error) {
-
-	var strLen uint
-
+func ReadStringItem(inp []byte, startIndex int) (str StringItem, nextStartIndex int, err error) {
 	if startIndex >= len(inp) {
-		return "", startIndex, fmt.Errorf("startIndex error") // TODO make this more formal
+		return "", 0, fmt.Errorf("startIndex error") // TODO make this more formal
+	}
+	if len(inp) >= MaxInputByteSize {
+		return "", 0, errors.New("max input size has reached")
 	}
 
+	var strLen uint
 	firstByte := inp[startIndex]
 	startIndex++
 
-	if firstByte > RLPLongStringRangeEnd {
-		return "", startIndex, fmt.Errorf("type mismatch")
+	if firstByte > LongStringRangeEnd {
+		return "", 0, fmt.Errorf("type mismatch")
 	}
 
 	// one byte
-	if firstByte < RLPShortStringRangeStart {
+	if firstByte < ShortStringRangeStart {
 		return StringItem(firstByte), startIndex, nil
 	}
 
@@ -135,13 +162,13 @@ func RLPReadStringItem(inp []byte, startIndex int) (str StringItem, nextStartInd
 	// if a string is 0-55 bytes long, the RLP encoding consists
 	// of a single byte with value 0x80 plus the length of the string
 	// followed by the string. The range of the first byte is thus [0x80, 0xB7].
-	if firstByte < RLPLongStringRangeStart {
-		strLen = uint(firstByte - RLPShortStringRangeStart)
+	if firstByte < LongStringRangeStart {
+		strLen = uint(firstByte - ShortStringRangeStart)
 		// TODO check for non zero len
 		endIndex := startIndex + int(strLen)
 		if len(inp) < int(endIndex) {
 			// TODO validate the range
-			return "", startIndex, fmt.Errorf("not enough bytes to read")
+			return "", 0, fmt.Errorf("not enough bytes to read")
 		}
 		return StringItem(inp[startIndex:endIndex]), endIndex, nil
 	}
@@ -154,11 +181,11 @@ func RLPReadStringItem(inp []byte, startIndex int) (str StringItem, nextStartInd
 	// 0xB90400 followed by the string. The range of the first byte is thus
 	// [0xB8, 0xBF].
 
-	bytesToReadForLen := uint(firstByte - RLPShortStringRangeEnd)
+	bytesToReadForLen := uint(firstByte - ShortStringRangeEnd)
 	switch bytesToReadForLen {
 	case 0:
 		// this condition never happens - TODO remove it
-		return "", startIndex, fmt.Errorf("invalid string size")
+		return "", 0, fmt.Errorf("invalid string size")
 
 	case 1:
 		strLen = uint(inp[startIndex])
@@ -176,35 +203,43 @@ func RLPReadStringItem(inp []byte, startIndex int) (str StringItem, nextStartInd
 		strLen = uint(binary.BigEndian.Uint64(lenData))
 	}
 
+	if strLen >= MaxStringSize {
+		return "", 0, fmt.Errorf("max string size has been hit")
+	}
+
 	endIndex := startIndex + int(strLen)
 	if len(inp) < int(endIndex) {
 		// TODO validate the range
-		return "", startIndex, fmt.Errorf("not enough bytes to read")
+		return "", 0, fmt.Errorf("not enough bytes to read")
 	}
 	return StringItem(inp[startIndex:endIndex]), endIndex, nil
 }
 
-func RLPReadListItem(inp []byte, startIndex int) (str ListItem, newStartIndex int, err error) {
-
-	var listDataSize uint
-	retList := make([]Item, 0)
-
+func ReadListItem(inp []byte, startIndex int, depth int) (str ListItem, newStartIndex int, err error) {
 	if len(inp) == 0 {
 		return nil, 0, fmt.Errorf("input is empty")
 	}
+	if len(inp) >= MaxInputByteSize {
+		return nil, 0, errors.New("max input size has reached")
+	}
+	if depth >= MaxDepthAllowed {
+		return nil, 0, errors.New("max depth has been reached")
+	}
+	var listDataSize uint
+	retList := make([]Item, 0)
 
 	firstByte := inp[startIndex]
 	startIndex++
 
-	if firstByte < RLPShortListRangeStart {
+	if firstByte < ShortListRangeStart {
 		return nil, 0, fmt.Errorf("type mismatch")
 	}
 
-	if firstByte < RLPLongListRangeStart { // short list
+	if firstByte < LongListRangeStart { // short list
 		// TODO check max depth, and max byte readable
 		// TODO check for non zero len
 
-		listDataSize = uint(firstByte - RLPShortListRangeStart)
+		listDataSize = uint(firstByte - ShortListRangeStart)
 		listDataStartIndex := startIndex
 		listDataPrevIndex := startIndex
 		bytesRead := 0
@@ -214,11 +249,11 @@ func RLPReadListItem(inp []byte, startIndex int) (str ListItem, newStartIndex in
 			listDataPrevIndex = listDataStartIndex
 			switch itemType {
 			case Byte:
-				item, listDataStartIndex, err = RLPReadByteItem(inp, listDataStartIndex)
+				item, listDataStartIndex, err = ReadByteItem(inp, listDataStartIndex)
 			case String:
-				item, listDataStartIndex, err = RLPReadStringItem(inp, listDataStartIndex)
+				item, listDataStartIndex, err = ReadStringItem(inp, listDataStartIndex)
 			case List:
-				item, listDataStartIndex, err = RLPReadListItem(inp, listDataStartIndex)
+				item, listDataStartIndex, err = ReadListItem(inp, listDataStartIndex, depth+1)
 			}
 			if err != nil {
 				return nil, 0, fmt.Errorf("cannot read list item: %w", err)
@@ -230,7 +265,7 @@ func RLPReadListItem(inp []byte, startIndex int) (str ListItem, newStartIndex in
 		return retList, listDataStartIndex, nil
 	}
 
-	bytesToReadForLen := uint(firstByte - RLPShortListRangeEnd)
+	bytesToReadForLen := uint(firstByte - ShortListRangeEnd)
 	// TODO
 	// if bytesToReadForLen < 56 {
 	// 	// error canonical size ????
@@ -266,11 +301,11 @@ func RLPReadListItem(inp []byte, startIndex int) (str ListItem, newStartIndex in
 		listDataPrevIndex = listDataStartIndex
 		switch itemType {
 		case Byte:
-			item, listDataStartIndex, err = RLPReadByteItem(inp, listDataStartIndex)
+			item, listDataStartIndex, err = ReadByteItem(inp, listDataStartIndex)
 		case String:
-			item, listDataStartIndex, err = RLPReadStringItem(inp, listDataStartIndex)
+			item, listDataStartIndex, err = ReadStringItem(inp, listDataStartIndex)
 		case List:
-			item, listDataStartIndex, err = RLPReadListItem(inp, listDataStartIndex)
+			item, listDataStartIndex, err = ReadListItem(inp, listDataStartIndex, depth+1)
 		}
 		if err != nil {
 			return nil, 0, fmt.Errorf("cannot read list item: %w", err)
