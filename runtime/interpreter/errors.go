79aa90d848e7464e6c92a63e00d603bc5e245d9d
robert <robert.davidson@forecastfoundation.org>
2022-02-02 15:54:08 -0800

    gofmt

--- runtime/interpreter/errors.go
+++ runtime/interpreter/errors.go
@@ -603,7 +603,6 @@ func (e InvalidPublicKeyError) Error() string {
 	return fmt.Sprintf("invalid public key: %s", e.PublicKey)
 }
 
-
 func (e InvalidPublicKeyError) Unwrap() error {
 	return e.Err
 }
