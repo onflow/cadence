91fd8d9857e2adde007ca2a526b78d3c5ed03815
ramtinms <ramtin@axiomzen.co>
2022-02-10 16:31:03 -0800

    add one more test

--- runtime/stdlib/rlp/rlp_test.go
+++ runtime/stdlib/rlp/rlp_test.go
@@ -323,6 +323,17 @@ func TestDecodeList(t *testing.T) {
 			rlp.ErrListSizeMismatch,
 		},
 		{
+			nil,
+			[]byte{
+				0xcf,                                     // 1 byte size
+				0x87,                                     // size of string
+				0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, // content
+				0x87,                               // size of string
+				0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, // content
+			},
+			rlp.ErrIncompleteInput,
+		},
+		{
 			[][]byte{
 				{0x82, 0x41, 0x42},
 				{0x82, 0x43, 0x44},
