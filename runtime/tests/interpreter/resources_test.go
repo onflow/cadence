35ebfdb379e85a893993b61c5728cd43e5bf7e9d
Bastian Müller <bastian@axiomzen.co>
2022-02-28 10:20:31 -0800

    lint

--- runtime/tests/interpreter/resources_test.go
+++ runtime/tests/interpreter/resources_test.go
@@ -22,8 +22,9 @@ import (
 	"testing"
 
 	"github.com/onflow/atree"
-	"github.com/onflow/cadence/runtime/sema"
 	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/cadence/runtime/sema"
 
 	"github.com/onflow/cadence/runtime/tests/checker"
 	. "github.com/onflow/cadence/runtime/tests/utils"
