a729bcacf2dbf64e86c8fc65992ec554bca5845f
Supun Setunga <supun.setunga@gmail.com>
2022-03-03 08:59:25 -0800

    Fix asserts and add license header

--- runtime/memory_metering_test.go
+++ runtime/memory_metering_test.go
@@ -1,3 +1,21 @@
+/*
+ * Cadence - The resource-oriented smart contract programming language
+ *
+ * Copyright 2022 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package runtime
 
 import (
@@ -53,7 +71,7 @@ func TestRuntimeArrayMetering(t *testing.T) {
 
 	require.NoError(t, err)
 
-	assert.Equal(t, meter[common.MemoryKindArray], uint64(6))
+	assert.Equal(t, uint64(6), meter[common.MemoryKindArray])
 }
 
 func TestRuntimeDictionaryMetering(t *testing.T) {
@@ -89,8 +107,8 @@ func TestRuntimeDictionaryMetering(t *testing.T) {
 
 	require.NoError(t, err)
 
-	assert.Equal(t, meter[common.MemoryKindString], uint64(4))
-	assert.Equal(t, meter[common.MemoryKindDictionary], uint64(3))
+	assert.Equal(t, uint64(4), meter[common.MemoryKindString])
+	assert.Equal(t, uint64(3), meter[common.MemoryKindDictionary])
 }
 
 func TestRuntimeCompositeMetering(t *testing.T) {
@@ -141,6 +159,6 @@ func TestRuntimeCompositeMetering(t *testing.T) {
 
 	require.NoError(t, err)
 
-	assert.Equal(t, meter[common.MemoryKindString], uint64(39))
-	assert.Equal(t, meter[common.MemoryKindComposite], uint64(2))
+	assert.Equal(t, uint64(39), meter[common.MemoryKindString])
+	assert.Equal(t, uint64(2), meter[common.MemoryKindComposite])
 }
