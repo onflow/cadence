ad1f54bf32efbf39ce012d29fca8204eeb4d87d5
Robert E. Davidson III <45945043+robert-e-davidson3@users.noreply.github.com>
2022-03-07 09:56:47 -0800

    Update runtime/convertValues.go
    Co-authored-by: Supun Setunga <supun.setunga@gmail.com>

--- runtime/convertValues.go
+++ runtime/convertValues.go
@@ -569,7 +569,11 @@ func importString(inter *interpreter.Interpreter, v cadence.String) *interpreter
 }
 
 func importPathValue(inter *interpreter.Interpreter, v cadence.Path) interpreter.PathValue {
-	return interpreter.NewPathValue(inter, common.PathDomainFromIdentifier(v.Domain), v.Identifier)
+	return interpreter.NewPathValue(
+		inter, 
+		common.PathDomainFromIdentifier(v.Domain),
+		v.Identifier,
+	)
 }
 
 func importTypeValue(
