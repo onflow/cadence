93be3510fb962d1733796fae07738af4f0fddb79
Daniel Sainati <sainatidaniel@gmail.com>
2022-03-08 14:40:32 -0500

    meter calls to transfer for arrays, dictionaries and composites

--- runtime/interpreter/value.go
+++ runtime/interpreter/value.go
@@ -2162,13 +2162,8 @@ func (v *ArrayValue) Transfer(
 	}
 
 	if res == nil {
-		res = &ArrayValue{
-			Type:             v.Type,
-			semaType:         v.semaType,
-			isResourceKinded: v.isResourceKinded,
-			array:            array,
-			isDestroyed:      v.isDestroyed,
-		}
+		res = newArrayValueFromAtreeValue(interpreter, array, v.Type)
+		res.semaType = v.semaType
 	}
 
 	return res
@@ -12663,22 +12658,22 @@ func (v *CompositeValue) Transfer(
 	}
 
 	if res == nil {
-		res = &CompositeValue{
-			dictionary:          dictionary,
-			Location:            v.Location,
-			QualifiedIdentifier: v.QualifiedIdentifier,
-			Kind:                v.Kind,
-			InjectedFields:      v.InjectedFields,
-			ComputedFields:      v.ComputedFields,
-			NestedVariables:     v.NestedVariables,
-			Functions:           v.Functions,
-			Destructor:          v.Destructor,
-			Stringer:            v.Stringer,
-			isDestroyed:         v.isDestroyed,
-			typeID:              v.typeID,
-			staticType:          v.staticType,
-			dynamicType:         v.dynamicType,
+		info := compositeTypeInfo{
+			location:            v.Location,
+			qualifiedIdentifier: v.QualifiedIdentifier,
+			kind:                v.Kind,
 		}
+		res = newCompositeValueFromOrderedMap(interpreter, dictionary, info)
+		res.InjectedFields = v.InjectedFields
+		res.ComputedFields = v.ComputedFields
+		res.NestedVariables = v.NestedVariables
+		res.Functions = v.Functions
+		res.Destructor = v.Destructor
+		res.Stringer = v.Stringer
+		res.isDestroyed = v.isDestroyed
+		res.typeID = v.typeID
+		res.staticType = v.staticType
+		res.dynamicType = v.dynamicType
 	}
 
 	if needsStoreTo &&
@@ -13773,13 +13768,10 @@ func (v *DictionaryValue) Transfer(
 	}
 
 	if res == nil {
-		res = &DictionaryValue{
-			Type:             v.Type,
-			semaType:         v.semaType,
-			isResourceKinded: v.isResourceKinded,
-			dictionary:       dictionary,
-			isDestroyed:      v.isDestroyed,
-		}
+		res = newDictionaryValueFromOrderedMap(interpreter, dictionary, v.Type)
+		res.semaType = v.semaType
+		res.isResourceKinded = v.isResourceKinded
+		res.isDestroyed = v.isDestroyed
 	}
 
 	return res
