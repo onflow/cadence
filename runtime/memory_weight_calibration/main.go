02cb703b9873556624f6b5f4ac24068fb895aaa0
Daniel Sainati <sainatidaniel@gmail.com>
2022-03-10 14:20:49 -0500

    framework to run programs and print memory stats

---
+++ runtime/memory_weight_calibration/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/onflow/cadence/runtime/common"
+	"github.com/onflow/cadence/runtime/interpreter"
+	"github.com/onflow/cadence/runtime/parser2"
+	"github.com/onflow/cadence/runtime/sema"
+	"github.com/onflow/cadence/runtime/tests/utils"
+)
+
+type calibrationMemoryGauge struct {
+	meter map[common.MemoryKind]uint64
+}
+
+func newTestMemoryGauge() *calibrationMemoryGauge {
+	return &calibrationMemoryGauge{
+		meter: make(map[common.MemoryKind]uint64),
+	}
+}
+
+func (g *calibrationMemoryGauge) UseMemory(usage common.MemoryUsage) {
+	g.meter[usage.Kind] += usage.Amount
+}
+
+func main() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	totalAlloc := m.TotalAlloc
+	for _, code := range test_programs {
+		fmt.Println(code.name)
+		memoryGauge := newTestMemoryGauge()
+		program, err := parser2.ParseProgram(code.code, memoryGauge)
+		if err != nil {
+			panic(err)
+		}
+		checker, err := sema.NewChecker(
+			program,
+			utils.TestLocation,
+			sema.WithAccessCheckMode(sema.AccessCheckModeNotSpecifiedUnrestricted),
+		)
+		if err != nil {
+			panic(err)
+		}
+		err = checker.Check()
+		if err != nil {
+			panic(err)
+		}
+		inter, err := interpreter.NewInterpreter(
+			interpreter.ProgramFromChecker(checker),
+			checker.Location,
+			interpreter.WithStorage(interpreter.NewInMemoryStorage(nil)),
+			interpreter.WithAtreeValueValidationEnabled(true),
+			interpreter.WithAtreeStorageValidationEnabled(true),
+			interpreter.WithMemoryGauge(memoryGauge),
+		)
+		if err != nil {
+			panic(err)
+		}
+		err = inter.Interpret()
+		if err != nil {
+			panic(err)
+		}
+		_, err = inter.Invoke("main")
+		if err != nil {
+			panic(err)
+		}
+
+		for kind, amount := range memoryGauge.meter {
+			fmt.Printf("%s: %d\n", kind.String(), amount)
+		}
+		runtime.ReadMemStats(&m)
+		fmt.Printf("Actual Memory: %d\n", m.TotalAlloc-totalAlloc)
+		fmt.Println("--------------------")
+		totalAlloc = m.TotalAlloc
+	}
+}
---
+++ runtime/memory_weight_calibration/programs.go
@@ -0,0 +1,40 @@
+package main
+
+var bool_test = `
+pub fun main() {
+	var i = 0
+	while i < 10000 {
+		let b = true
+		i = i + 1
+	}
+}
+`
+
+var void_test = `
+pub fun main() {
+	var i = 0
+	while i < 10000 {
+		let v = nil
+		i = i + 1
+	}
+}
+`
+
+var array_test = `
+pub fun main() {
+	var i = 0
+	while i < 10000 {
+		let a: [Int8] = []
+		i = i + 1
+	}
+}
+`
+
+var test_programs = []struct {
+	name string
+	code string
+}{
+	{name: "bool", code: bool_test},
+	{name: "void", code: void_test},
+	{name: "array", code: array_test},
+}
