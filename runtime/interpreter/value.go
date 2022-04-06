e560bd61b2398286460ff8d3843fac28a077eb0e
robert <robert.davidson@forecastfoundation.org>
2022-04-05 17:08:38 -0700

    Meter memory for Primitive Static Type

--- runtime/interpreter/value.go
+++ runtime/interpreter/value.go
@@ -472,6 +472,7 @@ func (VoidValue) DynamicType(_ *Interpreter, _ SeenReferences) DynamicType {
 }
 
 func (VoidValue) StaticType(interpreter *Interpreter) StaticType {
+	common.UseConstantMemory(interpreter, common.MemoryKindPrimitiveStaticType)
 	return NewPrimitiveStaticType(interpreter, PrimitiveStaticTypeVoid)
 }
 
