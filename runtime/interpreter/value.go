ed619f1118710684b2cffc939b2e8395da9d19ab
robert <robert.davidson@forecastfoundation.org>
2022-04-05 21:01:13 -0700

    tests

--- runtime/interpreter/value.go
+++ runtime/interpreter/value.go
@@ -463,7 +463,6 @@ func (VoidValue) Walk(_ *Interpreter, _ func(Value)) {
 }
 
 func (VoidValue) StaticType(interpreter *Interpreter) StaticType {
-	common.UseConstantMemory(interpreter, common.MemoryKindPrimitiveStaticType)
 	return NewPrimitiveStaticType(interpreter, PrimitiveStaticTypeVoid)
 }
 
