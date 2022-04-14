c154df463e94c430d6e30ec623ae1e4687aff040
Bastian Müller <bastian@axiomzen.co>
2022-04-14 15:01:51 -0700

    add start of a Cadence analysis package inspired by Go tools 'packages' and 'analysis' packages

---
+++ runtime/sema/resolve.go
@@ -0,0 +1,101 @@
+/*
+ * Cadence - The resource-oriented smart contract programming language
+ *
+ * Copyright 2019-2022 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sema
+
+import (
+	"github.com/onflow/cadence/runtime/ast"
+	"github.com/onflow/cadence/runtime/common"
+)
+
+type AddressContractNamesResolver func(address common.Address) ([]string, error)
+
+// AddressLocationHandlerFunc returns a location handler
+// which returns a single location for non-address locations,
+// and uses the given address contract names resolve function
+// to get all contract names for an address
+//
+func AddressLocationHandlerFunc(resolveAddressContractNames AddressContractNamesResolver) LocationHandlerFunc {
+	return func(identifiers []ast.Identifier, location common.Location) ([]ResolvedLocation, error) {
+		addressLocation, isAddress := location.(common.AddressLocation)
+
+		// if the location is not an address location, e.g. an identifier location (`import Crypto`),
+		// then return a single resolved location which declares all identifiers.
+
+		if !isAddress {
+			return []ResolvedLocation{
+				{
+					Location:    location,
+					Identifiers: identifiers,
+				},
+			}, nil
+		}
+
+		// if the location is an address,
+		// and no specific identifiers where requested in the import statement,
+		// then fetch all identifiers at this address
+
+		if len(identifiers) == 0 {
+			// if there is no contract name resolver,
+			// then return no resolved locations
+
+			if resolveAddressContractNames == nil {
+				return nil, nil
+			}
+
+			contractNames, err := resolveAddressContractNames(addressLocation.Address)
+			if err != nil {
+				panic(err)
+			}
+
+			// if there are no contracts deployed,
+			// then return no resolved locations
+
+			if len(contractNames) == 0 {
+				return nil, nil
+			}
+
+			identifiers = make([]ast.Identifier, len(contractNames))
+
+			for i := range identifiers {
+				identifiers[i] = ast.Identifier{
+					Identifier: contractNames[i],
+				}
+			}
+		}
+
+		// return one resolved location per identifier.
+		// each resolved location is an address contract location
+
+		resolvedLocations := make([]ResolvedLocation, len(identifiers))
+		for i := range resolvedLocations {
+			identifier := identifiers[i]
+			resolvedLocations[i] = ResolvedLocation{
+				Location: common.AddressLocation{
+					Address: addressLocation.Address,
+					Name:    identifier.Identifier,
+				},
+				Identifiers: []ast.Identifier{
+					identifier,
+				},
+			}
+		}
+
+		return resolvedLocations, nil
+	}
+}
---
+++ tools/analysis/analysis.go
@@ -0,0 +1,179 @@
+/*
+ * Cadence - The resource-oriented smart contract programming language
+ *
+ * Copyright 2019-2022 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package analysis
+
+import (
+	"github.com/onflow/cadence/runtime/ast"
+	"github.com/onflow/cadence/runtime/common"
+	"github.com/onflow/cadence/runtime/parser2"
+	"github.com/onflow/cadence/runtime/sema"
+	"github.com/onflow/cadence/runtime/stdlib"
+)
+
+var valueDeclarations = append(
+	stdlib.FlowBuiltInFunctions(stdlib.FlowBuiltinImpls{}),
+	stdlib.BuiltinFunctions...,
+).ToSemaValueDeclarations()
+
+var typeDeclarations = append(
+	stdlib.FlowBuiltInTypes,
+	stdlib.BuiltinTypes...,
+).ToTypeDeclarations()
+
+// LoadMode controls the amount of detail to return when loading.
+// The bits below can be combined to specify what information is required.
+//
+type LoadMode int
+
+const (
+	// NeedSyntax provides the AST.
+	NeedSyntax LoadMode = 0
+
+	// NeedTypes provides the elaboration.
+	NeedTypes LoadMode = 1 << iota
+)
+
+type Programs map[common.LocationID]*Program
+
+type Program struct {
+	Location    common.Location
+	Program     *ast.Program
+	Elaboration *sema.Elaboration
+}
+
+// A Config specifies details about how programs should be loaded.
+// The zero value is a valid configuration.
+// Calls to Load do not modify this struct.
+type Config struct {
+	// Mode controls the level of information returned for each program.
+	Mode LoadMode
+
+	// ResolveAddressContractNames is called to resolve the contract names of an address location.
+	ResolveAddressContractNames func(address common.Address) ([]string, error)
+
+	// ResolveImport is called to resolve an import to its source code.
+	ResolveImport func(
+		importingLocation common.Location,
+		importedLocation common.Location,
+		importRange ast.Range,
+	) (string, error)
+}
+
+func Load(config *Config, locations ...common.Location) (Programs, error) {
+	programs := make(Programs, len(locations))
+	for _, location := range locations {
+		err := programs.load(config, location, nil, ast.Range{})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return programs, nil
+}
+
+func (programs Programs) load(
+	config *Config,
+	location common.Location,
+	importingLocation common.Location,
+	importRange ast.Range,
+) error {
+
+	if programs[location.ID()] != nil {
+		return nil
+	}
+
+	code, err := config.ResolveImport(importingLocation, location, importRange)
+	if err != nil {
+		return err
+	}
+	program, err := parser2.ParseProgram(code)
+	if err != nil {
+		return err
+	}
+
+	var elaboration *sema.Elaboration
+	if config.Mode&NeedTypes != 0 {
+		elaboration, err = programs.check(config, program, location)
+		if err != nil {
+			return err
+		}
+	}
+
+	programs[location.ID()] = &Program{
+		Location:    location,
+		Program:     program,
+		Elaboration: elaboration,
+	}
+
+	return nil
+}
+
+func (programs Programs) check(
+	config *Config,
+	program *ast.Program,
+	location common.Location,
+) (
+	*sema.Elaboration,
+	error,
+) {
+	checker, err := sema.NewChecker(
+		program,
+		location,
+		sema.WithPredeclaredValues(valueDeclarations),
+		sema.WithPredeclaredTypes(typeDeclarations),
+		sema.WithLocationHandler(
+			sema.AddressLocationHandlerFunc(
+				config.ResolveAddressContractNames,
+			),
+		),
+		sema.WithPositionInfoEnabled(true),
+		sema.WithImportHandler(
+			func(checker *sema.Checker, importedLocation common.Location, importRange ast.Range) (sema.Import, error) {
+
+				var elaboration *sema.Elaboration
+				switch importedLocation {
+				case stdlib.CryptoChecker.Location:
+					elaboration = stdlib.CryptoChecker.Elaboration
+
+				default:
+					err := programs.load(config, importedLocation, location, importRange)
+					if err != nil {
+						return nil, err
+					}
+
+					elaboration = programs[importedLocation.ID()].Elaboration
+				}
+
+				return sema.ElaborationImport{
+					Elaboration: elaboration,
+				}, nil
+			},
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	err = checker.Check()
+	if err != nil {
+		return nil, err
+	}
+
+	return checker.Elaboration, nil
+}
