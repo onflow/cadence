d3503c43a25573f75e79371fa02c0afc9a667de6
Supun Setunga <supun.setunga@gmail.com>
2022-05-02 12:20:12 -0700

    Update generated code

--- runtime/interpreter/variable_activations.go
+++ runtime/interpreter/variable_activations.go
@@ -125,7 +125,7 @@ type VariableActivations struct {
 }
 
 func NewVariableActivations(memoryGauge common.MemoryGauge) *VariableActivations {
-	// No need to meter since this is created only once per interpreter.
+	// No need to meter.
 	return &VariableActivations{
 		memoryGauge: memoryGauge,
 	}
