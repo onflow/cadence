ce402a51a22b3b6f80063f72f558edd7286a899b
Robert E. Davidson III <45945043+robert-e-davidson3@users.noreply.github.com>
2022-05-20 09:36:33 -0700

    Wrap memory error in FatalError and check for it when parsing (#1660)
    * wrap UseMemory errors with FatalError
    
    * do not eat fatal errs
    
    * testing
    
    * verify which fatal error
    
    
    Co-authored-by: robert <robert.davidson@forecastfoundation.org>

--- runtime/common/metering.go
+++ runtime/common/metering.go
@@ -20,6 +20,7 @@ package common
 
 import (
 	"errors"
+	"fmt"
 	"math"
 	"math/big"
 	"unsafe"
@@ -137,7 +138,7 @@ func UseMemory(gauge MemoryGauge, usage MemoryUsage) {
 
 	err := gauge.MeterMemory(usage)
 	if err != nil {
-		panic(err)
+		panic(FatalError{Err: err})
 	}
 }
 
@@ -737,4 +738,18 @@ func UseConstantMemory(memoryGauge MemoryGauge, kind MemoryKind) {
 		Kind:   kind,
 		Amount: 1,
 	})
+}
+
+// FatalError indicates an error that should end
+// the Cadence parsing, checking, or interpretation.
+type FatalError struct {
+	Err error
+}
+
+func (e FatalError) Unwrap() error {
+	return e.Err
+}
+
+func (e FatalError) Error() string {
+	return fmt.Sprintf("Fatal error: %s", e.Err.Error())
 }
