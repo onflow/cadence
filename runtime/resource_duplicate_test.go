cb1e57a6a6b821ef5dd751d5c0671595bd454253
Supun Setunga <supun.setunga@gmail.com>
2022-05-26 13:35:06 -0700

    Add resource duplicating reproducer

---
+++ runtime/resource_duplicate_test.go
@@ -0,0 +1,211 @@
+package runtime
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/onflow/cadence"
+	"github.com/onflow/cadence/encoding/json"
+	"github.com/onflow/cadence/runtime/common"
+	"github.com/onflow/cadence/runtime/tests/utils"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestResourceDuplicate(t *testing.T) {
+
+	runtime := newTestInterpreterRuntime()
+
+	accountCodes := map[common.LocationID][]byte{}
+
+	var events []cadence.Event
+
+	signerAccount := common.MustBytesToAddress([]byte{0x1})
+
+	storage := newTestLedger(nil, nil)
+
+	runtimeInterface := &testRuntimeInterface{
+		getCode: func(location Location) (bytes []byte, err error) {
+			return accountCodes[location.ID()], nil
+		},
+		storage: storage,
+		getSigningAccounts: func() ([]Address, error) {
+			return []Address{signerAccount}, nil
+		},
+		resolveLocation: singleIdentifierLocationResolver(t),
+		getAccountContractCode: func(address Address, name string) (code []byte, err error) {
+			location := common.AddressLocation{
+				Address: address,
+				Name:    name,
+			}
+			return accountCodes[location.ID()], nil
+		},
+		updateAccountContractCode: func(address Address, name string, code []byte) error {
+			location := common.AddressLocation{
+				Address: address,
+				Name:    name,
+			}
+			accountCodes[location.ID()] = code
+			return nil
+		},
+		emitEvent: func(event cadence.Event) error {
+			events = append(events, event)
+			return nil
+		},
+	}
+	runtimeInterface.decodeArgument = func(b []byte, t cadence.Type) (value cadence.Value, err error) {
+		return json.Decode(runtimeInterface, b)
+	}
+
+	nextTransactionLocation := newTransactionLocationGenerator()
+
+	// ---------------- Deploy Fungible Token contract ----------------
+
+	err := runtime.ExecuteTransaction(
+		Script{
+			Source: utils.DeploymentTransaction(
+				"FungibleToken",
+				[]byte(realFungibleTokenContractInterface),
+			),
+		},
+		Context{
+			Interface: runtimeInterface,
+			Location:  nextTransactionLocation(),
+		},
+	)
+	require.NoError(t, err)
+
+	// ---------------- Deploy Flow Token contract ----------------
+
+	err = runtime.ExecuteTransaction(
+		Script{
+			Source: []byte(fmt.Sprintf(
+				`
+                  transaction {
+
+                      prepare(signer: AuthAccount) {
+                          signer.contracts.add(name: "FlowToken", code: "%s".decodeHex(), signer)
+                      }
+                  }
+                `,
+				hex.EncodeToString([]byte(realFlowContract)),
+			)),
+		},
+		Context{
+			Interface: runtimeInterface,
+			Location:  nextTransactionLocation(),
+		},
+	)
+	require.NoError(t, err)
+
+	// ---------------- Deploy Hello interface ----------------
+
+	//signerAccount = common.MustBytesToAddress([]byte{0x2})
+
+	helloInterfaceCode := `
+		import FlowToken from 0x1
+		// import FlowToken from 0x0ae53cb6e3f42a79
+
+		access(all) contract interface HolderInterface {
+			pub (set) var content:  @FlowToken.Vault?
+		}
+`
+	err = runtime.ExecuteTransaction(
+		Script{
+			Source: utils.DeploymentTransaction(
+				"HolderInterface",
+				[]byte(helloInterfaceCode),
+			),
+		},
+		Context{
+			Interface: runtimeInterface,
+			Location:  nextTransactionLocation(),
+		},
+	)
+	require.NoError(t, err)
+
+	// --------------- Deploy Hello contract ----------------
+
+	helloCode := `
+		import FlowToken from 0x1
+		import HolderInterface from 0x1
+
+		// import FlowToken from 0x0ae53cb6e3f42a79
+		// import HolderInterface from 0x01cf0e2f2f715450
+
+		access(all) contract Holder: HolderInterface {
+			pub (set) var content:  @FlowToken.Vault?
+			init(){
+				self.content<-nil
+			}
+		}
+	`
+	err = runtime.ExecuteTransaction(
+		Script{
+			Source: utils.DeploymentTransaction(
+				"Holder",
+				[]byte(helloCode),
+			),
+		},
+		Context{
+			Interface: runtimeInterface,
+			Location:  nextTransactionLocation(),
+		},
+	)
+	require.NoError(t, err)
+
+	// --------------------------------
+
+	code := `
+		import FlowToken from 0x1
+		import HolderInterface from 0x1
+		import Holder from 0x1
+
+		// import Holder from 0x01cf0e2f2f715450
+		// import HolderInterface from 0x01cf0e2f2f715450
+		// import FlowToken from 0x0ae53cb6e3f42a79
+		
+		transaction {
+		
+		  prepare(acct: AuthAccount) {
+		
+			  //get current vault
+			  var vault <- acct.load<@FlowToken.Vault>(from:/storage/flowTokenVault)
+		
+			  //put it to contract
+			  Holder.content <-! vault
+		
+			  //save to storage
+			  acct.save(Holder as AnyStruct, to:/storage/dnz)
+		
+			  //remove vault
+			  var exvault <- Holder.content <- nil
+			  var unwrappedExVault <- exvault!
+		
+			  //abracadabra
+			  var dupe = acct.load<AnyStruct>(from:/storage/dnz)!
+			  var dupeContract = dupe as! HolderInterface
+			  var dupeVault <- dupeContract.content <- nil
+		
+		
+			  unwrappedExVault.deposit(from: <- dupeVault!)
+		
+			  //put out vault back
+			  acct.save(<-unwrappedExVault, to:/storage/flowTokenVault)
+		  }
+		
+		  execute {
+		  }
+		}
+		`
+
+	err = runtime.ExecuteTransaction(
+		Script{
+			Source: []byte(code),
+		},
+		Context{
+			Interface: runtimeInterface,
+			Location:  nextTransactionLocation(),
+		},
+	)
+	require.NoError(t, err)
+}
