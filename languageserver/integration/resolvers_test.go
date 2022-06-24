0a74d3a17e366fe18638d1e49dd59d85715bf979
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-06-24 12:27:20 +0200

    add resolver test

--- languageserver/integration/resolvers_test.go
+++ languageserver/integration/resolvers_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 
 	"github.com/spf13/afero"
-
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_FileImport(t *testing.T) {
+	t.Parallel()
 	mockFS := afero.NewMemMapFs()
 	af := afero.Afero{Fs: mockFS}
 	_ = afero.WriteFile(mockFS, "./test.cdc", []byte(`hello test`), 0644)
@@ -27,5 +27,15 @@ func Test_FileImport(t *testing.T) {
 		resolved, err := resolver.fileImport("./foo.cdc")
 		assert.EqualError(t, err, "open foo.cdc: file does not exist")
 		assert.Equal(t, "", resolved)
+	})
+}
+
+func Test_AddressImport(t *testing.T) {
+	resolver := resolvers{
+		client: nil,
+	}
+
+	t.Run("existing address", func(t *testing.T) {
+
 	})
 }
