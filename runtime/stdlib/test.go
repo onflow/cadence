9bbf0052ac4e095b98c8bb7708ce6829bde03c32
Bastian Müller <bastian@axiomzen.co>
2022-11-09 13:14:44 -0800

    fix test matcher

--- runtime/stdlib/test.go
+++ runtime/stdlib/test.go
@@ -54,7 +54,7 @@ const transactionArgsFieldName = "arguments"
 
 const accountAddressFieldName = "address"
 
-const matcherTestFunctionName = "test"
+const matcherTestFieldName = "test"
 
 const addressesFieldName = "addresses"
 
@@ -185,6 +185,17 @@ var matcherType = func() *sema.CompositeType {
 
 	return compositeType
 }()
+
+var matcherTestFieldType = compositeFunctionType(matcherType, matcherTestFieldName)
+
+func compositeFunctionType(parent *sema.CompositeType, funcName string) *sema.FunctionType {
+	testFunc, ok := parent.Members.Get(funcName)
+	if !ok {
+		panic(memberNotFoundError(parent.Identifier, funcName))
+	}
+
+	return getFunctionTypeFromMember(testFunc, funcName)
+}
 
 func interfaceFunctionType(parent *sema.InterfaceType, funcName string) *sema.FunctionType {
 	testFunc, ok := parent.Members.Get(funcName)
@@ -465,14 +476,14 @@ func invokeMatcherTest(
 	testFunc := matcher.GetMember(
 		inter,
 		locationRange,
-		matcherTestFunctionName,
+		matcherTestFieldName,
 	)
 
 	funcValue, ok := testFunc.(interpreter.FunctionValue)
 	if !ok {
 		panic(errors.NewUnexpectedError(
-			"invalid type for '%s'. expected function",
-			matcherTestFunctionName,
+			"invalid value type for field '%s'. expected function value",
+			matcherTestFieldName,
 		))
 	}
 
@@ -1472,12 +1483,12 @@ func newMatcherWithGenericTestFunction(
 
 	inter := invocation.Interpreter
 
-	staticType, ok := testFunc.StaticType(inter).(interpreter.FunctionStaticType)
-	if !ok {
+	typeParameterPair := invocation.TypeParameterTypes.Oldest()
+	if typeParameterPair == nil {
 		panic(errors.NewUnreachableError())
 	}
 
-	parameters := staticType.Type.Parameters
+	parameterType := typeParameterPair.Value
 
 	// Wrap the user provided test function with a function that validates the argument types.
 	// i.e: create a closure that cast the arguments.
@@ -1495,15 +1506,14 @@ func newMatcherWithGenericTestFunction(
 		func(invocation interpreter.Invocation) interpreter.Value {
 			inter := invocation.Interpreter
 
-			for i, argument := range invocation.Arguments {
-				paramType := parameters[i].TypeAnnotation.Type
+			for _, argument := range invocation.Arguments {
 				argumentStaticType := argument.StaticType(inter)
 
-				if !inter.IsSubTypeOfSemaType(argumentStaticType, paramType) {
+				if !inter.IsSubTypeOfSemaType(argumentStaticType, parameterType) {
 					argumentSemaType := inter.MustConvertStaticToSemaType(argumentStaticType)
 
 					panic(interpreter.TypeMismatchError{
-						ExpectedType:  paramType,
+						ExpectedType:  parameterType,
 						ActualType:    argumentSemaType,
 						LocationRange: invocation.LocationRange,
 					})
@@ -1517,7 +1527,7 @@ func newMatcherWithGenericTestFunction(
 
 			return value
 		},
-		matcherTestFunctionType,
+		matcherTestFieldType,
 	)
 
 	matcherConstructor := getNestedTypeConstructorValue(
