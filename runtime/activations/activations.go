ac763fb7370164fb4a62cdb2a5c64266a06d2252
Daniel Sainati <sainatidaniel@gmail.com>
2022-11-17 11:49:12 -0500

    fix issue with base value shadowing in constructor arguments

--- runtime/activations/activations.go
+++ runtime/activations/activations.go
@@ -107,15 +107,6 @@ func (a *Activation[T]) Set(name string, value T) {
 	a.entries[name] = value
 }
 
-// Remove removes the given name from the activation.
-func (a *Activation[T]) Remove(name string) {
-	if a.entries == nil {
-		return
-	}
-
-	delete(a.entries, name)
-}
-
 // Activations is a stack of activation records.
 // Each entry represents a new activation record.
 //
@@ -166,16 +157,6 @@ func (a *Activations[T]) Set(name string, value T) {
 	}
 
 	current.Set(name, value)
-}
-
-// Remove removes the given name from the current scope.
-func (a *Activations[T]) Remove(name string) {
-	current := a.Current()
-	if current == nil {
-		return
-	}
-
-	current.Remove(name)
 }
 
 // PushNewWithParent pushes a new empty activation
