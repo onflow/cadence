757e255e178028f46c97b0b0f0688a812cd79803
Naomi Liu <naomi.liu@dapperlabs.com>
2023-02-07 16:48:45 -0800

    add TargetPath field and stubs

--- runtime/format/capability.go
+++ runtime/format/capability.go
@@ -36,14 +36,15 @@ func StorageCapability(borrowType string, address string, path string) string {
 	)
 }
 
-func CapabilityController(borrowType string) string {
+func CapabilityController(borrowType string, targetPath string) string {
 	var typeArgument string
 	if borrowType != "" {
 		typeArgument = fmt.Sprintf("<%s>", borrowType)
 	}
 
 	return fmt.Sprintf(
-		"CapabilityController(borrowType: %s)",
+		"CapabilityController(borrowType: %s, target: %s)",
 		typeArgument,
+		targetPath,
 	)
 }
