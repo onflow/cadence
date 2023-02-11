52ff5716f30bae5b18f0f677c28d4535f210c948
Naomi Liu <naomi.liu@dapperlabs.com>
2023-02-10 16:24:37 -0800

    add AuthAccount and PublicAccountCapabilities types

---
+++ runtime/sema/auth_account_capabilities.go
@@ -0,0 +1,83 @@
+/*
+ * Cadence - The resource-oriented smart contract programming language
+ *
+ * Copyright 2019-2022 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sema
+
+import "github.com/onflow/cadence/runtime/common"
+
+var AuthAccountCapabilitiesTypeName = "Capabilities"
+
+var AuthAccountCapabilitiesType = fix(func(authAccountCapabilitiesType *CompositeType) CompositeType {
+	members := []*Member{
+		NewUnmeteredPublicFunctionMember(
+			authAccountCapabilitiesType,
+			AccountCapabilitiesTypeGetFunctionName,
+			AccountCapabilitiesTypeGetFunctionType,
+			AccountCapabilitiesTypeGetFunctionDocString,
+		),
+		NewUnmeteredPublicFunctionMember(
+			authAccountCapabilitiesType,
+			AccountCapabilitiesTypeBorrowFunctionName,
+			AccountCapabilitiesTypeBorrowFunctionType,
+			AccountCapabilitiesTypeBorrowFunctionDocString,
+		),
+		NewUnmeteredPublicFunctionMember(
+			authAccountCapabilitiesType,
+			AccountCapabilitiesTypeForEachFunctionName,
+			AccountCapabilitiesTypeForEachFunctionType,
+			AccountCapabilitiesTypeForEachFunctionDocString,
+		),
+		NewUnmeteredPublicFunctionMember(
+			authAccountCapabilitiesType,
+			AuthAccountCapabilitiesTypeGetControllerFunctionName,
+			AuthAccountCapabilitiesTypeGetControllerFunctionType,
+			AuthAccountCapabilitiesTypeGetControllerFunctionDocString,
+		),
+		NewUnmeteredPublicFunctionMember(
+			authAccountCapabilitiesType,
+			AuthAccountCapabilitiesTypeGetControllersFunctionName,
+			AuthAccountCapabilitiesTypeGetControllersFunctionType,
+			AuthAccountCapabilitiesTypeGetControllersFunctionDocString,
+		),
+		NewUnmeteredPublicFunctionMember(
+			authAccountCapabilitiesType,
+			AuthAccountCapabilitiesTypeForEachControllerFunctionName,
+			AuthAccountCapabilitiesTypeForEachControllerFunctionType,
+			AuthAccountCapabilitiesTypeForEachControllerFunctionDocString,
+		),
+		NewUnmeteredPublicFunctionMember(
+			authAccountCapabilitiesType,
+			AuthAccountCapabilitiesTypeIssueFunctionName,
+			AuthAccountCapabilitiesTypeIssueFunctionType,
+			AuthAccountCapabilitiesTypeIssueFunctionDocString,
+		),
+	}
+
+	return CompositeType{
+		Identifier: AuthAccountCapabilitiesTypeName,
+		Kind:       common.CompositeKindStructure,
+		importable: false,
+		Members:    GetMembersAsMap(members),
+		Fields:     GetFieldNames(members),
+	}
+})
+
+func init() {
+	// Set the container type after initializing the `AuthAccountContractsType`, to avoid initializing loop.
+	AuthAccountCapabilitiesType.SetContainerType(AuthAccountType)
+}
