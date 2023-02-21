a133c16cd02e7a3492c618d8096bfc78c087581b
Bastian Müller <bastian@axiomzen.co>
2023-02-21 10:43:29 -0800

    convert storage and account reference values

--- runtime/interpreter/interpreter.go
+++ runtime/interpreter/interpreter.go
@@ -1744,10 +1744,21 @@ func (interpreter *Interpreter) convert(value Value, valueType, targetType sema.
 			)
 
 		case *StorageReferenceValue:
-			// TODO
+			return NewStorageReferenceValue(
+				interpreter,
+				ref.Authorized,
+				ref.TargetStorageAddress,
+				ref.TargetPath,
+				unwrappedTargetType.Type,
+			)
 
 		case *AccountReferenceValue:
-			// TODO:
+			return NewAccountReferenceValue(
+				interpreter,
+				ref.Address,
+				ref.Path,
+				unwrappedTargetType.Type,
+			)
 
 		default:
 			panic(errors.NewUnexpectedError("unsupported reference value: %T", ref))
