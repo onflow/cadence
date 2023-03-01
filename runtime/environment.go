48a7d16e4786ab632ae2664b3539ad61f0260ff2
Janez Podhostnik <janez.podhostnik@gmail.com>
2023-03-01 19:42:29 +0100

    Rename GetAndSetProgram to GetOrLoadProgram

--- runtime/environment.go
+++ runtime/environment.go
@@ -547,7 +547,7 @@ func (e *interpreterEnvironment) getProgram(
 	}
 
 	errors.WrapPanic(func() {
-		program, err = e.runtimeInterface.GetAndSetProgram(location, func() (program *interpreter.Program, err error) {
+		program, err = e.runtimeInterface.GetOrLoadProgram(location, func() (program *interpreter.Program, err error) {
 			// Loading is done by Cadence.
 			// If it panics with a user error, e.g. when parsing fails due to a memory metering error,
 			// then do not treat it as an external error (the load callback is called by the embedder)
