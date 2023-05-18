eaca0f3ac62e561f6d18435e57ac9a69fc098802
Bastian Müller <bastian@axiomzen.co>
2023-05-18 12:28:18 -0700

    lint

--- runtime/tests/checker/entitlements_test.go
+++ runtime/tests/checker/entitlements_test.go
@@ -21,9 +21,10 @@ package checker
 import (
 	"testing"
 
-	"github.com/onflow/cadence/runtime/sema"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/cadence/runtime/sema"
 )
 
 func TestCheckBasicEntitlementDeclaration(t *testing.T) {
