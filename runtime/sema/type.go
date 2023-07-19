f774a65e6e2e1544cbd4f655f41d688492effb7c
darkdrag00nv2 <122124396+darkdrag00nv2@users.noreply.github.com>
2023-07-19 19:49:04 +0530

    Ensure ArrayReverseFunctionType only accepts sema.ArrayType
    Co-authored-by: Supun Setunga <supun.setunga@gmail.com>

--- runtime/sema/type.go
+++ runtime/sema/type.go
@@ -2225,7 +2225,7 @@ func ArraySliceFunctionType(elementType Type) *FunctionType {
 	}
 }
 
-func ArrayReverseFunctionType(arrayType Type) *FunctionType {
+func ArrayReverseFunctionType(arrayType sema.ArrayType) *FunctionType {
 	return &FunctionType{
 		Parameters:           []Parameter{},
 		ReturnTypeAnnotation: NewTypeAnnotation(arrayType),
