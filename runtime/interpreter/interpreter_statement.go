c9089c8feac3f30486d75c3052e9973c3581b511
Daniel Sainati <sainatidaniel@gmail.com>
2023-10-05 10:04:00 -0400

    set base during attachment iteration

--- runtime/interpreter/interpreter_statement.go
+++ runtime/interpreter/interpreter_statement.go
@@ -473,7 +473,7 @@ func (interpreter *Interpreter) VisitRemoveStatement(removeStatement *ast.Remove
 
 	if attachment.IsResourceKinded(interpreter) {
 		// this attachment is no longer attached to its base, but the `base` variable is still available in the destructor
-		attachment.setBaseValue(interpreter, base, attachmentBaseAuthorization(interpreter, attachment))
+		attachment.setBaseValue(interpreter, base)
 		attachment.Destroy(interpreter, locationRange)
 	}
 
