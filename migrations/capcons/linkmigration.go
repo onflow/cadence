2996575b5e2a0e6157234501b84f73d1bcb090e3
Bastian Müller <bastian@turbolent.com>
2023-12-15 11:19:53 -0800

    Update migrations/capcons/linkmigration.go

--- migrations/capcons/linkmigration.go
+++ migrations/capcons/linkmigration.go
@@ -108,7 +108,6 @@ func (m *LinkMigration) Migrate(
 		inter,
 		accountAddress,
 		pathValue,
-		// TODO:
 		// Use top-most type to follow link all the way to final target
 		&sema.ReferenceType{
 			Authorization: sema.UnauthorizedAccess,
