de138faf9d8c54f6cbbfd9b464b0487caddd88ce
Daniel Sainati <sainatidaniel@gmail.com>
2024-01-05 13:15:08 -0500

    add test for typeValue keys

--- migrations/entitlements/migration_test.go
+++ migrations/entitlements/migration_test.go
@@ -1866,3 +1866,244 @@ func TestMigrateDictOfValues(t *testing.T) {
 		ref.Authorization,
 	)
 }
+
+func TestMigrateDictOfWithTypeValueKey(t *testing.T) {
+	t.Parallel()
+
+	address1 := [8]byte{0, 0, 0, 0, 0, 0, 0, 1}
+	address2 := [8]byte{0, 0, 0, 0, 0, 0, 0, 2}
+
+	storage := NewTestLedger(nil, nil)
+	rt := NewTestInterpreterRuntime()
+
+	accountCodes := map[common.Location][]byte{}
+	interfaces := map[common.Location]*TestRuntimeInterface{}
+
+	runtimeInterface1 := &TestRuntimeInterface{
+		Storage: storage,
+		OnEmitEvent: func(event cadence.Event) error {
+			return nil
+		},
+		OnGetSigningAccounts: func() ([]runtime.Address, error) {
+			return []runtime.Address{address1}, nil
+		},
+		OnGetCode: func(location common.Location) (bytes []byte, err error) {
+			return accountCodes[location], nil
+		},
+		OnResolveLocation: MultipleIdentifierLocationResolver,
+		OnGetAccountContractCode: func(location common.AddressLocation) (code []byte, err error) {
+			code = accountCodes[location]
+			return code, nil
+		},
+	}
+	runtimeInterface1.OnUpdateAccountContractCode = func(location common.AddressLocation, code []byte) error {
+		accountCodes[location] = code
+		interfaces[location] = runtimeInterface1
+		return nil
+	}
+
+	runtimeInterface2 := &TestRuntimeInterface{
+		Storage: storage,
+		OnEmitEvent: func(event cadence.Event) error {
+			return nil
+		},
+		OnGetCode: func(location common.Location) (bytes []byte, err error) {
+			return accountCodes[location], nil
+		},
+		OnGetSigningAccounts: func() ([]runtime.Address, error) {
+			return []runtime.Address{address2}, nil
+		},
+		OnResolveLocation: MultipleIdentifierLocationResolver,
+		OnGetAccountContractCode: func(location common.AddressLocation) (code []byte, err error) {
+			code = accountCodes[location]
+			return code, nil
+		},
+	}
+	runtimeInterface2.OnUpdateAccountContractCode = func(location common.AddressLocation, code []byte) error {
+		accountCodes[location] = code
+		interfaces[location] = runtimeInterface2
+		return nil
+	}
+
+	nextTransactionLocation := NewTransactionLocationGenerator()
+
+	oldContract := []byte(`
+		access(all) contract C {
+			access(all) resource R {
+				access(all) fun foo() {}
+			}
+			access(all) fun makeR(): @R {
+				return <- create R()
+			}
+		}
+	`)
+
+	contract := []byte(`
+		access(all) contract C {
+			access(all) entitlement E
+			access(all) resource R {
+				access(E) fun foo() {}
+			}
+			access(all) fun makeR(): @R {
+				return <- create R()
+			}
+		}
+	`)
+
+	saveValues := []byte(`
+		import C from 0x1	
+
+		transaction {
+			prepare(signer: auth(Storage, Capabilities) &Account) {
+				let r1 <- C.makeR()
+				let r2 <- C.makeR()
+				let rType = ReferenceType(entitlements: [], type: r1.getType())!
+				signer.storage.save(<-r1, to: /storage/foo)
+				signer.storage.save(<-r2, to: /storage/bar)
+				let cap1 = signer.capabilities.storage.issue<&C.R>(/storage/foo)
+				let cap2 = signer.capabilities.storage.issue<&C.R>(/storage/bar)
+				let arr = {rType: cap1, Type<Int>(): cap2}
+				signer.storage.save(arr, to: /storage/caps)
+			}
+		}
+	`)
+
+	// Deploy contract to 0x1
+	err := rt.ExecuteTransaction(
+		runtime.Script{
+			Source: DeploymentTransaction("C", oldContract),
+		},
+		runtime.Context{
+			Interface: runtimeInterface1,
+			Location:  nextTransactionLocation(),
+		},
+	)
+	require.NoError(t, err)
+
+	// Execute transaction on 0x2
+	err = rt.ExecuteTransaction(
+		runtime.Script{
+			Source: saveValues,
+		},
+		runtime.Context{
+			Interface: runtimeInterface2,
+			Location:  nextTransactionLocation(),
+		},
+	)
+	require.NoError(t, err)
+
+	// update contract on 0x1
+	err = rt.ExecuteTransaction(
+		runtime.Script{
+			Source: UpdateTransaction("C", contract),
+		},
+		runtime.Context{
+			Interface: runtimeInterface1,
+			Location:  nextTransactionLocation(),
+		},
+	)
+	require.NoError(t, err)
+
+	runtimeStorage := runtime.NewStorage(storage, nil)
+
+	inter, err := interpreter.NewInterpreter(
+		nil,
+		utils.TestLocation,
+		&interpreter.Config{
+			Storage:                       runtimeStorage,
+			AtreeValueValidationEnabled:   false,
+			AtreeStorageValidationEnabled: false,
+			ImportLocationHandler: func(inter *interpreter.Interpreter, location common.Location) interpreter.Import {
+				program, err := rt.ParseAndCheckProgram(
+					accountCodes[location],
+					runtime.Context{
+						Interface: interfaces[location],
+						Location:  location,
+					},
+				)
+				require.NoError(t, err)
+
+				subInterpreter, err := inter.NewSubInterpreter(program, location)
+				require.NoError(t, err)
+
+				return interpreter.InterpreterImport{
+					Interpreter: subInterpreter,
+				}
+			},
+		},
+	)
+	require.NoError(t, err)
+
+	storageIdentifier := common.PathDomainStorage.Identifier()
+	storageMap := runtimeStorage.GetStorageMap(address2, storageIdentifier, false)
+	require.NotNil(t, storageMap)
+	require.Greater(t, storageMap.Count(), uint64(0))
+
+	// Migrate
+
+	migration := migrations.NewStorageMigration(inter, runtimeStorage)
+	pathMigrator := migration.NewValueMigrationsPathMigrator(nil, NewEntitlementsMigration(inter))
+	migration.Migrate(
+		&migrations.AddressSliceIterator{
+			Addresses: []common.Address{
+				address1,
+				address2,
+			},
+		},
+		pathMigrator,
+	)
+
+	dictValue := storageMap.ReadValue(nil, interpreter.StringStorageMapKey("caps"))
+	require.IsType(t, &interpreter.DictionaryValue{}, dictValue)
+	dictionaryValue := dictValue.(*interpreter.DictionaryValue)
+
+	valueType := dictionaryValue.Type.ValueType
+	require.IsType(t, &interpreter.CapabilityStaticType{}, valueType)
+	capElementType := valueType.(*interpreter.CapabilityStaticType)
+	require.IsType(t, &interpreter.ReferenceStaticType{}, capElementType.BorrowType)
+	ref := capElementType.BorrowType.(*interpreter.ReferenceStaticType)
+	require.Equal(t,
+		interpreter.NewEntitlementSetAuthorization(
+			inter,
+			func() []common.TypeID { return []common.TypeID{"A.0000000000000001.C.E"} },
+			1,
+			sema.Conjunction,
+		),
+		ref.Authorization,
+	)
+
+	rTypeKey := interpreter.NewTypeValue(nil, ref)
+	intTypeKey := interpreter.NewTypeValue(nil, interpreter.PrimitiveStaticTypeInt)
+
+	cap1, present := dictionaryValue.Get(inter, interpreter.EmptyLocationRange, rTypeKey)
+	require.True(t, present)
+	require.IsType(t, &interpreter.CapabilityValue{}, cap1)
+	capValue := cap1.(*interpreter.CapabilityValue)
+	require.IsType(t, &interpreter.ReferenceStaticType{}, capValue.BorrowType)
+	ref = capValue.BorrowType.(*interpreter.ReferenceStaticType)
+	require.Equal(t,
+		interpreter.NewEntitlementSetAuthorization(
+			inter,
+			func() []common.TypeID { return []common.TypeID{"A.0000000000000001.C.E"} },
+			1,
+			sema.Conjunction,
+		),
+		ref.Authorization,
+	)
+
+	cap2, present := dictionaryValue.Get(inter, interpreter.EmptyLocationRange, intTypeKey)
+	require.True(t, present)
+	require.IsType(t, &interpreter.CapabilityValue{}, cap2)
+	capValue = cap1.(*interpreter.CapabilityValue)
+	require.IsType(t, &interpreter.ReferenceStaticType{}, capValue.BorrowType)
+	ref = capValue.BorrowType.(*interpreter.ReferenceStaticType)
+	require.Equal(t,
+		interpreter.NewEntitlementSetAuthorization(
+			inter,
+			func() []common.TypeID { return []common.TypeID{"A.0000000000000001.C.E"} },
+			1,
+			sema.Conjunction,
+		),
+		ref.Authorization,
+	)
+}
