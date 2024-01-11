6de2576927b737dc0f1130589a5b3249a3cbb7c8
Bastian Müller <bastian@turbolent.com>
2024-01-11 14:13:41 -0800

    allow issuing capability controllers using type values

--- runtime/interpreter/errors.go
+++ runtime/interpreter/errors.go
@@ -1051,3 +1051,22 @@ func (e InclusiveRangeConstructionError) Error() string {
 	}
 	return fmt.Sprintf("%s: %s", message, e.Message)
 }
+
+// InvalidCapabilityIssueTypeError
+type InvalidCapabilityIssueTypeError struct {
+	ExpectedTypeDescription string
+	ActualType              sema.Type
+	LocationRange
+}
+
+var _ errors.UserError = InvalidCapabilityIssueTypeError{}
+
+func (InvalidCapabilityIssueTypeError) IsUserError() {}
+
+func (e InvalidCapabilityIssueTypeError) Error() string {
+	return fmt.Sprintf(
+		"invalid type: expected %s, got `%s`",
+		e.ExpectedTypeDescription,
+		e.ActualType.QualifiedString(),
+	)
+}
