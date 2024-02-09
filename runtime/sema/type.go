5ad55256bfa7f02f3a335fa729a5946ff7bcc831
Bastian Müller <bastian@turbolent.com>
2024-02-08 18:55:10 -0800

    unify the requirement for all interface types to have to occur in intersection types
    only requiring resource interface types and struct interface types
    but not contract interface types to have to occur in intersection types
    is inconsistent and confusing

--- runtime/sema/type.go
+++ runtime/sema/type.go
@@ -5818,16 +5818,9 @@ func (*InterfaceType) TypeAnnotationState() TypeAnnotationState {
 }
 
 func (t *InterfaceType) RewriteWithIntersectionTypes() (Type, bool) {
-	switch t.CompositeKind {
-	case common.CompositeKindResource, common.CompositeKindStructure:
-		return &IntersectionType{
-			Types: []*InterfaceType{t},
-		}, true
-
-	default:
-		return t, false
-	}
-
+	return &IntersectionType{
+		Types: []*InterfaceType{t},
+	}, true
 }
 
 func (*InterfaceType) Unify(
