d737d3dbcc46ad2b16f7f35aa9ebf894e84660c8
Bastian Müller <bastian@turbolent.com>
2024-07-12 14:03:07 -0700

    add tests

--- runtime/webassembly_test.go
+++ runtime/webassembly_test.go
@@ -28,7 +28,6 @@ import (
 	"github.com/onflow/cadence/encoding/json"
 	. "github.com/onflow/cadence/runtime"
 	"github.com/onflow/cadence/runtime/common"
-
 	. "github.com/onflow/cadence/runtime/tests/runtime_utils"
 	. "github.com/onflow/cadence/runtime/tests/utils"
 )
@@ -145,4 +144,133 @@ func TestRuntimeWebAssemblyDisabled(t *testing.T) {
 		},
 	)
 	RequireError(t, err)
+}
+
+func TestRuntimeWebAssemblyLoop(t *testing.T) {
+
+	t.Parallel()
+
+	runtime := NewTestInterpreterRuntimeWithConfig(Config{
+		WebAssemblyEnabled: true,
+	})
+
+	// A simple program which exports a function that loops forever:
+	//
+	//  (module
+	//    (func (loop $loop (br 0)))
+	//    (export "loop" (func 0)))
+	//
+	addProgram := []byte{
+		0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x04, 0x01, 0x60,
+		0x00, 0x00, 0x03, 0x02, 0x01, 0x00, 0x07, 0x08, 0x01, 0x04, 0x6c, 0x6f,
+		0x6f, 0x70, 0x00, 0x00, 0x0a, 0x09, 0x01, 0x07, 0x00, 0x03, 0x40, 0x0c,
+		0x00, 0x0b, 0x0b,
+	}
+
+	// language=cadence
+	script := []byte(`
+      access(all)
+      fun main(program: [UInt8]) {
+          let instance = WebAssembly.compileAndInstantiate(bytes: program).instance
+          let loop = instance.getExport<fun(): Void>(name: "loop")
+          loop()
+      }
+    `)
+
+	var webAssemblyFuelComputationMeterings []uint
+
+	runtimeInterface := &TestRuntimeInterface{
+		Storage:              NewTestLedger(nil, nil),
+		OnCompileWebAssembly: NewWasmtimeWebAssemblyModule,
+		OnDecodeArgument: func(b []byte, _ cadence.Type) (cadence.Value, error) {
+			return json.Decode(nil, b)
+		},
+		OnMeterComputation: func(compKind common.ComputationKind, intensity uint) error {
+			if compKind != common.ComputationKindWebAssemblyFuel {
+				return nil
+			}
+
+			webAssemblyFuelComputationMeterings = append(
+				webAssemblyFuelComputationMeterings,
+				intensity,
+			)
+
+			return nil
+		},
+	}
+
+	_, err := runtime.ExecuteScript(
+		Script{
+			Source: script,
+			Arguments: encodeArgs(
+				newBytesValue(addProgram),
+			),
+		},
+		Context{
+			Interface: runtimeInterface,
+			Location:  common.ScriptLocation{},
+		},
+	)
+
+	require.Error(t, err)
+	// TODO: check error type
+
+	assert.Equal(t,
+		// TODO: adjust, currently todoAvailableFuel
+		[]uint{1000},
+		webAssemblyFuelComputationMeterings,
+	)
+}
+
+func TestRuntimeWebAssemblyInfiniteLoopAtStart(t *testing.T) {
+
+	t.Parallel()
+
+	runtime := NewTestInterpreterRuntimeWithConfig(Config{
+		WebAssemblyEnabled: true,
+	})
+
+	// A simple program which loops forever, on start/load:
+	//
+	//  (module
+	//    (func (loop $loop (br 0)))
+	//    (start 0))
+	//
+	addProgram := []byte{
+		0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x04, 0x01, 0x60,
+		0x00, 0x00, 0x03, 0x02, 0x01, 0x00, 0x08, 0x01, 0x00, 0x0a, 0x09, 0x01,
+		0x07, 0x00, 0x03, 0x40, 0x0c, 0x00, 0x0b, 0x0b,
+	}
+
+	// language=cadence
+	script := []byte(`
+      access(all)
+      fun main(program: [UInt8]) {
+          let instance = WebAssembly.compileAndInstantiate(bytes: program).instance
+      }
+    `)
+
+	runtimeInterface := &TestRuntimeInterface{
+		Storage:              NewTestLedger(nil, nil),
+		OnCompileWebAssembly: NewWasmtimeWebAssemblyModule,
+		OnDecodeArgument: func(b []byte, _ cadence.Type) (cadence.Value, error) {
+			return json.Decode(nil, b)
+		},
+	}
+
+	_, err := runtime.ExecuteScript(
+		Script{
+			Source: script,
+			Arguments: encodeArgs(
+				newBytesValue(addProgram),
+			),
+		},
+		Context{
+			Interface: runtimeInterface,
+			Location:  common.ScriptLocation{},
+		},
+	)
+
+	RequireError(t, err)
+	// TODO: check error type
 }
