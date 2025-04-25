15a5cf1aeda3b2f3ad34353549c3acf0ae4f89b5
Bastian Müller <bastian@turbolent.com>
2025-04-25 13:40:33 -0700

    import and compile program, and construct VM while preprocessing

--- runtime/contract_function_executor.go
+++ runtime/contract_function_executor.go
@@ -23,7 +23,6 @@ import (
 
 	"github.com/onflow/cadence"
 	"github.com/onflow/cadence/ast"
-	"github.com/onflow/cadence/bbq/commons"
 	"github.com/onflow/cadence/common"
 	"github.com/onflow/cadence/errors"
 	"github.com/onflow/cadence/interpreter"
