bb98923c63315d050756b29072d80f7b7f4ef9bc
Supun Setunga <supun.setunga@gmail.com>
2025-05-07 08:08:29 -0700

    Lint

--- interpreter/interface_test.go
+++ interpreter/interface_test.go
@@ -19,13 +19,13 @@
 package interpreter_test
 
 import (
-	"github.com/onflow/cadence/ast"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/cadence/activations"
+	"github.com/onflow/cadence/ast"
 	"github.com/onflow/cadence/common"
 	"github.com/onflow/cadence/interpreter"
 	"github.com/onflow/cadence/sema"
