dd26ae569385a2d12c0150fe1f02a2a056638b67
Bastian Müller <bastian@turbolent.com>
2025-05-15 16:23:06 -0700

    improve invocation of external functions and transactions

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -426,7 +426,37 @@ func InvokeExternally(
 	result Value,
 	err error,
 ) {
+	preparedArguments, err := PrepareExternalInvocationArguments(context, functionType, arguments)
+	if err != nil {
+		return nil, err
+	}
 
+	var self *Value
+	var base *EphemeralReferenceValue
+	if boundFunc, ok := functionValue.(BoundFunctionValue); ok {
+		self = boundFunc.SelfReference.ReferencedValue(
+			context,
+			EmptyLocationRange,
+			true,
+		)
+		base = boundFunc.Base
+	}
+
+	// NOTE: can't fill argument types, as they are unknown
+	invocation := NewInvocation(
+		context,
+		self,
+		base,
+		preparedArguments,
+		nil,
+		nil,
+		EmptyLocationRange,
+	)
+
+	return functionValue.Invoke(invocation), nil
+}
+
+func PrepareExternalInvocationArguments(context InvocationContext, functionType *sema.FunctionType, arguments []Value) ([]Value, error) {
 	// ensures the invocation's argument count matches the function's parameter count
 
 	parameters := functionType.Parameters
@@ -451,8 +481,6 @@ func InvokeExternally(
 		}
 	}
 
-	locationRange := EmptyLocationRange
-
 	var preparedArguments []Value
 	if argumentCount > 0 {
 		preparedArguments = make([]Value, argumentCount)
@@ -460,33 +488,11 @@ func InvokeExternally(
 			parameterType := parameters[i].TypeAnnotation.Type
 
 			// converts the argument into the parameter type declared by the function
-			preparedArguments[i] = ConvertAndBox(context, locationRange, argument, nil, parameterType)
+			preparedArguments[i] = ConvertAndBox(context, EmptyLocationRange, argument, nil, parameterType)
 		}
 	}
 
-	var self *Value
-	var base *EphemeralReferenceValue
-	if boundFunc, ok := functionValue.(BoundFunctionValue); ok {
-		self = boundFunc.SelfReference.ReferencedValue(
-			context,
-			EmptyLocationRange,
-			true,
-		)
-		base = boundFunc.Base
-	}
-
-	// NOTE: can't fill argument types, as they are unknown
-	invocation := NewInvocation(
-		context,
-		self,
-		base,
-		preparedArguments,
-		nil,
-		nil,
-		locationRange,
-	)
-
-	return functionValue.Invoke(invocation), nil
+	return preparedArguments, nil
 }
 
 // Invoke invokes a global function with the given arguments
