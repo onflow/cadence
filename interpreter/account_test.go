c02e4d978a01b7dfa06f8f608dd44412e14c016a
Bastian Müller <bastian@turbolent.com>
2025-05-15 10:27:13 -0700

    fix merge: implement new computation metering API, fix reference type constructor invocation

--- interpreter/account_test.go
+++ interpreter/account_test.go
@@ -33,6 +33,7 @@ import (
 	"github.com/onflow/cadence/interpreter"
 	"github.com/onflow/cadence/sema"
 	"github.com/onflow/cadence/stdlib"
+	"github.com/onflow/cadence/test_utils"
 	. "github.com/onflow/cadence/test_utils/common_utils"
 	. "github.com/onflow/cadence/test_utils/interpreter_utils"
 )
