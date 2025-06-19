07aa8733027ab3fe95f63073f1cdf9180957c7e3
Bastian Müller <bastian@turbolent.com>
2025-06-19 09:00:37 -0700

    implement RLP functions for VM

--- stdlib/builtin.go
+++ stdlib/builtin.go
@@ -72,8 +72,8 @@ func VMDefaultStandardLibraryValues(handler StandardLibraryHandler) []StandardLi
 		NewVMAccountConstructor(handler),
 		// TODO: PublicKeyConstructor
 		// TODO: HashAlgorithmConstructor
-		// TODO: RLPContract,
-		// TODO: BLSContract
+		RLPContract,
+		// TODO: NewBLSContract(nil, handler),
 	}
 }
 
@@ -110,6 +110,9 @@ func VMFunctions(handler StandardLibraryHandler) []VMFunction {
 		NewVMAccountAccountCapabilitiesForEachControllerFunction(handler),
 		NewVMAccountAccountCapabilitiesIssueFunction(handler),
 		NewVMAccountAccountCapabilitiesIssueWithTypeFunction(handler),
+
+		vmRLPDecodeStringFunction,
+		vmRLPDecodeListFunction,
 	}
 }
 
