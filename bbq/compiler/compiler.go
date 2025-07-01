02a671c8fc60521f6f44d50c4f368f87c3ce8e2f
Bastian Müller <bastian@turbolent.com>
2025-07-01 15:53:59 -0700

    handle self imports: compiled locations may import additional globals from the same location

--- bbq/compiler/compiler.go
+++ bbq/compiler/compiler.go
@@ -3173,7 +3173,12 @@ func (c *Compiler[_, _]) addGlobalsFromImportedProgram(location common.Location)
 
 	// Recursively add transitive imports.
 	for _, impt := range importedProgram.Imports {
-		c.addGlobalsFromImportedProgram(impt.Location)
+		// Handle self imports for injected globals
+		if impt.Location == location {
+			c.addImportedGlobal(impt.Location, impt.Name)
+		} else {
+			c.addGlobalsFromImportedProgram(impt.Location)
+		}
 	}
 }
 
