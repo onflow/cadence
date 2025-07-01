78a43351f45fca3ffc1895dc955fb2e098324079
Bastian Müller <bastian@turbolent.com>
2025-07-01 15:54:11 -0700

    test compilation of injected contract

--- bbq/compiler/compiler_test.go
+++ bbq/compiler/compiler_test.go
@@ -37,6 +37,7 @@ import (
 	"github.com/onflow/cadence/interpreter"
 	"github.com/onflow/cadence/sema"
 	"github.com/onflow/cadence/stdlib"
+	. "github.com/onflow/cadence/test_utils/common_utils"
 	. "github.com/onflow/cadence/test_utils/sema_utils"
 )
 
@@ -8909,5 +8910,168 @@ func TestCompileImportEnumCase(t *testing.T) {
 			},
 		},
 		bProgram.Imports,
+	)
+}
+
+func TestCompileInjectedContract(t *testing.T) {
+
+	t.Parallel()
+
+	cType := &sema.FunctionType{
+		Parameters: []sema.Parameter{
+			{
+				Label:          sema.ArgumentLabelNotRequired,
+				Identifier:     "n",
+				TypeAnnotation: sema.IntTypeAnnotation,
+			},
+		},
+		ReturnTypeAnnotation: sema.NewTypeAnnotation(sema.IntType),
+	}
+
+	bType := &sema.CompositeType{
+		Identifier: "B",
+		Location:   TestLocation,
+		Kind:       common.CompositeKindContract,
+	}
+
+	bType.Members = sema.MembersAsMap([]*sema.Member{
+		sema.NewUnmeteredPublicFunctionMember(
+			bType,
+			"c",
+			cType,
+			"",
+		),
+		sema.NewUnmeteredPublicConstantFieldMember(
+			bType,
+			"d",
+			sema.IntType,
+			"",
+		),
+	})
+
+	bStaticType := interpreter.ConvertSemaCompositeTypeToStaticCompositeType(nil, bType)
+
+	bValue := interpreter.NewSimpleCompositeValue(
+		nil,
+		bType.ID(),
+		bStaticType,
+		[]string{"d"},
+		map[string]interpreter.Value{
+			"d": interpreter.NewUnmeteredIntValueFromInt64(1),
+		},
+		nil,
+		nil,
+		nil,
+		nil,
+	)
+
+	baseValueActivation := sema.NewVariableActivation(sema.BaseValueActivation)
+	baseValueActivation.DeclareValue(stdlib.StandardLibraryValue{
+		Name:  bType.Identifier,
+		Type:  bType,
+		Value: bValue,
+		Kind:  common.DeclarationKindContract,
+	})
+
+	checker, err := ParseAndCheckWithOptions(t,
+		`
+          contract A {
+              fun test(): Int {
+                  return B.c(B.d)
+              }
+          }
+        `,
+		ParseAndCheckOptions{
+			Location: TestLocation,
+			Config: &sema.Config{
+				BaseValueActivationHandler: func(location common.Location) *sema.VariableActivation {
+					assert.Equal(t, TestLocation, location)
+					return baseValueActivation
+				},
+			},
+		},
+	)
+	require.NoError(t, err)
+
+	config := &compiler.Config{
+		BuiltinGlobalsProvider: func(location common.Location) *activations.Activation[compiler.GlobalImport] {
+			assert.Equal(t, TestLocation, location)
+			activation := activations.NewActivation(nil, compiler.DefaultBuiltinGlobals())
+			activation.Set(
+				"B",
+				compiler.GlobalImport{
+					Name:     "B",
+					Location: TestLocation,
+				},
+			)
+			activation.Set(
+				"B.c",
+				compiler.GlobalImport{
+					Name:     "B.c",
+					Location: TestLocation,
+				},
+			)
+			return activation
+		},
+	}
+
+	comp := compiler.NewInstructionCompilerWithConfig(
+		interpreter.ProgramFromChecker(checker),
+		checker.Location,
+		config,
+	)
+	program := comp.Compile()
+
+	functions := program.Functions
+	require.Len(t, functions, 4)
+
+	aTestFunction := functions[3]
+
+	require.Equal(t, aTestFunction.Name, "A.test")
+
+	assert.Equal(t,
+		[]opcode.Instruction{
+			// return B.c(B.d)
+			opcode.InstructionStatement{},
+			// B.c(...)
+			opcode.InstructionGetGlobal{Global: 5},
+			opcode.InstructionGetMethod{Method: 6},
+			// B.d
+			opcode.InstructionGetGlobal{Global: 5},
+			opcode.InstructionGetField{
+				FieldName:    0,
+				AccessedType: 5,
+			},
+			opcode.InstructionTransferAndConvert{Type: 6},
+			opcode.InstructionInvokeMethodStatic{ArgCount: 1},
+			opcode.InstructionTransferAndConvert{Type: 6},
+			// return
+			opcode.InstructionReturnValue{},
+		},
+		aTestFunction.Code,
+	)
+
+	assert.Equal(t,
+		[]constant.Constant{
+			{
+				Data: []byte("d"),
+				Kind: constant.String,
+			},
+		},
+		program.Constants,
+	)
+
+	assert.Equal(t,
+		[]bbq.Import{
+			{
+				Location: TestLocation,
+				Name:     "B",
+			},
+			{
+				Location: TestLocation,
+				Name:     "B.c",
+			},
+		},
+		program.Imports,
 	)
 }
