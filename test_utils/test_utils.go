6fe87bb712afe64be0919293edfb1ce6be0517c5
Bastian Müller <bastian@turbolent.com>
2025-07-09 15:21:52 -0700

    fix bad conflict resolution

--- test_utils/test_utils.go
+++ test_utils/test_utils.go
@@ -369,22 +369,6 @@ func ParseCheckAndPrepareWithOptions(
 
 				return nil
 			}
-		}
-
-		if interpreterConfig.CompositeTypeHandler != nil {
-			originalTypeLoader := vmConfig.TypeLoader
-			vmConfig.TypeLoader = func(location common.Location, typeID interpreter.TypeID) sema.Type {
-				ty := interpreterConfig.CompositeTypeHandler(location, typeID)
-				if ty != nil {
-					return ty
-				}
-
-				if originalTypeLoader != nil {
-					return originalTypeLoader(location, typeID)
-				}
-
-				return nil
-			}
 
 			vmConfig.ElaborationResolver = func(location common.Location) (*sema.Elaboration, error) {
 				impt := interpreterConfig.ImportLocationHandler(nil, location)
@@ -401,6 +385,22 @@ func ParseCheckAndPrepareWithOptions(
 				}
 
 				return elaboration, nil
+			}
+		}
+
+		if interpreterConfig.CompositeTypeHandler != nil {
+			originalTypeLoader := vmConfig.TypeLoader
+			vmConfig.TypeLoader = func(location common.Location, typeID interpreter.TypeID) sema.Type {
+				ty := interpreterConfig.CompositeTypeHandler(location, typeID)
+				if ty != nil {
+					return ty
+				}
+
+				if originalTypeLoader != nil {
+					return originalTypeLoader(location, typeID)
+				}
+
+				return nil
 			}
 		}
 	}
