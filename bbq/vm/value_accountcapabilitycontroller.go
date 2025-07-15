155f618a0d1e42cb34fdcf25b809872a23ff94f5
Supun Setunga <supun.setunga@gmail.com>
2025-07-15 12:47:21 -0700

    Lint

--- bbq/vm/value_accountcapabilitycontroller.go
+++ bbq/vm/value_accountcapabilitycontroller.go
@@ -67,7 +67,7 @@ func init() {
 				var receiver interpreter.Value
 
 				// arg[0] is the receiver. Actual arguments starts from 1.
-				receiver, args = SplitReceiverAndArgs(context, args) // nolint:staticcheck
+				receiver, _ = SplitReceiverAndArgs(context, args)
 
 				v := getCheckedAccountCapabilityControllerReceiver(receiver)
 
