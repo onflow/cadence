36773dc9bab58e58056ebc23e077ec6fbd1627ac
Brian Doyle <briandoyle81@gmail.com>
2025-07-15 09:25:30 -0400

    Add improved error messages

--- sema/errors.go
+++ sema/errors.go
@@ -418,6 +418,8 @@ type InsufficientArgumentsError struct {
 var _ SemanticError = &InsufficientArgumentsError{}
 var _ errors.UserError = &InsufficientArgumentsError{}
 var _ errors.SecondaryError = &InsufficientArgumentsError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &InsufficientArgumentsError{}
+var _ errors.HasDocumentationLink = &InsufficientArgumentsError{}
 
 func (*InsufficientArgumentsError) isSemanticError() {}
 
@@ -429,10 +431,33 @@ func (e *InsufficientArgumentsError) Error() string {
 
 func (e *InsufficientArgumentsError) SecondaryError() string {
 	return fmt.Sprintf(
-		"expected at least %d, got %d",
+		"expected at least %d, got %d. Add the missing arguments to match the function signature",
 		e.MinCount,
 		e.ActualCount,
 	)
+}
+
+func (e *InsufficientArgumentsError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	// For insufficient arguments, we suggest adding the missing arguments
+	// This is a general suggestion since we don't know which specific arguments to add
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "add missing arguments",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// Add missing arguments to match function signature\n",
+					Range: ast.Range{
+						StartPos: e.StartPos,
+						EndPos:   e.StartPos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *InsufficientArgumentsError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/functions"
 }
 
 // ExcessiveArgumentsError
@@ -446,6 +471,8 @@ type ExcessiveArgumentsError struct {
 var _ SemanticError = &ExcessiveArgumentsError{}
 var _ errors.UserError = &ExcessiveArgumentsError{}
 var _ errors.SecondaryError = &ExcessiveArgumentsError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &ExcessiveArgumentsError{}
+var _ errors.HasDocumentationLink = &ExcessiveArgumentsError{}
 
 func (*ExcessiveArgumentsError) isSemanticError() {}
 
@@ -457,10 +484,33 @@ func (e *ExcessiveArgumentsError) Error() string {
 
 func (e *ExcessiveArgumentsError) SecondaryError() string {
 	return fmt.Sprintf(
-		"expected up to %d, got %d",
+		"expected up to %d, got %d. Remove the extra arguments to match the function signature",
 		e.MaxCount,
 		e.ActualCount,
 	)
+}
+
+func (e *ExcessiveArgumentsError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	// For excessive arguments, we suggest removing the extra arguments
+	// This is a general suggestion since we don't know which specific arguments to remove
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "remove extra arguments",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// Remove extra arguments to match function signature\n",
+					Range: ast.Range{
+						StartPos: e.StartPos,
+						EndPos:   e.StartPos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *ExcessiveArgumentsError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/functions"
 }
 
 // MissingArgumentLabelError
@@ -516,6 +566,7 @@ var _ SemanticError = &IncorrectArgumentLabelError{}
 var _ errors.UserError = &IncorrectArgumentLabelError{}
 var _ errors.SecondaryError = &IncorrectArgumentLabelError{}
 var _ errors.HasSuggestedFixes[ast.TextEdit] = &IncorrectArgumentLabelError{}
+var _ errors.HasDocumentationLink = &IncorrectArgumentLabelError{}
 
 func (*IncorrectArgumentLabelError) isSemanticError() {}
 
@@ -531,7 +582,7 @@ func (e *IncorrectArgumentLabelError) SecondaryError() string {
 		expected = fmt.Sprintf("`%s`", e.ExpectedArgumentLabel)
 	}
 	return fmt.Sprintf(
-		"expected %s, got `%s`",
+		"expected %s, got `%s`. Function calls must use the exact argument labels defined in the function signature",
 		expected,
 		e.ActualArgumentLabel,
 	)
@@ -579,6 +630,10 @@ func (e *IncorrectArgumentLabelError) SuggestFixes(code string) []errors.Suggest
 			},
 		}
 	}
+}
+
+func (e *IncorrectArgumentLabelError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/functions"
 }
 
 // InvalidUnaryOperandError
@@ -1159,6 +1214,9 @@ type FieldReinitializationError struct {
 
 var _ SemanticError = &FieldReinitializationError{}
 var _ errors.UserError = &FieldReinitializationError{}
+var _ errors.SecondaryError = &FieldReinitializationError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &FieldReinitializationError{}
+var _ errors.HasDocumentationLink = &FieldReinitializationError{}
 
 func (*FieldReinitializationError) isSemanticError() {}
 
@@ -1166,6 +1224,31 @@ func (*FieldReinitializationError) IsUserError() {}
 
 func (e *FieldReinitializationError) Error() string {
 	return fmt.Sprintf("invalid reinitialization of field: `%s`", e.Name)
+}
+
+func (e *FieldReinitializationError) SecondaryError() string {
+	return "fields can only be initialized once. Remove the duplicate initialization or use assignment instead"
+}
+
+func (e *FieldReinitializationError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "remove duplicate field initialization",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// Remove duplicate initialization - use assignment instead\n",
+					Range: ast.Range{
+						StartPos: e.StartPos,
+						EndPos:   e.StartPos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *FieldReinitializationError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/types-and-type-system/composite-types#composite-type-fields"
 }
 
 // FieldUninitializedError
@@ -1178,6 +1261,8 @@ type FieldUninitializedError struct {
 var _ SemanticError = &FieldUninitializedError{}
 var _ errors.UserError = &FieldUninitializedError{}
 var _ errors.SecondaryError = &FieldUninitializedError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &FieldUninitializedError{}
+var _ errors.HasDocumentationLink = &FieldUninitializedError{}
 
 func (*FieldUninitializedError) isSemanticError() {}
 
@@ -1192,7 +1277,28 @@ func (e *FieldUninitializedError) Error() string {
 }
 
 func (e *FieldUninitializedError) SecondaryError() string {
-	return "not initialized"
+	return "all fields must be initialized when creating a composite type. Add an initializer or provide a default value"
+}
+
+func (e *FieldUninitializedError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "initialize the field",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// Add field initialization: fieldName: value\n",
+					Range: ast.Range{
+						StartPos: e.Pos,
+						EndPos:   e.Pos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *FieldUninitializedError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/types-and-type-system/composite-types#composite-type-fields"
 }
 
 func (e *FieldUninitializedError) StartPosition() ast.Position {
@@ -1223,6 +1329,8 @@ type FieldTypeNotStorableError struct {
 var _ SemanticError = &FieldTypeNotStorableError{}
 var _ errors.UserError = &FieldTypeNotStorableError{}
 var _ errors.SecondaryError = &FieldTypeNotStorableError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &FieldTypeNotStorableError{}
+var _ errors.HasDocumentationLink = &FieldTypeNotStorableError{}
 
 func (*FieldTypeNotStorableError) isSemanticError() {}
 
@@ -1237,7 +1345,28 @@ func (e *FieldTypeNotStorableError) Error() string {
 }
 
 func (e *FieldTypeNotStorableError) SecondaryError() string {
-	return "all contract fields must be storable"
+	return "all contract fields must be storable. Use concrete types like `String`, `Int`, `Bool`, or specific struct/resource types instead of `Any` or function types"
+}
+
+func (e *FieldTypeNotStorableError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "use a storable type",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// Replace with a concrete storable type like String, Int, Bool, or a specific struct/resource\n",
+					Range: ast.Range{
+						StartPos: e.Pos,
+						EndPos:   e.Pos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *FieldTypeNotStorableError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/types-and-type-system/composite-types#composite-type-fields"
 }
 
 func (e *FieldTypeNotStorableError) StartPosition() ast.Position {
@@ -1257,6 +1386,9 @@ type FunctionExpressionInConditionError struct {
 
 var _ SemanticError = &FunctionExpressionInConditionError{}
 var _ errors.UserError = &FunctionExpressionInConditionError{}
+var _ errors.SecondaryError = &FunctionExpressionInConditionError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &FunctionExpressionInConditionError{}
+var _ errors.HasDocumentationLink = &FunctionExpressionInConditionError{}
 
 func (*FunctionExpressionInConditionError) isSemanticError() {}
 
@@ -1264,6 +1396,31 @@ func (*FunctionExpressionInConditionError) IsUserError() {}
 
 func (e *FunctionExpressionInConditionError) Error() string {
 	return "condition contains function"
+}
+
+func (e *FunctionExpressionInConditionError) SecondaryError() string {
+	return "conditions must evaluate to a boolean value. Call the function to get its return value"
+}
+
+func (e *FunctionExpressionInConditionError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "call the function to get its return value",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// Add () to call the function: functionName()\n",
+					Range: ast.Range{
+						StartPos: e.StartPos,
+						EndPos:   e.StartPos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *FunctionExpressionInConditionError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/control-flow"
 }
 
 // InvalidEmitConditionError
@@ -1679,6 +1836,9 @@ type InterfaceMemberConflictError struct {
 
 var _ SemanticError = &InterfaceMemberConflictError{}
 var _ errors.UserError = &InterfaceMemberConflictError{}
+var _ errors.SecondaryError = &InterfaceMemberConflictError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &InterfaceMemberConflictError{}
+var _ errors.HasDocumentationLink = &InterfaceMemberConflictError{}
 
 func (*InterfaceMemberConflictError) isSemanticError() {}
 
@@ -1693,6 +1853,31 @@ func (e *InterfaceMemberConflictError) Error() string {
 		e.ConflictingMemberKind.Name(),
 		e.ConflictingInterfaceType.QualifiedString(),
 	)
+}
+
+func (e *InterfaceMemberConflictError) SecondaryError() string {
+	return "interface members must have unique names. Rename one of the conflicting members to resolve the conflict"
+}
+
+func (e *InterfaceMemberConflictError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "rename the conflicting member",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// Rename this member to avoid conflict with the other interface\n",
+					Range: ast.Range{
+						StartPos: e.StartPos,
+						EndPos:   e.StartPos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *InterfaceMemberConflictError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/interfaces"
 }
 
 // MissingConformanceError
@@ -1793,6 +1978,9 @@ type ImportedProgramError struct {
 var _ SemanticError = &ImportedProgramError{}
 var _ errors.UserError = &ImportedProgramError{}
 var _ errors.ParentError = &ImportedProgramError{}
+var _ errors.SecondaryError = &ImportedProgramError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &ImportedProgramError{}
+var _ errors.HasDocumentationLink = &ImportedProgramError{}
 
 func (*ImportedProgramError) isSemanticError() {}
 
@@ -1800,9 +1988,34 @@ func (*ImportedProgramError) IsUserError() {}
 
 func (e *ImportedProgramError) Error() string {
 	return fmt.Sprintf(
-		"checking of imported program `%s` failed",
+		"checking of imported program (smart contract) `%s` failed",
 		e.Location,
 	)
+}
+
+func (e *ImportedProgramError) SecondaryError() string {
+	return "the imported program (smart contract) contains errors that must be fixed before it can be used. Check the imported program for syntax or semantic errors"
+}
+
+func (e *ImportedProgramError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "fix errors in the imported program (smart contract)",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// Fix errors in the imported program (smart contract) before using it\n",
+					Range: ast.Range{
+						StartPos: e.StartPos,
+						EndPos:   e.StartPos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *ImportedProgramError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/imports"
 }
 
 func (e *ImportedProgramError) ImportLocation() common.Location {
@@ -2189,6 +2402,8 @@ type IncorrectTransferOperationError struct {
 var _ SemanticError = &IncorrectTransferOperationError{}
 var _ errors.UserError = &IncorrectTransferOperationError{}
 var _ errors.SecondaryError = &IncorrectTransferOperationError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &IncorrectTransferOperationError{}
+var _ errors.HasDocumentationLink = &IncorrectTransferOperationError{}
 
 func (*IncorrectTransferOperationError) isSemanticError() {}
 
@@ -2200,9 +2415,27 @@ func (e *IncorrectTransferOperationError) Error() string {
 
 func (e *IncorrectTransferOperationError) SecondaryError() string {
 	return fmt.Sprintf(
-		"expected `%s`",
+		"expected `%s`. Transfer operations must match the expected operation for the context (e.g., `<-` for move, `<-!` for force move)",
 		e.ExpectedOperation.Operator(),
 	)
+}
+
+func (e *IncorrectTransferOperationError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "use the correct transfer operation",
+			TextEdits: []ast.TextEdit{
+				{
+					Replacement: e.ExpectedOperation.Operator(),
+					Range:       e.Range,
+				},
+			},
+		},
+	}
+}
+
+func (e *IncorrectTransferOperationError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/resources"
 }
 
 // InvalidConstructionError
@@ -3651,17 +3884,45 @@ type IntersectionCompositeKindMismatchError struct {
 
 var _ SemanticError = &IntersectionCompositeKindMismatchError{}
 var _ errors.UserError = &IntersectionCompositeKindMismatchError{}
+var _ errors.SecondaryError = &IntersectionCompositeKindMismatchError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &IntersectionCompositeKindMismatchError{}
+var _ errors.HasDocumentationLink = &IntersectionCompositeKindMismatchError{}
 
 func (*IntersectionCompositeKindMismatchError) isSemanticError() {}
 
 func (*IntersectionCompositeKindMismatchError) IsUserError() {}
 
 func (e *IntersectionCompositeKindMismatchError) Error() string {
+	return "interface kinds in intersection type must match"
+}
+
+func (e *IntersectionCompositeKindMismatchError) SecondaryError() string {
 	return fmt.Sprintf(
-		"interface kind %s does not match previous interface kind %s",
-		e.CompositeKind,
-		e.PreviousCompositeKind,
+		"expected `%s`, got `%s`. All interfaces in an intersection type must have the same composite kind (struct, resource, contract, etc.)",
+		e.PreviousCompositeKind.Name(),
+		e.CompositeKind.Name(),
 	)
+}
+
+func (e *IntersectionCompositeKindMismatchError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "ensure all interfaces have the same composite kind",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// All interfaces in intersection types must have the same composite kind\n",
+					Range: ast.Range{
+						StartPos: e.StartPos,
+						EndPos:   e.StartPos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *IntersectionCompositeKindMismatchError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/types-and-type-system/intersection-types"
 }
 
 // InvalidIntersectionTypeDuplicateError
@@ -3696,6 +3957,9 @@ type IntersectionMemberClashError struct {
 
 var _ SemanticError = &IntersectionMemberClashError{}
 var _ errors.UserError = &IntersectionMemberClashError{}
+var _ errors.SecondaryError = &IntersectionMemberClashError{}
+var _ errors.HasSuggestedFixes[ast.TextEdit] = &IntersectionMemberClashError{}
+var _ errors.HasDocumentationLink = &IntersectionMemberClashError{}
 
 func (*IntersectionMemberClashError) isSemanticError() {}
 
@@ -3703,10 +3967,39 @@ func (*IntersectionMemberClashError) IsUserError() {}
 
 func (e *IntersectionMemberClashError) Error() string {
 	return fmt.Sprintf(
-		"intersected type has member clash with previous intersected type `%s`: %s",
-		e.OriginalDeclaringType.QualifiedString(),
+		"member `%s` conflicts between intersection types",
 		e.Name,
 	)
+}
+
+func (e *IntersectionMemberClashError) SecondaryError() string {
+	return fmt.Sprintf(
+		"member `%s` is declared in both `%s` and `%s`. Intersection types cannot have conflicting member declarations with the same name",
+		e.Name,
+		e.OriginalDeclaringType.QualifiedString(),
+		e.RedeclaringType.QualifiedString(),
+	)
+}
+
+func (e *IntersectionMemberClashError) SuggestFixes(_ string) []errors.SuggestedFix[ast.TextEdit] {
+	return []errors.SuggestedFix[ast.TextEdit]{
+		{
+			Message: "resolve member name conflict in intersection types",
+			TextEdits: []ast.TextEdit{
+				{
+					Insertion: "// Rename conflicting member or remove one of the declarations\n",
+					Range: ast.Range{
+						StartPos: e.StartPos,
+						EndPos:   e.StartPos,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (e *IntersectionMemberClashError) DocumentationLink() string {
+	return "https://cadence-lang.org/docs/language/types-and-type-system/intersection-types"
 }
 
 // AmbiguousIntersectionTypeError
