cd42eb491021db75443f0c22eebe11e24d3662f6
Supun Setunga <supun.setunga@gmail.com>
2025-08-27 08:13:10 -0700

    Use truncation division for big.Int divisions

--- interpreter/value_uint128.go
+++ interpreter/value_uint128.go
@@ -376,7 +376,7 @@ func (v UInt128Value) Div(context NumberValueArithmeticContext, other NumberValu
 					LocationRange: locationRange,
 				})
 			}
-			return res.Div(v.BigInt, o.BigInt)
+			return res.Quo(v.BigInt, o.BigInt)
 		},
 	)
 
