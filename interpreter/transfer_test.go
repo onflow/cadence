2b59ec6eb1cdf4ea2e1e992b4d8fdd00b1686e1c
Supun Setunga <supun.setunga@gmail.com>
2025-09-26 08:55:04 -0700

    Rename function. Fix import.

--- interpreter/transfer_test.go
+++ interpreter/transfer_test.go
@@ -19,9 +19,9 @@
 package interpreter_test
 
 import (
-	"github.com/zeebo/assert"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/cadence/activations"
