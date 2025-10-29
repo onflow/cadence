6444a7a15cd96c189a341b55e7655da6cfcc7f64
Supun Setunga <supun.setunga@gmail.com>
2025-10-29 12:42:33 -0700

    Add position info to parser errors

--- tools/subtype-gen/rules_parser.go
+++ tools/subtype-gen/rules_parser.go
@@ -68,7 +68,11 @@ func ParseRules() (RulesFile, error) {
 func parseRulesFromDocument(doc *ast.DocumentNode) (RulesFile, error) {
 	rules, ok := doc.Body.(*ast.MappingNode)
 	if !ok {
-		return RulesFile{}, fmt.Errorf("failed to parse YAML: expected a mapping, found %T", doc.Body)
+		return RulesFile{},
+			NewParsingError(
+				fmt.Sprintf("failed to parse YAML: expected a mapping, found %T", doc.Body),
+				doc.Body,
+			)
 	}
 
 	// Should contain `rules: ...`
@@ -77,7 +81,11 @@ func parseRulesFromDocument(doc *ast.DocumentNode) (RulesFile, error) {
 		return RulesFile{}, err
 	}
 	if key != "rules" {
-		return RulesFile{}, fmt.Errorf("failed to parse YAML: expected key 'rules', found '%s'", key)
+		return RulesFile{},
+			NewParsingError(
+				fmt.Sprintf("failed to parse YAML: expected key 'rules', found '%s'", key),
+				rules,
+			)
 	}
 
 	// Value must be a list of rules
@@ -91,7 +99,11 @@ func parseRulesFromDocument(doc *ast.DocumentNode) (RulesFile, error) {
 	for _, rule := range rulesList {
 		ruleFields, ok := rule.(*ast.MappingNode)
 		if !ok {
-			return RulesFile{}, fmt.Errorf("failed to parse YAML: expected a mapping, found %T", rule)
+			return RulesFile{},
+				NewParsingError(
+					fmt.Sprintf("failed to parse YAML: expected a mapping, found %T", rule),
+					rule,
+				)
 		}
 
 		var typ Type
@@ -111,7 +123,11 @@ func parseRulesFromDocument(doc *ast.DocumentNode) (RulesFile, error) {
 			case "super":
 				stringNode, ok := propertyValue.(*ast.StringNode)
 				if !ok {
-					return RulesFile{}, fmt.Errorf("failed to parse YAML: expected a string key, found %T", propertyValue)
+					return RulesFile{},
+						NewParsingError(
+							fmt.Sprintf("failed to parse YAML: expected a string key, found %T", propertyValue),
+							propertyValue,
+						)
 				}
 
 				typ = parseType(stringNode.Value)
@@ -123,7 +139,11 @@ func parseRulesFromDocument(doc *ast.DocumentNode) (RulesFile, error) {
 				}
 
 			default:
-				return RulesFile{}, fmt.Errorf("failed to parse YAML: property '%s' is unsupported", property)
+				return RulesFile{},
+					NewParsingError(
+						fmt.Sprintf("failed to parse YAML: property '%s' is unsupported", property),
+						property,
+					)
 			}
 		}
 
@@ -146,6 +166,7 @@ func parseRulesFromDocument(doc *ast.DocumentNode) (RulesFile, error) {
 // parseType parses a type with just a name.
 func parseType(typePlaceHolder string) Type {
 	if !strings.HasSuffix(typePlaceHolder, TypePlaceholderSuffix) {
+		// TODO: maybe return error?
 		panic(fmt.Errorf(
 			"type name %#[1]q is not suffixed with %#[2]q."+
 				" Replace the type-name with `%[1]s%[2]s`",
@@ -212,7 +233,11 @@ func parsePredicate(predicate ast.Node) (Predicate, error) {
 				description: description,
 			}, nil
 		default:
-			return nil, fmt.Errorf("unsupported string predicate: %s", predicate.Value)
+			return nil,
+				NewParsingError(
+					fmt.Sprintf("unsupported string predicate: %s", predicate.Value),
+					predicate,
+				)
 		}
 
 	case *ast.MappingNode:
@@ -383,7 +408,11 @@ func parsePredicate(predicate ast.Node) (Predicate, error) {
 			// Get source
 			source, ok := keyValues["source"]
 			if !ok {
-				return nil, fmt.Errorf("cannot find `source` property for `mustType` predicate")
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("cannot find `source` property for `mustType` predicate"),
+						value,
+					)
 			}
 
 			sourceExpr, err := parseExpression(source)
@@ -394,12 +423,20 @@ func parsePredicate(predicate ast.Node) (Predicate, error) {
 			// Get target
 			typ, ok := keyValues["type"]
 			if !ok {
-				return nil, fmt.Errorf("cannot find `target` property for `mustType` predicate")
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("cannot find `target` property for `mustType` predicate"),
+						value,
+					)
 			}
 
 			typeStr, ok := typ.(*ast.StringNode)
 			if !ok {
-				return nil, fmt.Errorf("type placeholder must be a string, got %s", typ)
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("type placeholder must be a string, got %s", typ),
+						typ,
+					)
 			}
 
 			expectedType := parseType(typeStr.Value)
@@ -421,7 +458,11 @@ func parsePredicate(predicate ast.Node) (Predicate, error) {
 			// Get the set
 			set, ok := keyValues["set"]
 			if !ok {
-				return nil, fmt.Errorf("cannot find `set` property for `setContains` predicate")
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("cannot find `set` property for `setContains` predicate"),
+						value,
+					)
 			}
 
 			setExpr, err := parseExpression(set)
@@ -432,7 +473,11 @@ func parsePredicate(predicate ast.Node) (Predicate, error) {
 			// Get element
 			element, ok := keyValues["element"]
 			if !ok {
-				return nil, fmt.Errorf("cannot find `element` property for `setContains` predicate")
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("cannot find `element` property for `setContains` predicate"),
+						value,
+					)
 			}
 
 			elementExpr, err := parseExpression(element)
@@ -498,7 +543,11 @@ func parsePredicate(predicate ast.Node) (Predicate, error) {
 			// Get source
 			source, ok := keyValues["source"]
 			if !ok {
-				return nil, fmt.Errorf("cannot find `source` property for %#q predicate", key)
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("cannot find `source` property for %#q predicate", key),
+						value,
+					)
 			}
 
 			sourceExpr, err := parseExpression(source)
@@ -509,7 +558,11 @@ func parsePredicate(predicate ast.Node) (Predicate, error) {
 			// Get target
 			target, ok := keyValues["target"]
 			if !ok {
-				return nil, fmt.Errorf("cannot find `target` property for %#q predicate", key)
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("cannot find `target` property for %#q predicate", key),
+						value,
+					)
 			}
 
 			targetExpr, err := parseExpression(target)
@@ -520,7 +573,11 @@ func parsePredicate(predicate ast.Node) (Predicate, error) {
 			// Get inner predicate
 			predicate, ok := keyValues["predicate"]
 			if !ok {
-				return nil, fmt.Errorf("cannot find `predicate` property for %#q predicate", key)
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("cannot find `predicate` property for %#q predicate", key),
+						value,
+					)
 			}
 
 			innerPredicate, err := parsePredicate(predicate)
@@ -535,11 +592,19 @@ func parsePredicate(predicate ast.Node) (Predicate, error) {
 				Predicate:   innerPredicate,
 			}, nil
 		default:
-			return nil, fmt.Errorf("unsupported predicate: %s", key)
+			return nil,
+				NewParsingError(
+					fmt.Sprintf("unsupported predicate: %s", key),
+					predicate,
+				)
 		}
 
 	default:
-		return nil, fmt.Errorf("unsupported predicate type: %T", predicate)
+		return nil,
+			NewParsingError(
+				fmt.Sprintf("unsupported predicate type: %T", predicate),
+				predicate,
+			)
 	}
 }
 
@@ -555,7 +620,11 @@ func nodeComments(node ast.Node) string {
 func nodeAsKeyValues(node ast.Node) (keyValues KeyValues, comments string, err error) {
 	mappingNode, ok := node.(*ast.MappingNode)
 	if !ok {
-		return nil, "", fmt.Errorf("expected KeyValues, got %s", node.Type())
+		return nil, "",
+			NewParsingError(
+				fmt.Sprintf("expected KeyValues, got %s", node.Type()),
+				node,
+			)
 	}
 
 	comments += nodeComments(node)
@@ -586,7 +655,11 @@ func stringKeyAndValueFromPair(pair *ast.MappingValueNode) (
 	keyNode := pair.Key
 	strKey, ok := keyNode.(*ast.StringNode)
 	if !ok {
-		return "", nil, "", fmt.Errorf("expected string-type key, got %s", keyNode.Type())
+		return "", nil, "",
+			NewParsingError(
+				fmt.Sprintf("expected string-type key, got %s", keyNode.Type()),
+				keyNode,
+			)
 	}
 
 	valueNode := pair.Value
@@ -599,7 +672,11 @@ func stringKeyAndValueFromPair(pair *ast.MappingValueNode) (
 func nodeAsList(node ast.Node) ([]ast.Node, error) {
 	sequenceNode, ok := node.(*ast.SequenceNode)
 	if !ok {
-		return nil, fmt.Errorf("expected a list, got %s", node.Type())
+		return nil,
+			NewParsingError(
+				fmt.Sprintf("expected a list, got %s", node.Type()),
+				node,
+			)
 	}
 
 	list := sequenceNode.Values
@@ -614,7 +691,11 @@ func nodeAsList(node ast.Node) ([]ast.Node, error) {
 
 			// Both the list comment and the first item comment cannot exist together.
 			if listComment != nil && itemComment != nil {
-				return nil, fmt.Errorf("found comments for both the list and the first item")
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("found comments for both the list and the first item"),
+						item,
+					)
 			}
 
 			err := item.SetComment(listComment)
@@ -649,7 +730,11 @@ func parseSourceAndTarget(predicateName string, value ast.Node) (
 	// Get source
 	source, ok := keyValues["source"]
 	if !ok {
-		return nil, nil, "", fmt.Errorf("cannot find `source` property for %#q predicate", predicateName)
+		return nil, nil, "",
+			NewParsingError(
+				fmt.Sprintf("cannot find `source` property for %#q predicate", predicateName),
+				value,
+			)
 	}
 
 	sourceExpr, err := parseExpression(source)
@@ -660,7 +745,11 @@ func parseSourceAndTarget(predicateName string, value ast.Node) (
 	// Get target
 	target, ok := keyValues["target"]
 	if !ok {
-		return nil, nil, "", fmt.Errorf("cannot find `target` property for %#q predicate", predicateName)
+		return nil, nil, "",
+			NewParsingError(
+				fmt.Sprintf("cannot find `target` property for %#q predicate", predicateName),
+				value,
+			)
 	}
 
 	targetExpr, err := parseExpression(target)
@@ -685,7 +774,11 @@ func parseSuperAndSubExpressions(predicateName string, value ast.Node) (
 	// Get super type
 	super, ok := keyValues["super"]
 	if !ok {
-		return nil, nil, "", fmt.Errorf("cannot find `super` property for %#q predicate", predicateName)
+		return nil, nil, "",
+			NewParsingError(
+				fmt.Sprintf("cannot find `super` property for %#q predicate", predicateName),
+				value,
+			)
 	}
 
 	superType, err := parseExpression(super)
@@ -696,7 +789,11 @@ func parseSuperAndSubExpressions(predicateName string, value ast.Node) (
 	// Get subtype
 	sub, ok := keyValues["sub"]
 	if !ok {
-		return nil, nil, "", fmt.Errorf("cannot find `sub` property for %#q predicate", predicateName)
+		return nil, nil, "",
+			NewParsingError(
+				fmt.Sprintf("cannot find `sub` property for %#q predicate", predicateName),
+				value,
+			)
 	}
 
 	subType, err := parseExpression(sub)
@@ -715,7 +812,11 @@ func singleKeyValueFromMap(mappingNode *ast.MappingNode) (
 ) {
 	keyValuePairs := mappingNode.Values
 	if len(keyValuePairs) != 1 {
-		return "", nil, "", fmt.Errorf("expected exactly one key value pair")
+		return "", nil, "",
+			NewParsingError(
+				fmt.Sprintf("expected exactly one key value pair"),
+				mappingNode,
+			)
 	}
 
 	key, value, comments, err = stringKeyAndValueFromPair(keyValuePairs[0])
@@ -738,7 +839,11 @@ func parseExpression(expr ast.Node) (Expression, error) {
 		case "oneOf":
 			list, ok := value.(*ast.SequenceNode)
 			if !ok {
-				return nil, fmt.Errorf("expected a list of predicates, got %s", value.Type())
+				return nil,
+					NewParsingError(
+						fmt.Sprintf("expected a list of predicates, got %s", value.Type()),
+						value,
+					)
 			}
 
 			var expressions []Expression
@@ -753,11 +858,19 @@ func parseExpression(expr ast.Node) (Expression, error) {
 
 			return OneOfExpression{Expressions: expressions}, nil
 		default:
-			return nil, fmt.Errorf("unsupported key: %s", key)
+			return nil,
+				NewParsingError(
+					fmt.Sprintf("unsupported key: %s", key),
+					expr,
+				)
 		}
 
 	default:
-		return nil, fmt.Errorf("unsupported expression: %v", expr)
+		return nil,
+			NewParsingError(
+				fmt.Sprintf("unsupported expression: %v", expr),
+				expr,
+			)
 	}
 }
 
