3378a56f00a62070064b62e296aca7b74e82c380
Supun Setunga <supun.setunga@gmail.com>
2025-11-20 15:48:15 -0800

    Add test for self referencing declarations

---
+++ sema/check_declaration_test.go
@@ -0,0 +1,84 @@
+/*
+ * Cadence - The resource-oriented smart contract programming language
+ *
+ * Copyright Flow Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sema_test
+
+import (
+	"testing"
+
+	. "github.com/onflow/cadence/test_utils/sema_utils"
+)
+
+func TestCheckSelfReferencingDeclaration(t *testing.T) {
+	t.Parallel()
+
+	t.Run("attachment", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := ParseAndCheck(t, `
+            attachment A for A {
+                fun f(_: A) {}
+            }
+
+            fun main(): Int {
+                return 0
+            }
+	    `)
+
+		errs := RequireCheckerErrors(t, err, 1)
+		_ = errs
+	})
+
+	t.Run("contract interface", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := ParseAndCheck(t, `
+            resource R {
+                contract interface I: I {
+                    init()
+                }
+            }
+
+            fun main(): Int {
+                return 0
+            }
+	    `)
+
+		errs := RequireCheckerErrors(t, err, 1)
+		_ = errs
+	})
+
+	t.Run("struct interface", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := ParseAndCheck(t, `
+           struct interface SI: SI {
+               fun foo() {
+                   (self.foo.foo.foo.foo)
+               }
+           }
+
+           fun main(): Int {
+               return 0
+           }
+	    `)
+
+		errs := RequireCheckerErrors(t, err, 1)
+		_ = errs
+	})
+}
