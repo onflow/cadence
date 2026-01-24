28dd7115e84b3dcc101233ea2c20ebf83ed313a5
Supun Setunga <supun.setunga@gmail.com>
2026-01-23 16:37:03 -0800

    Cleanup

--- interpreter/value_composite.go
+++ interpreter/value_composite.go
@@ -1288,15 +1288,9 @@ func (v *CompositeValue) Transfer(
 	needsStoreTo := v.NeedsStoreTo(address)
 	isResourceKinded := v.IsResourceKinded(context)
 
-	if needsStoreTo && v.Kind == common.CompositeKindContract {
-		panic(&NonTransferableValueError{
-			Value: v,
-		})
-	}
-
 	if needsStoreTo {
 		if v.Kind == common.CompositeKindContract {
-			panic(NonTransferableValueError{
+			panic(&NonTransferableValueError{
 				Value: v,
 			})
 		}
