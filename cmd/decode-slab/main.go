4c06aa5460891eb8e305be0bc4870128b7c7b242
Bastian Müller <bastian@turbolent.com>
2026-04-01 10:48:25 -0700

    add support for array data slabs, keep track of path into children

--- cmd/decode-slab/main.go
+++ cmd/decode-slab/main.go
@@ -112,18 +112,73 @@ func main() {
 		fmt.Println(slab2)
 		fmt.Println()
 
+		rootPath := fmt.Sprintf("slab(%s)", slabID)
+
 		if mapDataSlab1, ok := slab1.(*atree.MapDataSlab); ok {
 			if mapDataSlab2, ok := slab2.(*atree.MapDataSlab); ok {
-				compareMapDataSlabs(mapDataSlab1, mapDataSlab2)
+				diffs := compareMapDataSlabs(rootPath, mapDataSlab1, mapDataSlab2)
+				if diffs == 0 {
+					fmt.Println("No differences found")
+				} else {
+					fmt.Printf("%d difference(s) found\n", diffs)
+					os.Exit(1)
+				}
 				return
 			}
 		}
 
-		if !bytes.Equal(data, data2) {
-			fmt.Printf("Slabs are different!")
+		if arrayDataSlab1, ok := slab1.(*atree.ArrayDataSlab); ok {
+			if arrayDataSlab2, ok := slab2.(*atree.ArrayDataSlab); ok {
+				diffs := compareArrayDataSlabs(rootPath, arrayDataSlab1, arrayDataSlab2)
+				if diffs == 0 {
+					fmt.Println("No differences found")
+				} else {
+					fmt.Printf("%d difference(s) found\n", diffs)
+					os.Exit(1)
+				}
+				return
+			}
+		}
+
+		if bytes.Equal(data, data2) {
+			fmt.Println("No differences found")
+		} else {
+			fmt.Println("Slabs are different!")
 			os.Exit(1)
 		}
 	}
+}
+
+func compareArrayDataSlabs(path string, slab1 *atree.ArrayDataSlab, slab2 *atree.ArrayDataSlab) int {
+	diffs := 0
+
+	childStorables1 := slab1.ChildStorables()
+	childStorables2 := slab2.ChildStorables()
+
+	if len(childStorables1) != len(childStorables2) {
+		fmt.Printf("%s: different count: %d vs %d\n", path, len(childStorables1), len(childStorables2))
+		diffs++
+	}
+
+	for i, childStorable1 := range childStorables1 {
+		childPath := fmt.Sprintf("%s[%d]", path, i)
+
+		if i >= len(childStorables2) {
+			fmt.Printf("%s: missing in slab 2\n", childPath)
+			diffs++
+			continue
+		}
+
+		childStorable2 := childStorables2[i]
+
+		diffs += compareChildStorables(
+			childPath,
+			childStorable1,
+			childStorable2,
+		)
+	}
+
+	return diffs
 }
 
 type mapEntry struct {
@@ -141,10 +196,12 @@ func newMapDataSlabIndexer(entries map[string]mapEntry) func(key atree.MapKey, v
 	}
 }
 
-func compareMapDataSlabs(slab1 *atree.MapDataSlab, slab2 *atree.MapDataSlab) {
+func compareMapDataSlabs(path string, slab1 *atree.MapDataSlab, slab2 *atree.MapDataSlab) int {
+	diffs := 0
 
 	if slab1.Count() != slab2.Count() {
-		fmt.Printf("Different count: %d vs %d\n", slab1.Count(), slab2.Count())
+		fmt.Printf("%s: different count: %d vs %d\n", path, slab1.Count(), slab2.Count())
+		diffs++
 	}
 
 	entries1 := make(map[string]mapEntry)
@@ -162,36 +219,68 @@ func compareMapDataSlabs(slab1 *atree.MapDataSlab, slab2 *atree.MapDataSlab) {
 	for encodedKey, entry1 := range entries1 { //nolint:maprange
 		entry2, ok := entries2[encodedKey]
 		if !ok {
-			fmt.Printf("Key %q missing in slab 2\n", entry1.key)
+			fmt.Printf("%s[%q]: missing in slab 2\n", path, entry1.key)
+			diffs++
 			continue
 		}
 
 		value1 := entry1.value
 		value2 := entry2.value
 
-		if mapDataSlabValue1, ok := value1.(*atree.MapDataSlab); ok {
-			if mapDataSlabValue2, ok := value2.(*atree.MapDataSlab); ok {
-				compareMapDataSlabs(mapDataSlabValue1, mapDataSlabValue2)
-				continue
-			}
-		}
-
-		if !bytes.Equal(encodeStorable(value1), encodeStorable(value2)) {
-			fmt.Printf(
-				"Different value for key %q: %q vs %q\n",
-				entry1.key,
-				value1,
-				value2,
-			)
-		}
+		diffs += compareChildStorables(
+			fmt.Sprintf("%s[%q]", path, entry1.key),
+			value1,
+			value2,
+		)
 	}
 
 	for encodedKey, entry2 := range entries2 { //nolint:maprange
 		_, ok := entries1[encodedKey]
 		if !ok {
-			fmt.Printf("Key %q missing in slab 1\n", entry2.key)
+			fmt.Printf("%s[%q]: missing in slab 1\n", path, entry2.key)
+			diffs++
 		}
 	}
+
+	return diffs
+}
+
+func compareChildStorables(path string, storable1, storable2 atree.Storable) int {
+	if mapDataSlabValue1, ok := storable1.(*atree.MapDataSlab); ok {
+		if mapDataSlabValue2, ok := storable2.(*atree.MapDataSlab); ok {
+			return compareMapDataSlabs(path, mapDataSlabValue1, mapDataSlabValue2)
+		}
+	}
+
+	if arrayDataSlabValue1, ok := storable1.(*atree.ArrayDataSlab); ok {
+		if arrayDataSlabValue2, ok := storable2.(*atree.ArrayDataSlab); ok {
+			return compareArrayDataSlabs(path, arrayDataSlabValue1, arrayDataSlabValue2)
+		}
+	}
+
+	if someStorable1, ok := storable1.(interpreter.SomeStorable); ok {
+		if someStorable2, ok := storable2.(interpreter.SomeStorable); ok {
+			return compareChildStorables(
+				path+".some",
+				someStorable1.Storable,
+				someStorable2.Storable,
+			)
+		}
+	}
+
+	if !bytes.Equal(encodeStorable(storable1), encodeStorable(storable2)) {
+		fmt.Printf(
+			"%s: %q (%T) vs %q (%T)\n",
+			path,
+			storable1,
+			storable1,
+			storable2,
+			storable2,
+		)
+		return 1
+	}
+
+	return 0
 }
 
 func encodeStorable(storable atree.Storable) []byte {
